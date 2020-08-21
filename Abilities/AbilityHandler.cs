bef3265ae21732f12281e69780dec2fc2b39df2a
bobfishkins23 <bobfishkins23@yahoo.com>
2020-08-21 02:39:40 -0400

    teh astral ability

--- Abilities/AbilityHandler.cs
+++ Abilities/AbilityHandler.cs
@@ -22,8 +22,6 @@ namespace StarlightRiver.Abilities
             get => activeAbility; 
             internal set
             {
-                if (ReferenceEquals(value, ActiveAbility))
-                    return;
                 if (value is null || Stamina > value.ActivationCost)
                 {
                     if (activeAbility != null)
--- Abilities/Infusion/DashAstral.cs
+++ Abilities/Infusion/DashAstral.cs
@@ -1,4 +1,5 @@
-﻿using StarlightRiver.Abilities.Content;
+﻿using Microsoft.Xna.Framework;
+using StarlightRiver.Abilities.Content;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria;
 using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
 
 namespace StarlightRiver.Abilities.Infusion
 {
@@ -39,7 +41,15 @@ namespace StarlightRiver.Abilities.Infusion
 
         public override void UpdateActiveEffects()
         {
-            Main.NewText("SHIT!");
+            Vector2 prevPos = Ability.Player.Center + Vector2.Normalize(Ability.Player.velocity) * 10;
+            int direction = Ability.time % 2 == 0 ? -1 : 1;
+
+            for (int k = 0; k < 60; k++)
+            {
+                float rot = (0.1f * k) * direction;
+                Dust dus = Dust.NewDustPerfect(prevPos + Vector2.Normalize(Ability.Player.velocity).RotatedBy(rot) * (k / 2) * (0.5f + Ability.time / 8f), DustType<Dusts.Starlight>());
+                dus.fadeIn = k - Ability.time * 3;
+            }
         }
     }
 }
