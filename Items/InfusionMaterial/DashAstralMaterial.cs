3136e0371096e57300337396d920f7981cb540c7
bobfishkins23 <bobfishkins23@yahoo.com>
2020-08-21 16:24:06 -0400

    tiny ceiros fix

---
+++ Items/InfusionMaterial/DashAstralMaterial.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace StarlightRiver.Items.InfusionMaterial
+{
+    class DashAstralMaterial : QuickMaterial
+    {
+        public DashAstralMaterial() : base("Comet essence", "Craft with a basic infusion to create an infusion!", 1, 0, ItemRarityID.Quest){}
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(item.type);
+            recipe.AddIngredient(ItemType<Abilities.Infusion.BasicInfusion>());
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(ItemType<Abilities.Infusion.DashAstral>());
+            recipe.AddRecipe();
+        }
+    }
+}
