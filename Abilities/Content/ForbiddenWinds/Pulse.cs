f87d5688df5bacd51ed402dfdf8dbbf1cc49df03
Dual <31146412+Dual-Iron@users.noreply.github.com>
2020-08-22 01:11:13 -0400

    Merge branch 'infusion-content' of https://github.com/TotallyNotAScalie/StarlightRiver into infusion-content

--- Abilities/Content/ForbiddenWinds/Pulse.cs
+++ Abilities/Content/ForbiddenWinds/Pulse.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria;
+using static Terraria.ModLoader.ModContent;
+using Microsoft.Xna.Framework;
 
 namespace StarlightRiver.Abilities.Content.ForbiddenWinds
 {
@@ -28,14 +31,24 @@ namespace StarlightRiver.Abilities.Content.ForbiddenWinds
             Ability.Time -= 4;
             Ability.CooldownBonus -= 15;
             Ability.ActivationCostBonus -= 0.15f;
-            Ability.Boost += 0.15f;
+            Ability.Boost += 0.1f;
             base.OnActivate();
         }
 
         public override void UpdateActiveEffects()
         {
-            // TODO visuals
-            base.UpdateActiveEffects();
+            Vector2 vel = Vector2.Normalize(Player.velocity) * -1;
+
+            if (Ability.Time > 0)
+                for (float k = 0; k < 6.28f; k += 0.02f)
+                {
+                    float factor = Ability.Time / 3f;
+                    Vector2 pos = Player.Center + (new Vector2((float)Math.Cos(k) * 20, (float)Math.Sin(k) * 40) * factor).RotatedBy(Player.velocity.ToRotation());
+
+                    Dust d = Dust.NewDustPerfect(pos, 264, vel * 10, 0, new Color(220, 20, 50), 0.5f);
+                    d.noGravity = true;
+                    d.noLight = true;
+                }
         }
 
         class Pulse2 : Pulse
@@ -51,7 +64,7 @@ namespace StarlightRiver.Abilities.Content.ForbiddenWinds
                 Ability.Time -= 1;
                 Ability.CooldownBonus -= 10;
                 Ability.ActivationCostBonus -= 0.1f;
-                Ability.Boost += 0.15f;
+                //Ability.Boost += 0.15f;
                 base.OnActivate();
             }
         }
@@ -69,7 +82,7 @@ namespace StarlightRiver.Abilities.Content.ForbiddenWinds
                 Ability.Time -= 1;
                 Ability.CooldownBonus -= 10;
                 Ability.ActivationCostBonus -= 0.25f;
-                Ability.Boost += 0.15f;
+                //Ability.Boost += 0.15f;
                 base.OnActivate();
             }
         }
