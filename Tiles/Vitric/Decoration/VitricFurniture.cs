d93e0e674d53e67222e269d63a55463092ec417d
bobfishkins23 <bobfishkins23@yahoo.com>
2020-08-23 02:12:43 -0400

    New wall sprite

--- Tiles/Vitric/Decoration/VitricFurniture.cs
+++ Tiles/Vitric/Decoration/VitricFurniture.cs
@@ -5,6 +5,6 @@ namespace StarlightRiver.Tiles.Vitric.Decoration
 {
     class VitricFurniture : AutoFurniture
     {
-        public VitricFurniture() : base("Vitric", "StarlightRiver/Tiles/Vitric/Decoration/", new Color(200, 150, 20), new Color(255, 220, 150), DustType<Dusts.Air>(), ItemType<Blocks.AncientSandstoneItem>()) { }
+        public VitricFurniture() : base("Vitric", "StarlightRiver/Tiles/Vitric/Decoration/", new Color(140, 97, 86), new Color(255, 220, 150), DustType<Dusts.Air>(), ItemType<Blocks.AncientSandstoneItem>()) { }
     }
 }
