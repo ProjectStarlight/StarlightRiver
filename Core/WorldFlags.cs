87a14d75a1a356ba60fe8c4bd7ca24ea539db82c
Dual <31146412+Dual-Iron@users.noreply.github.com>
2020-08-27 10:42:59 -0400

    somehow this didn't push

---
+++ Core/WorldFlags.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace StarlightRiver
+{
+    [Flags]
+    public enum WorldFlags
+    {
+        DesertOpen = 1 << 0,
+        SquidBossOpen = 1 << 1,
+        SquidBossDowned = 1 << 2,
+        GlassBossOpen = 1 << 3,
+        GlassBossDowned = 1 << 4,
+        OvergrowBossOpen = 1 << 5,
+        OvergrowBossFree = 1 << 6,
+        OvergrowBossDowned = 1 << 7,
+        SealOpen = 1 << 8,
+        AluminumMeteors = 1 << 9
+    }
+}
