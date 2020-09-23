14d628f75f4c6a40c44a555531722ce633ecb86b
bobfishkins23 <bobfishkins23@yahoo.com>
2020-09-22 23:20:47 -0400

    succ

--- Tiles/Permafrost/PermafrostIce.cs
+++ Tiles/Permafrost/PermafrostIce.cs
@@ -13,7 +13,7 @@ namespace StarlightRiver.Tiles.Permafrost
     {
         public override void SetDefaults()
         {
-            QuickBlock.QuickSet(this, 0, DustID.Ice, SoundID.Tink, new Color(255, 0, 0), ItemType<PermafrostIceItem>());
+            QuickBlock.QuickSet(this, 0, DustID.Ice, SoundID.Tink, new Color(134, 252, 237), ItemType<PermafrostIceItem>());
             Main.tileMerge[Type][TileID.SnowBlock] = true;
             Main.tileMerge[TileID.SnowBlock][Type] = true;
 
--- WorldGen/PermafrostGen.cs
+++ WorldGen/PermafrostGen.cs
@@ -59,7 +59,7 @@ namespace StarlightRiver
 
             Circle bigCircle = new Circle(new Point16(center + 15, iceBottom - 70), 110);
             circles.Add(bigCircle);
-            DigCircle(bigCircle, 30, 2400);
+            //DigCircle(bigCircle, 30, 2400);
 
             for (int k = 0; k < 150; k++)
             {
@@ -70,16 +70,172 @@ namespace StarlightRiver
 
                 if (!circles.Any(n => n.Colliding(circle)))
                 {
-                    DigCircle(circle);
                     circles.Add(circle);
                 }
             }
 
+            int row = WorldGen.genRand.Next(1000);
+
             var caves = GenerateLines(circles);
-            caves.ForEach(n => DigTunnel(n));
+            caves.ForEach(n => DigTunnel(n, row));
+
+            circles.ForEach(n => DigCircle(n));
+
+            caves.ForEach(n => DigTunnel(n, row, true));
 
             SquidBossArena = new Rectangle(center - 40, iceBottom - 150, 109, 180);
             StructureHelper.StructureHelper.GenerateStructure("Structures/SquidBossArena", new Point16(center - 40, iceBottom - 150), mod);
+
+            //uses some perlin noise to place snow/grass where there is air above ice.
+            for(int x = center - 400; x < center + 400; x++)
+                for(int y = iceBottom - 200; y < iceBottom + 100; y++)
+                {
+                    var noise = genNoise.GetPerlin(x, y);
+                    if (Math.Abs(noise) > 0.5f) continue;
+
+                    Tile floor = Framing.GetTileSafely(x, y);
+                    Tile above = Framing.GetTileSafely(x, y - 1);
+
+                    if (floor.type == TileType<PermafrostIce>() && !above.active())
+                        floor.type = (ushort)TileType<PermafrostSnow>();
+                }
+        }
+
+        private List<Vector4> GenerateLines(List<Circle> circles)
+        {
+            List<Vector4> output = new List<Vector4>();
+
+            for (int k = 0; k < circles.Count; k++)
+            {
+                for (int j = k + 1; j < circles.Count; j++)
+                {
+                    var line = new Vector4(circles[k].position.X, circles[k].position.Y, circles[j].position.X, circles[j].position.Y);
+                    var lineVector = new Vector2(line.Z - line.X, line.W - line.Y);
+
+                    var colliding = false;
+
+                    for (int n = 0; n < circles.Count; n++) //first, check against the circles
+                    {
+                        Circle check = circles[n].Inflate(11);
+                        if (n == k || n == j) continue;
+
+                        var off = check.position.ToVector2() - line.XY();
+
+                        var dot = Vector2.Dot(off, lineVector);
+                        var distAlong = dot / lineVector.LengthSquared();
+
+                        if (distAlong > 0 && distAlong < 1) //we've passed the check against the infinite line
+                        {
+                            float dist = Vector2.Distance(line.XY() + lineVector * distAlong, check.position.ToVector2());
+
+                            if(dist < check.radius)
+                            {
+                                colliding = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    for(int n = 0; n < output.Count; n++) //next, check against the lines we already have to prevent intersections
+                    {
+                        var enemyLine = output[n];
+
+                        if (line == enemyLine) continue;
+                        if (line.XY() == enemyLine.XY() || line.XY() == enemyLine.ZW()) continue; //if lines share a point it's fine, because the intersection will be at a sphere
+                        if (line.ZW() == enemyLine.XY() || line.ZW() == enemyLine.ZW()) continue;
+
+                        if (SegmentsColliding(line, enemyLine))
+                        {
+                            colliding = true;
+                            break;
+                        }
+                    }
+
+                    var caveLength = lineVector.Length() - circles[k].radius - circles[j].radius;
+                    if (!colliding && caveLength < 80) //final check to add a max length
+                        output.Add(line);
+                }
+            }
+
+            return output;
+        }
+
+        //Zoinked from https://www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+
+        private bool SegmentsColliding(Vector4 line1, Vector4 line2)
+        {
+            var p1 = line1.XY();
+            var q1 = line1.ZW();
+            var p2 = line2.XY();
+            var q2 = line2.ZW();
+
+            int o1 = orientation(p1, q1, p2);
+            int o2 = orientation(p1, q1, q2);
+            int o3 = orientation(p2, q2, p1);
+            int o4 = orientation(p2, q2, q1);
+
+            if (o1 != o2 && o3 != o4)
+                return true;
+
+            if (o1 == 0 && onSegment(p1, p2, q1)) return true;
+            if (o2 == 0 && onSegment(p1, q2, q1)) return true;
+            if (o3 == 0 && onSegment(p2, p1, q2)) return true;
+            if (o4 == 0 && onSegment(p2, q1, q2)) return true;
+
+            return false; 
+        }
+
+        static bool onSegment(Vector2 p, Vector2 q, Vector2 r)
+        {
+            if (q.X <= Math.Max(p.X, r.X) && q.X >= Math.Min(p.X, r.X) &&
+                q.Y <= Math.Max(p.Y, r.Y) && q.Y >= Math.Min(p.Y, r.Y))
+                return true;
+
+            return false;
+        }
+
+        static int orientation(Vector2 p, Vector2 q, Vector2 r)
+        {
+            int val = (int)((q.Y - p.Y) * (r.X - q.X) - (q.X - p.X) * (r.Y - q.Y));
+            if (val == 0) return 0;  
+
+            return (val > 0) ? 1 : 2; 
+        }
+
+        private void DigTunnel(Vector4 line, int row, bool digOnly = false)
+        {
+            var length = (line.XY() - line.ZW()).Length();
+
+            if (!digOnly)
+            {
+                for (float k = 0; k < 1; k += 1 / length)
+                {
+                    Point16 pos = Vector2.Lerp(line.XY(), line.ZW(), k).ToPoint16();
+                    int off = 7 + (int)Math.Abs(genNoise.GetPerlin(k * 1000, row) * 5);
+
+                    for (int x = pos.X - off; x < pos.X + off; x++)
+                    {
+                        for (int y = pos.Y - off; y < pos.Y + off; y++)
+                        {
+                            WorldGen.PlaceTile(x, y, TileType<PermafrostIce>(), true, true);
+                        }
+                    }
+                }
+            }
+
+            for (float k = 0; k < 1; k += 1 / length)
+            {
+                Point16 pos = Vector2.Lerp(line.XY(), line.ZW(), k).ToPoint16();
+                int off = 4 + (int)Math.Abs(genNoise.GetPerlin(k * 1000, row) * 5);
+
+                for (int x = pos.X - off; x < pos.X + off; x++)
+                {
+                    for (int y = pos.Y - off; y < pos.Y + off; y++)
+                    {
+                        WorldGen.KillTile(x, y);
+                    }
+                }
+            }
         }
 
         private void DigCircle(Circle circle, int maxOff = 0, int speed = 1800)
@@ -105,64 +261,6 @@ namespace StarlightRiver
                 }
         }
 
-        private List<Vector4> GenerateLines(List<Circle> circles)
-        {
-            List<Vector4> output = new List<Vector4>();
-
-            for (int k = 0; k < circles.Count; k++)
-            {
-                for (int j = k + 1; j < circles.Count; j++)
-                {
-                    var line = new Vector4(circles[k].position.X, circles[k].position.Y, circles[j].position.X, circles[j].position.Y);
-                    var lineVector = new Vector2(line.Z - line.X, line.W - line.Y);
-
-                    var colliding = false;
-
-                    for (int n = 0; n < circles.Count; n++)
-                    {
-                        Circle check = circles[n];
-                        if (n == k || n == j) continue;
-
-                        var off = check.position.ToVector2() - line.XY();
-
-                        var dot = Vector2.Dot(off, lineVector);
-                        var distAlong = dot / lineVector.LengthSquared();
-
-                        if (distAlong > 0 && distAlong < 1) //we've passed the check against the infinite line
-                        {
-                            float dist = Vector2.Distance(line.XY() + lineVector * distAlong, check.position.ToVector2());
-
-                            if(dist < check.radius)
-                            {
-                                colliding = true;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (!colliding)
-                        output.Add(line);
-                }
-            }
-
-            return output;
-        }
-
-        private void DigTunnel(Vector4 line, int type = TileID.EmeraldGemspark)
-        {
-            var length = (line.XY() - line.ZW()).Length();
-            for (float k = 0; k < 1; k += 1 / length)
-            {
-                Point16 pos = Vector2.Lerp(line.XY(), line.ZW(), k).ToPoint16();
-                for(int x = pos.X - 4; x < pos.X + 4; x++)
-                {
-                    for (int y = pos.Y - 4; y < pos.Y + 4; y++)
-                    {
-                        WorldGen.KillTile(x, y);
-                    }
-                }
-            }
-        }
     }
 
     public struct Circle
@@ -182,5 +280,7 @@ namespace StarlightRiver
                 return true;
             else return false;
         }
+
+        public Circle Inflate(int amount) => new Circle(position, radius + amount);
     }
 }
