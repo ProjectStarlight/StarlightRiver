ff0b61154209264670b6795af1f0402d437a82ce
bobfishkins23 <bobfishkins23@yahoo.com>
2020-10-23 18:34:11 -0400

    Update WorldFlags.cs

--- Core/WorldFlags.cs
+++ Core/WorldFlags.cs
@@ -6,14 +6,19 @@ namespace StarlightRiver
     public enum WorldFlags
     {
         DesertOpen = 1 << 0,
+
         SquidBossOpen = 1 << 1,
         SquidBossDowned = 1 << 2,
+
         GlassBossOpen = 1 << 3,
         GlassBossDowned = 1 << 4,
+
         OvergrowBossOpen = 1 << 5,
         OvergrowBossFree = 1 << 6,
         OvergrowBossDowned = 1 << 7,
+
         SealOpen = 1 << 8,
+
         AluminumMeteors = 1 << 9
     }
 }
