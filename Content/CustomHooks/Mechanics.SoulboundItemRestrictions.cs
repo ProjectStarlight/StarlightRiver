47d4f997bc697a7189db5c8cc00c652853a73451
bobfishkins23 <bobfishkins23@yahoo.com>
2020-11-22 21:42:56 -0500

    ILHOOKS.cs IS GONE

--- Content/CustomHooks/Mechanics.SoulboundItemRestrictions.cs
+++ Content/CustomHooks/Mechanics.SoulboundItemRestrictions.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using StarlightRiver.Core;
+using StarlightRiver.Items;
 using StarlightRiver.Physics;
 using System;
 using System.Collections.Generic;
@@ -37,19 +38,19 @@ namespace StarlightRiver.Content.CustomHooks
             IL.Terraria.UI.ChestUI.DepositAll -= PreventSoulboundStack;
         }
 
-        private bool NoSoulboundFrame(On.Terraria.Player.orig_ItemFitsItemFrame orig, Player self, Item i) => !(i.modItem is Items.SoulboundItem) && orig(self, i);
+        private bool NoSoulboundFrame(On.Terraria.Player.orig_ItemFitsItemFrame orig, Player self, Item i) => !(i.modItem is SoulboundItem) && orig(self, i);
 
-        private bool NoSoulboundRack(On.Terraria.Player.orig_ItemFitsWeaponRack orig, Player self, Item i) => !(i.modItem is Items.SoulboundItem) && orig(self, i);
+        private bool NoSoulboundRack(On.Terraria.Player.orig_ItemFitsWeaponRack orig, Player self, Item i) => !(i.modItem is SoulboundItem) && orig(self, i);
 
         private void SoulboundPriority(On.Terraria.Player.orig_dropItemCheck orig, Player self)
         {
-            if (Main.mouseItem.type > ItemID.None && !Main.playerInventory && Main.mouseItem.modItem != null && Main.mouseItem.modItem is Items.SoulboundItem)
+            if (Main.mouseItem.type > ItemID.None && !Main.playerInventory && Main.mouseItem.modItem != null && Main.mouseItem.modItem is SoulboundItem)
             {
                 for (int k = 49; k > 0; k--)
                 {
                     Item item = self.inventory[k];
 
-                    if (!(self.inventory[k].modItem is Items.SoulboundItem) || k == 0)
+                    if (!(self.inventory[k].modItem is SoulboundItem) || k == 0)
                     {
                         int index = Item.NewItem(self.position, item.type, item.stack, false, item.prefix, false, false);
                         Main.item[index] = item.Clone();
@@ -64,7 +65,7 @@ namespace StarlightRiver.Content.CustomHooks
 
         private void DontDropSoulbound(On.Terraria.Player.orig_DropSelectedItem orig, Player self)
         {
-            if (self.inventory[self.selectedItem].modItem is Items.SoulboundItem || Main.mouseItem.modItem is Items.SoulboundItem) return;
+            if (self.inventory[self.selectedItem].modItem is SoulboundItem || Main.mouseItem.modItem is SoulboundItem) return;
             else orig(self);
         }
 
@@ -88,7 +89,7 @@ namespace StarlightRiver.Content.CustomHooks
         private delegate bool SoulboundDelegate(int index);
         private bool EmitSoulboundDel(int index)
         {
-            return Main.LocalPlayer.inventory[index].modItem is Items.SoulboundItem;
+            return Main.LocalPlayer.inventory[index].modItem is SoulboundItem;
         }
     }
 }
--- ILHooks.cs
+++ ILHooks.cs
@@ -22,22 +22,6 @@ namespace StarlightRiver
     {
         private void HookIL()
         {
-            //Auroracle layer
-            IL.Terraria.Main.DoDraw += DrawWater;
-
-            //soulbound items
-            IL.Terraria.UI.ChestUI.DepositAll += PreventSoulboundStack;
-
-            //dynamic map icons
-            IL.Terraria.Main.DrawMap += DynamicBossIcon;
-
-            //jungle grass
-            IL.Terraria.WorldGen.Convert += JungleGrassConvert;
-            IL.Terraria.WorldGen.hardUpdateWorld += JungleGrassSpread;
-
-            //grappling hooks on moving platforms
-            IL.Terraria.Projectile.VanillaAI += GrapplePlatforms;
-
             //Town NPC name swaps
             IL.Terraria.WorldGen.SpawnTownNPC += SwapTitle;
             IL.Terraria.NPC.checkDead += SwapTitleDeath;
@@ -49,29 +33,7 @@ namespace StarlightRiver
 
         private void UnhookIL()
         {
-            // Vitric lighting
-            IL.Terraria.Lighting.PreRenderPhase -= VitricLighting;
 
-            //moonlord draw layer
-            IL.Terraria.Main.DoDraw -= DrawMoonlordLayer;
-
-            //Auroracle layer
-            IL.Terraria.Main.DoDraw -= DrawWater;
-
-            //soulbound items
-            IL.Terraria.UI.ChestUI.DepositAll -= PreventSoulboundStack;
-
-            //dynamic map icons
-            IL.Terraria.Main.DrawMap -= DynamicBossIcon;
-
-            //jungle grass
-            IL.Terraria.WorldGen.Convert -= JungleGrassConvert;
-            IL.Terraria.WorldGen.hardUpdateWorld -= JungleGrassSpread;
-
-            //grappling hooks on moving platforms
-            IL.Terraria.Projectile.VanillaAI -= GrapplePlatforms;
-
-            //Town NPC name swaps
             IL.Terraria.WorldGen.SpawnTownNPC -= SwapTitle;
             IL.Terraria.NPC.checkDead -= SwapTitleDeath;
             IL.Terraria.Main.DrawInventory -= SwapTitleMenu;
@@ -256,193 +218,9 @@ namespace StarlightRiver
         }
 
         //IL edit for the conversion and spread of specialized jungle grasses
-        private void JungleGrassSpread(ILContext il)
-        {
-            ILCursor c = new ILCursor(il);
-            for (int k = 0; k < 3; k++)
-            {
-                int type;
-                switch (k)
-                {
-                    case 0: type = ModContent.TileType<Tiles.JungleBloody.GrassJungleBloody>(); break;
-                    case 2: type = ModContent.TileType<Tiles.JungleCorrupt.GrassJungleCorrupt>(); break;
-                    case 1: type = ModContent.TileType<Tiles.JungleHoly.GrassJungleHoly>(); break;
-                    default: type = 2; break;
-                }
+        
 
-                for (int n = 0; n < 2; n++)
-                {
-                    c.TryGotoNext(i => i.MatchLdcI4(0), i => i.MatchStfld<Tile>("type"));
-                    c.Index--;
-                    c.Emit(OpCodes.Pop);
-                    c.Emit(OpCodes.Ldc_I4, type);
-                }
-            }
-        }
-
-        private void JungleGrassConvert(ILContext il) //Fun stuff.
-        {
-            ILCursor c = new ILCursor(il);
-            for (int k = 0; k < 3; k++)
-            {
-                int type;
-                int index;
-                switch (k)
-                {
-                    case 0: type = ModContent.TileType<Tiles.JungleBloody.GrassJungleBloody>(); break;
-                    case 2: type = ModContent.TileType<Tiles.JungleCorrupt.GrassJungleCorrupt>(); break;
-                    case 1: type = ModContent.TileType<Tiles.JungleHoly.GrassJungleHoly>(); break;
-                    default: type = 2; break;
-                }
-                c.TryGotoNext(i => i.MatchLdsfld(typeof(TileID.Sets.Conversion).GetField("Grass")));
-                c.TryGotoNext(i => i.MatchLdsfld(typeof(TileID.Sets.Conversion).GetField("Ice")));
-                index = c.Index--;
-                c.TryGotoPrev(i => i.MatchLdsfld(typeof(TileID.Sets.Conversion).GetField("Grass")));
-                c.Index++;
-                c.Emit(OpCodes.Pop);
-                c.Emit(OpCodes.Ldc_I4, type);
-                c.Emit(OpCodes.Ldloc_0);
-                c.Emit(OpCodes.Ldloc_1);
-                c.EmitDelegate<GrassConvertDelegate>(EmitGrassConvertDelegate);
-                c.Emit(OpCodes.Brtrue, il.Instrs[index]);
-                c.Emit(OpCodes.Ldsfld, typeof(TileID.Sets.Conversion).GetField("Grass"));
-            }
-            c.TryGotoNext(i => i.MatchLdfld<Tile>("wall"), i => i.MatchLdcI4(69)); //funny sex number!!!
-            c.TryGotoPrev(i => i.MatchLdsfld<Main>("tile"));
-            c.Index++;
-            c.Emit(OpCodes.Ldc_I4, TileID.JungleGrass);
-            c.Emit(OpCodes.Ldloc_0);
-            c.Emit(OpCodes.Ldloc_1);
-            c.EmitDelegate<GrassConvertDelegate>(EmitGrassConvertDelegate);
-            c.Emit(OpCodes.Pop);
-        }
-
-        //IL edit for auroracle arena
-
-
-        private delegate bool GrassConvertDelegate(int type, int x, int y);
-        private bool EmitGrassConvertDelegate(int type, int x, int y)
-        {
-            Tile tile = Main.tile[x, y];
-            if (tile.type == TileID.JungleGrass || tile.type == ModContent.TileType<Tiles.JungleBloody.GrassJungleBloody>() || tile.type == ModContent.TileType<Tiles.JungleCorrupt.GrassJungleCorrupt>() || tile.type == ModContent.TileType<Tiles.JungleHoly.GrassJungleHoly>())
-            {
-                tile.type = (ushort)type;
-                return true;
-            }
-            return false;
-        }
-
-        //IL edit for dynamic map icons
-        private void DynamicBossIcon(ILContext il)
-        {
-            //Hillariously repetitive IL edit to draw custom icons dynamically. Funny. Fuck vanilla.
-            ILCursor c = new ILCursor(il);
-            //the overlay map comes first
-            c.TryGotoNext(i => i.MatchCallvirt<NPC>("GetBossHeadTextureIndex")); //only gonna break down one of these, finds a point in vanilla where we can reasonably draw boss icons
-
-            c.EmitDelegate<DynamicIconDelegate>(EmitDynamicIconDelegateOverlay);
-
-            c.TryGotoNext(i => i.MatchCallvirt<NPC>("GetBossHeadTextureIndex"));
-            c.Index++;
-            c.TryGotoNext(i => i.MatchCallvirt<NPC>("GetBossHeadTextureIndex"));
-            c.Index++;
-            c.TryGotoNext(i => i.MatchCallvirt<NPC>("GetBossHeadTextureIndex"));
-
-            c.EmitDelegate<DynamicIconDelegate>(EmitDynamicIconDelegateMinimap);
-
-            c.TryGotoNext(i => i.MatchCallvirt<NPC>("GetBossHeadTextureIndex"));
-            c.Index++;
-            c.TryGotoNext(i => i.MatchCallvirt<NPC>("GetBossHeadTextureIndex"));
-            c.Index++;
-            c.TryGotoNext(i => i.MatchCallvirt<NPC>("GetBossHeadTextureIndex"));
-
-            c.EmitDelegate<DynamicIconDelegate>(EmitDynamicIconDelegateFullmap);
-        }
-
-        private delegate NPC DynamicIconDelegate(NPC npc);
-        private NPC EmitDynamicIconDelegateOverlay(NPC npc)
-        {
-            if (npc?.active == true && npc.modNPC is NPCs.IDynamicMapIcon)
-            {
-                Vector2 npcPos = npc.Center;
-                Vector2 framePos = (Main.screenPosition + new Vector2(Main.screenWidth / 2, Main.screenHeight / 2));
-                Vector2 target = new Vector2(Main.screenWidth / 2, Main.screenHeight / 2) + (npcPos - framePos) / 16 * Main.mapOverlayScale;
-
-                float scale = (Main.mapMinimapScale * 0.25f * 2f + 1f) / 3f;
-
-                (npc.modNPC as NPCs.IDynamicMapIcon).DrawOnMap(Main.spriteBatch, target, scale, Color.White * Main.mapOverlayAlpha);
-            }
-            return npc;
-        }
-        private NPC EmitDynamicIconDelegateMinimap(NPC npc)
-        {
-            if (npc?.active == true && npc.modNPC is NPCs.IDynamicMapIcon)
-            {
-                Vector2 mapPos = new Vector2(Main.miniMapX, Main.miniMapY);
-                Vector2 npcPos = npc.Center;
-                Vector2 framePos = (Main.screenPosition + new Vector2(Main.screenWidth / 2, Main.screenHeight / 2));
-                Vector2 target = mapPos + Vector2.One * 117 + (npcPos - framePos) / 16 * Main.mapMinimapScale;
-
-                if (target.X > Main.miniMapX + 15 && target.Y > Main.miniMapY + 15 && target.X < Main.miniMapX + 235 && target.Y < Main.miniMapY + 235) //only draw on the minimap
-                {
-                    float scale = (Main.mapMinimapScale * 0.25f * 2f + 1f) / 3f;
-
-                    (npc.modNPC as NPCs.IDynamicMapIcon).DrawOnMap(Main.spriteBatch, target, scale, Color.White);
-                    if (new Rectangle((int)target.X - (int)(15 * scale), (int)target.Y - (int)(15 * scale), (int)(30 * scale), (int)(30 * scale)).Contains(Main.MouseScreen.ToPoint()))
-                    {
-                        Utils.DrawBorderString(Main.spriteBatch, npc.GivenOrTypeName, Main.MouseScreen + Vector2.One * 15, Main.mouseTextColorReal);
-                    }
-                }
-            }
-            return npc;
-        }
-        private NPC EmitDynamicIconDelegateFullmap(NPC npc)
-        {
-            if (npc?.active == true && npc.modNPC is NPCs.IDynamicMapIcon)
-            {
-                float mapScale = Main.mapFullscreenScale / Main.UIScale;
-
-                float mapFullscrX = Main.mapFullscreenPos.X * mapScale;
-                float mapFullscrY = Main.mapFullscreenPos.Y * mapScale;
-                float mapX = -mapFullscrX + (Main.screenWidth / 2f);
-                float mapY = -mapFullscrY + (Main.screenHeight / 2f);
-
-                Vector2 mapPos = new Vector2(mapX, mapY);
-                Vector2 npcPos = npc.Center;
-                Vector2 target = mapPos + npcPos / 16 * Main.mapFullscreenScale;
-
-                float scale = Main.UIScale;
-
-                (npc.modNPC as NPCs.IDynamicMapIcon).DrawOnMap(Main.spriteBatch, target, scale, Color.White);
-                if (new Rectangle((int)target.X - (int)(15 * scale), (int)target.Y - (int)(15 * scale), (int)(30 * scale), (int)(30 * scale)).Contains(Main.MouseScreen.ToPoint()))
-                {
-                    Utils.DrawBorderString(Main.spriteBatch, npc.GivenOrTypeName, Main.MouseScreen + Vector2.One * 15, Main.mouseTextColorReal);
-                }
-            }
-            return npc;
-        }
-
-        //IL edit to prevent quickstacking of soulbound items
-        private void PreventSoulboundStack(ILContext il)
-        {
-            ILCursor c = new ILCursor(il);
-
-            c.TryGotoNext(i => i.MatchLdloc(1), i => i.MatchLdcI4(1), i => i.MatchSub());
-            Instruction target = c.Prev.Previous;
-
-            c.TryGotoPrev(n => n.MatchLdfld<Item>("favorited"));
-            c.Index++;
-
-            c.Emit(OpCodes.Ldloc_0);
-            c.EmitDelegate<SoulboundDelegate>(EmitSoulboundDel);
-            c.Emit(OpCodes.Brtrue_S, target);
-        }
-
-        private delegate bool SoulboundDelegate(int index);
-        private bool EmitSoulboundDel(int index)
-        {
-            return Main.LocalPlayer.inventory[index].modItem is Items.SoulboundItem;
-        }
+       
         #endregion
     }
 }
