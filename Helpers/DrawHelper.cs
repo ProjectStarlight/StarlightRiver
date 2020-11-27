c9f1915bd4793451b792e28af548e8f7aebc17a1
GraydeeSi <wiseguy0002@gmail.com>
2020-11-27 11:29:45 -0500

    Degodifying helpers part 1

--- Helpers/DrawHelper.cs
+++ Helpers/DrawHelper.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Linq;
 using Terraria;
 using Terraria.Graphics.Effects;
 using StarlightRiver.Core;
@@ -254,7 +256,51 @@ namespace StarlightRiver
 
             gd.SetVertexBuffer(null);
         }
+        public static void DrawLine(SpriteBatch spritebatch, Vector2 startPoint, Vector2 endPoint, Texture2D texture, Color color, Rectangle sourceRect = default)
+        {
+            Vector2 edge = endPoint - startPoint;
+            // calculate angle to rotate line
+            float angle =
+                (float)Math.Atan2(edge.Y, edge.X);
 
+            Vector2 offsetStart = startPoint + new Vector2(0, -(sourceRect.Width / 2)).RotatedBy(angle);//multiply before adding to startpoint to make the points closer
+
+            spritebatch.Draw(texture,
+                new Rectangle(// rectangle defines shape of line and position of start of line
+                    (int)offsetStart.X,
+                    (int)offsetStart.Y,
+                    (int)edge.Length(), //sb will stretch the texture to fill this rectangle
+                    sourceRect.Width), //width of line, change this to make thicker line (may have to offset?)
+                sourceRect,
+                color, //colour of line
+                angle, //angle of line (calulated above)
+                new Vector2(0, 0), // point in line about which to rotate
+                SpriteEffects.None,
+                default);
+
+
+        }
+        public static void DrawElectricity(Vector2 point1, Vector2 point2, int dusttype, float scale = 1, int armLength = 30, Color color = default)
+        {
+            int nodeCount = (int)Vector2.Distance(point1, point2) / armLength;
+            Vector2[] nodes = new Vector2[nodeCount + 1];
+
+            nodes[nodeCount] = point2; //adds the end as the last point
+
+            for (int k = 1; k < nodes.Count(); k++)
+            {
+                //Sets all intermediate nodes to their appropriate randomized dot product positions
+                nodes[k] = Vector2.Lerp(point1, point2, k / (float)nodeCount) +
+                    (k == nodes.Count() - 1 ? Vector2.Zero : Vector2.Normalize(point1 - point2).RotatedBy(1.58f) * Main.rand.NextFloat(-armLength / 2, armLength / 2));
+
+                //Spawns the dust between each node
+                Vector2 prevPos = k == 1 ? point1 : nodes[k - 1];
+                for (float i = 0; i < 1; i += 0.05f)
+                {
+                    Dust.NewDustPerfect(Vector2.Lerp(prevPos, nodes[k], i), dusttype, Vector2.Zero, 0, color, scale);
+                }
+            }
+        }
         public static float ConvertX(float input) => input / (Main.screenWidth / 2) - 1;
 
         public static float ConvertY(float input) => -1 * (input / (Main.screenHeight / 2) - 1);
---
+++ Helpers/Helper.cs
@@ -0,0 +1,207 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Graphics;
+using StarlightRiver.Codex;
+using StarlightRiver.Core;
+using StarlightRiver.Core.Loaders;
+using StarlightRiver.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.Graphics;
+using Terraria.ID;
+using Terraria.ObjectData;
+using Terraria.UI;
+using static Terraria.ModLoader.ModContent;
+
+
+namespace StarlightRiver
+{
+    public static partial class Helper
+    {
+        private static int tiltTime;
+        private static float tiltMax;
+
+        public static Rectangle ToRectangle(this Vector2 vector) => new Rectangle(0, 0, (int)vector.X, (int)vector.Y);
+        public static Player Owner(this Projectile proj) => Main.player[proj.owner];
+        public static Vector2 TileAdj => Lighting.lightMode > 1 ? Vector2.Zero : Vector2.One * 12;
+        public static Vector2 ScreenSize => new Vector2(Main.screenWidth, Main.screenHeight);
+
+        /// <summary>
+        /// Updates the value used for flipping rotation on the player. Should be reset to 0 when not in use.
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="value"></param>
+        public static void UpdateRotation(this Player player, float value) => player.GetModPlayer<StarlightPlayer>().rotation = value;
+
+        public static bool OnScreen(Vector2 pos) => (pos.X > -16 && pos.X < Main.screenWidth + 16 && pos.Y > -16 && pos.Y < Main.screenHeight + 16);
+
+        public static bool OnScreen(Rectangle rect) => rect.Intersects(new Rectangle(0, 0, Main.screenWidth, Main.screenHeight));
+
+        public static bool OnScreen(Vector2 pos, Vector2 size) => OnScreen(new Rectangle((int)pos.X, (int)pos.Y, (int)size.X, (int)size.Y));
+
+        public static Vector3 Vec3(this Vector2 vector) => new Vector3(vector.X, vector.Y, 0);
+
+        public static Vector3 ScreenCoord(this Vector3 vector) => new Vector3(-1 + vector.X / Main.screenWidth * 2, (-1 + vector.Y / Main.screenHeight * 2f) * -1, 0);
+
+        public static void UnlockEntry<type>(Player player)
+        {
+            CodexHandler mp = player.GetModPlayer<CodexHandler>();
+            CodexEntry entry = mp.Entries.FirstOrDefault(n => n is type);
+
+            if (entry.RequiresUpgradedBook && mp.CodexState != 2) return; //dont give the player void entries if they dont have the void book
+            entry.Locked = false;
+            entry.New = true;
+            if (mp.CodexState != 0) UILoader.GetUIState<CodexPopup>().TripEntry(entry.Title);
+            Main.PlaySound(SoundID.Item30);
+        }
+
+        public static void SpawnGem(int ID, Vector2 position)
+        {
+            int item = Item.NewItem(position, ItemType<Items.StarlightGem>());
+            (Main.item[item].modItem as Items.StarlightGem).gemID = ID;
+        }
+
+        public static void DrawSymbol(SpriteBatch spriteBatch, Vector2 position, Color color)
+        {
+            Texture2D tex = GetTexture("StarlightRiver/Assets/Symbol");
+            spriteBatch.Draw(tex, position, tex.Frame(), color * 0.8f, 0, tex.Size() / 2, 1, 0, 0);
+
+            Texture2D tex2 = GetTexture("StarlightRiver/Assets/Tiles/Interactive/WispSwitchGlow2");
+
+            float fade = StarlightWorld.rottime / 6.28f;
+            spriteBatch.Draw(tex2, position, tex2.Frame(), color * (1 - fade), 0, tex2.Size() / 2f, fade * 1.1f, 0, 0);
+        }
+
+        public static bool CheckCircularCollision(Vector2 center, int radius, Rectangle hitbox)
+        {
+            if (Vector2.Distance(center, hitbox.TopLeft()) <= radius) return true;
+            if (Vector2.Distance(center, hitbox.TopRight()) <= radius) return true;
+            if (Vector2.Distance(center, hitbox.BottomLeft()) <= radius) return true;
+            return Vector2.Distance(center, hitbox.BottomRight()) <= radius;
+        }
+
+        public static bool CheckConicalCollision(Vector2 center, int radius, float angle, float width, Rectangle hitbox)
+        {
+            if (CheckPoint(center, radius, hitbox.TopLeft(), angle, width)) return true;
+            if (CheckPoint(center, radius, hitbox.TopRight(), angle, width)) return true;
+            if (CheckPoint(center, radius, hitbox.BottomLeft(), angle, width)) return true;
+            return CheckPoint(center, radius, hitbox.BottomRight(), angle, width);
+        }
+
+        private static bool CheckPoint(Vector2 center, int radius, Vector2 check, float angle, float width)
+        {
+            float thisAngle = (center - check).ToRotation() % 6.28f;
+            return Vector2.Distance(center, check) <= radius && thisAngle > angle - width && thisAngle < angle + width;
+        }
+
+        public static string TicksToTime(int ticks)
+        {
+            int sec = ticks / 60;
+            return (sec / 60) + ":" + (sec % 60 < 10 ? "0" + sec % 60 : "" + sec % 60);
+        }
+
+        public static void DoTilt(float intensity)
+        {
+            tiltMax = intensity; tiltTime = 0;
+        }
+
+        public static void UpdateTilt()
+        {
+            if (Math.Abs(tiltMax) > 0)
+            {
+                tiltTime++;
+                if (tiltTime >= 1 && tiltTime < 40)
+                {
+                    float tilt = tiltMax - tiltTime * tiltMax / 40f;
+                    StarlightRiver.Rotation = tilt * (float)Math.Sin(Math.Pow(tiltTime / 40f * 6.28f, 0.9f));
+                }
+
+                if (tiltTime >= 40) { StarlightRiver.Rotation = 0; tiltMax = 0; }
+            }
+        }
+        public static bool ScanForTypeDown(int startX, int startY, int type, int maxDown = 50)
+        {
+            for (int k = 0; k <= maxDown && k + startY < Main.maxTilesY; k++)
+            {
+                Tile tile = Framing.GetTileSafely(startX, startY + k);
+                if (tile.active() && tile?.type == type)
+                    return true;
+            }
+            return false;
+        }
+
+        public static int SamplePerlin2D(int x, int y, int min, int max)
+        {
+            Texture2D perlin = TextureManager.Load("Images/Misc/Perlin");
+
+            Color[] rawData = new Color[perlin.Width]; //array of colors
+            Rectangle row = new Rectangle(0, y, perlin.Width, 1); //one row of the image
+            perlin.GetData<Color>(0, row, rawData, 0, perlin.Width); //put the color data from the image into the array
+            return (int)(min + rawData[x % 512].R / 255f * max);
+        }
+
+        public static float CompareAngle(float baseAngle, float targetAngle)
+        {
+            return (baseAngle - targetAngle + (float)Math.PI * 3) % MathHelper.TwoPi - (float)Math.PI;
+        }
+
+        public static string WrapString(string input, int length, DynamicSpriteFont font, float scale)
+        {
+            string output = "";
+            string[] words = input.Split();
+
+            string line = "";
+            foreach (string str in words)
+            {
+                if (str == "NEWBLOCK")
+                {
+                    output += ("\n\n");
+                    line = ("");
+                    continue;
+                }
+
+                if (font.MeasureString(line).X * scale < length)
+                {
+                    output += (" " + str);
+                    line += (" " + str);
+                }
+                else
+                {
+                    output += ("\n" + str);
+                    line = (str);
+                }
+            }
+            return output;
+        }
+
+        public static List<T> RandomizeList<T>(List<T> input)
+        {
+            int n = input.Count();
+            while (n > 1)
+            {
+                n--;
+                int k = Main.rand.Next(n + 1);
+                T value = input[k];
+                input[k] = input[n];
+                input[n] = value;
+            }
+            return input;
+        }
+
+        public static Player FindNearestPlayer(Vector2 position)
+        {
+            Player player = null;
+
+            for (int k = 0; k < Main.maxPlayers; k++)
+            {
+                if (Main.player[k] != null && Main.player[k].active && (player == null || Vector2.DistanceSquared(position, Main.player[k].Center) < Vector2.DistanceSquared(position, player.Center)))
+                    player = Main.player[k];
+            }
+            return player;
+        }
+    }
+}
+
