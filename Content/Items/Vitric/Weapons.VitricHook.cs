34e1ebcd3a8cba90e07fe5fc63406ac15a4f8447
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2021-02-14 14:25:27 -0600

    vitric hook improvements

--- Content/Items/Vitric/Weapons.VitricHook.cs
+++ Content/Items/Vitric/Weapons.VitricHook.cs
@@ -31,16 +31,25 @@ namespace StarlightRiver.Content.Items.Vitric
             item.useTime = 20;
             item.useAnimation = 20;
             item.shoot = ProjectileType<VitricHookProjectile>();
-            item.shootSpeed = 15;
-            item.useStyle = 1;
+            item.shootSpeed = 20;
+            item.useStyle = ItemUseStyleID.SwingThrow;
             item.noUseGraphic = true;
+            item.knockBack = 3;
         }
 
-        public override void HoldItem(Player player)
+        public override bool CanUseItem(Player player)
         {
-            if (Main.projectile.Any(n => n.type == ProjectileType<VitricHookProjectile>() && n.owner == player.whoAmI)) { }
-                //player.itemAnimation = 5;
+            if (player.velocity.Y == 0 || Main.projectile.Any(n => n.active && n.type == ProjectileType<VitricHookProjectile>() && n.owner == player.whoAmI))
+                return false;
+
+            return base.CanUseItem(player);
         }
+
+        //public override void HoldItem(Player player)
+        //{
+        //    if (Main.projectile.Any(n => n.type == ProjectileType<VitricHookProjectile>() && n.owner == player.whoAmI))
+        //        player.itemAnimation = 5;
+        //}
     }
 
     class VitricHookProjectile : ModProjectile 
@@ -65,11 +74,16 @@ namespace StarlightRiver.Content.Items.Vitric
             projectile.penetrate = 2;
         }
 
+
         public override void AI()
         {
-            var player = Main.player[projectile.owner];
+            Player player = Main.player[projectile.owner];
 
             projectile.rotation = projectile.velocity.ToRotation();
+            if(projectile.timeLeft < 40)//slows down the projectile by 8%, for about 10 ticks before it retracts
+            {
+                projectile.velocity *= 0.92f;
+            }
 
             if(projectile.timeLeft == 30)
             {
@@ -86,6 +100,7 @@ namespace StarlightRiver.Content.Items.Vitric
             {
                 projectile.timeLeft = 32;
                 projectile.Center = hooked.Center;
+                player.velocity = Vector2.Zero;//resets wings / double jumps
 
                 Progress += 20f / Distance;
                 player.Center = Vector2.Lerp(startPos, hooked.Center, Progress);
@@ -100,18 +115,17 @@ namespace StarlightRiver.Content.Items.Vitric
                     player.velocity = Vector2.Normalize(startPos - hooked.Center) * 15;
                     player.GetModPlayer<StarlightPlayer>().Shake += 15;
 
-                    hooked.StrikeNPC(30, 2, player.Center.X < hooked.Center.X ? -1 : 1);
+                    hooked.StrikeNPC(projectile.damage, projectile.knockBack, player.Center.X < hooked.Center.X ? -1 : 1);
 
                     for(int k = 0; k < 30; k++)
-                        Dust.NewDustPerfect(player.Center, DustType<Dusts.GlassNoGravity>(), Vector2.One.RotatedByRandom(6.28f) * Main.rand.NextFloat(5), 0, default, Main.rand.NextFloat(0.5f, 4));
-
+                        Dust.NewDustPerfect(player.Center, DustType<Dusts.GlassNoGravity>(), Vector2.One.RotatedByRandom(6.28f) * Main.rand.NextFloat(4f), 0, default, Main.rand.NextFloat(1f, 2f));
                     Main.PlaySound(SoundID.Shatter);
                 }
             }
 
             if(struck)
             {
-                player.fullRotation = projectile.timeLeft / 20f * 6.28f * player.direction;
+                player.fullRotation = (projectile.timeLeft / 20f) * 3.14f * player.direction;
                 player.fullRotationOrigin = player.Size / 2;
                 player.velocity *= 0.95f;
             }
@@ -119,7 +133,7 @@ namespace StarlightRiver.Content.Items.Vitric
 
         public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
         {
-            var player = Main.player[projectile.owner];
+            Player player = Main.player[projectile.owner];
 
             if (target.life <= 0) return;
 
@@ -133,23 +147,42 @@ namespace StarlightRiver.Content.Items.Vitric
             }
         }
 
+        public override void ModifyHitNPC(NPC target, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            damage /= 4;
+            knockback /= 4f;
+            crit = false;
+            base.ModifyHitNPC(target, ref damage, ref knockback, ref crit, ref hitDirection);
+        }
+
         public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
         {
-            if (struck) return false;
+            if(struck) 
+                return false;
 
-            var tex = GetTexture(AssetDirectory.VitricItem + "VitricHookChain");
-            var player = Main.player[projectile.owner];
-            var dist = Vector2.Distance(player.Center, projectile.Center);
+            Texture2D chainTex1 = GetTexture(AssetDirectory.VitricItem + "VitricHookChain1");
+            Texture2D chainTex2 = GetTexture(AssetDirectory.VitricItem + "VitricHookChain2");
+            Player player = Main.player[projectile.owner];
 
-            for (float k = 0; k < 1; k += 1f / dist * tex.Height)
+            float dist = Vector2.Distance(player.Center, projectile.Center);
+            float rot = (player.Center - projectile.Center).ToRotation() + (float)Math.PI / 2f;
+
+
+            float length = 1f / dist * chainTex1.Height;
+            for (int k = 0; k * length < 1; k++)
             {
-                var pos = Vector2.Lerp(player.Center, projectile.Center, k);
-                var rot = (player.Center - projectile.Center).ToRotation() + (float)Math.PI / 2f;
-
-                spriteBatch.Draw(tex, pos - Main.screenPosition, null, lightColor, rot, tex.Size() / 2, 1, 0, 0);
+                var pos = Vector2.Lerp(projectile.Center, player.Center, k * length);
+                if(k % 2 == 0)
+                    spriteBatch.Draw(chainTex1, pos - Main.screenPosition, null, lightColor, rot, chainTex1.Size() / 2, 1, 0, 0);
+                else
+                    spriteBatch.Draw(chainTex2, pos - Main.screenPosition, null, lightColor, rot, chainTex1.Size() / 2, 1, 0, 0);
             }
 
-            return true;
+            Texture2D hook = Main.projectileTexture[projectile.type];
+
+            spriteBatch.Draw(hook, projectile.Center - Main.screenPosition, null, lightColor, rot + ((float)Math.PI * 0.75f), hook.Size() / 2, 1, 0, 0);
+
+            return false;
         }
     }
 }
