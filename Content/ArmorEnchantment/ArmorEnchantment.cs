86055ecdfce82133e9e6293dd7d99d0a3326f4cf
bobfishkins23 <bobfishkins23@yahoo.com>
2021-02-16 19:59:46 -0500

    Armor bags and enchantment prototype

---
+++ Content/ArmorEnchantment/ArmorEnchantment.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader.IO;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace StarlightRiver.Content.ArmorEnchantment
+{
+    abstract class ArmorEnchantment
+    {
+        public virtual Color Color => Color.White;
+
+        public Guid Guid;
+
+        public ArmorEnchantment(Guid guid)
+        {
+            Guid = guid;
+        }
+
+        public virtual void UpdateSet(Player player)
+        {
+
+        }
+
+        public virtual void DrawInInventory(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+
+        }
+
+        public virtual bool PreDrawInInventory(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            return true;
+        }
+
+        public virtual TagCompound Save()
+        {
+            return new TagCompound()
+            {
+
+            };
+        }
+
+        public virtual void Load(TagCompound tag)
+        {
+
+        }
+
+        public sealed override bool Equals(object obj)
+        {
+            if(obj is ArmorEnchantment)
+            {
+                var a = obj as ArmorEnchantment;
+
+                if (a.Guid == Guid) 
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static void EnchantArmor(Item head, Item chest, Item legs, ArmorEnchantment enchant)
+        {
+            EnchantArmor(head, enchant);
+            EnchantArmor(chest, enchant);
+            EnchantArmor(legs, enchant);
+        }
+
+        public static void EnchantArmor(Item item, ArmorEnchantment enchant)
+        {
+            item.GetGlobalItem<EnchantedArmorGlobalItem>().Enchantment = enchant;
+        }
+    }
+}
---
+++ Content/ArmorEnchantment/EnchantedArmorGlobalItem.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Reflection;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace StarlightRiver.Content.ArmorEnchantment
+{
+    class EnchantedArmorGlobalItem : GlobalItem
+    {
+        public ArmorEnchantment Enchantment { get; set; }
+
+        public override bool InstancePerEntity => true;
+
+        public override bool CloneNewInstances => true;
+
+        public override bool NeedsSaving(Item item) => Enchantment != null;
+
+        public override void PostDrawInInventory(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            Enchantment?.DrawInInventory(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+        }
+
+        public override bool PreDrawInInventory(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            if (Enchantment is null) return base.PreDrawInInventory(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+            return Enchantment.PreDrawInInventory(item, spriteBatch, position, frame, drawColor, itemColor, origin, scale);
+        }
+
+        public override string IsArmorSet(Item head, Item body, Item legs)
+        {
+            if (GetEnchant(head) != null && //so unenchanted items are unaffected
+                GetEnchant(body) != null &&
+                GetEnchant(legs) != null &&
+                GetEnchant(head).Equals(GetEnchant(body)) && 
+                GetEnchant(head).Equals(GetEnchant(legs)))
+
+                return "CurrentEnchant";
+
+            return "";
+        }
+
+        public override void UpdateArmorSet(Player player, string set)
+        {
+            var Enchantment = player.armor[0].GetGlobalItem<EnchantedArmorGlobalItem>().Enchantment;
+
+            if(set == "CurrentEnchant" && Enchantment != null)
+            {
+                Enchantment.UpdateSet(player);
+            }
+        }
+
+        public override TagCompound Save(Item item)
+        {
+            return new TagCompound()
+            {
+                ["EnchantGuid"] = Enchantment.Guid.ToString(),
+                ["EnchantType"] = Enchantment.GetType().FullName,
+                ["Enchantment"] = Enchantment.Save()
+            };
+        }
+
+        public override void Load(Item item, TagCompound tag)
+        {
+            var guid = Guid.ParseExact(tag.GetString("EnchantGuid"), "D");
+            var type = tag.GetString("EnchantType");
+
+            if (guid != null && type != null)
+            {
+                var a = Assembly.GetAssembly(this.GetType()).GetType(type);
+
+                Enchantment = (ArmorEnchantment)Activator.CreateInstance
+                    (
+                        a,
+                        new object[] { guid }
+                    );
+
+                Enchantment.Load(tag.GetCompound("Enchantment"));
+            }
+        }
+
+        public static ArmorEnchantment GetEnchant(Item item)
+        {
+            if (item.IsAir) 
+                return null;
+
+
+            var globalItem = item.GetGlobalItem<EnchantedArmorGlobalItem>();
+            
+            if (globalItem is null)
+                return null;
+
+            return globalItem.Enchantment;
+        }
+    }
+}
