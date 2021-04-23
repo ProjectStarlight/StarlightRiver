8565f92ae7e78185715a55c1125e0053bedd2dc7
bobfishkins23 <bobfishkins23@yahoo.com>
2021-04-22 22:40:16 -0400

    Gaming

--- Content/NPCs/Corruption/Dweller.cs
+++ Content/NPCs/Corruption/Dweller.cs
@@ -41,11 +41,17 @@ namespace StarlightRiver.Content.NPCs.Corruption
 
 		public override void SetDefaults()
 		{
-			npc.width = 54;
-			npc.height = 54;
+			Variant = Main.rand.Next(3);
+
+			if (Variant == 0) npc.width = 66;
+			if (Variant == 1) npc.width = 58;
+			if (Variant == 2) npc.width = 64;
+
+			npc.height = npc.width;
+
 			npc.lifeMax = 120;
-			npc.defense = 5;
-			npc.knockBackResist = 1.2f;
+			npc.defense = 6;
+			npc.knockBackResist = 0;
 			npc.aiStyle = -1;
 			npc.noGravity = true;
 			npc.behindTiles = true;
@@ -55,6 +61,8 @@ namespace StarlightRiver.Content.NPCs.Corruption
 		{
 			if(root is null) //scan to find a valid root tile under where it spawned
 			{
+				npc.Center = npc.Center - new Vector2(npc.Center.X % 16, npc.Center.Y % 16);
+
 				for(int k = 0; root is null; k++)
 				{
 					var tile = Framing.GetTileSafely((int)npc.Center.X / 16, (int)npc.Center.Y / 16 + k);
@@ -69,6 +77,8 @@ namespace StarlightRiver.Content.NPCs.Corruption
 					{
 						root = tile;
 						Height = k * 16;
+
+						Main.NewText(tile.type);
 					}
 				}
 			}
@@ -101,7 +111,7 @@ namespace StarlightRiver.Content.NPCs.Corruption
 
 					var yOff = Target.Center.Y - npc.Center.Y;
 
-					if (Timer > 60 && Math.Abs(Target.Center.X - npc.Center.X) < 32 && yOff < (Height + 16) && yOff > 0) //under the tree
+					if (Timer > 60 && Math.Abs(Target.Center.X - npc.Center.X) < 32 && yOff < (Height - 16) && yOff > 0) //under the tree
 					{
 						State = (int)States.Transforming;
 						Timer = 0;
@@ -111,16 +121,31 @@ namespace StarlightRiver.Content.NPCs.Corruption
 
 				case (int)States.Transforming:
 
-					if(Timer == 80)
+					if(Timer == 100)
 					{
 						Vignette.visible = false;
 						npc.noGravity = false;
+
+						Main.PlaySound(SoundID.Grass, npc.Center);
 					}
 
-					if(Timer >= 80)
+					if(Timer > 60)
+					{
+						for (int k = 0; k < 5; k++)
+						{
+							Dust.NewDustPerfect(npc.Center + Vector2.One.RotatedByRandom(6.28f) * Main.rand.NextFloat(npc.width / 2),
+								DustType<Dusts.GreyLeaf>(), Vector2.One.RotatedByRandom(6.28f) * Main.rand.NextFloat(2), 0, new Color(210, 200, 255), Main.rand.NextFloat(1.0f, 1.3f));
+						}
+					}
+
+					if(Timer >= 100)
 					{
 						if (npc.velocity.Y == 0)
+						{
 							State = (int)States.Attacking;
+							npc.knockBackResist = 1.2f;
+							npc.damage = 20;
+						}
 					}
 
 					break;
@@ -147,6 +172,7 @@ namespace StarlightRiver.Content.NPCs.Corruption
 
 			switch(State)
 			{
+				case (int)States.Transforming: //fall-through moment
 				case (int)States.Idle:
 
 					var barkTex = GetTexture("Terraria/Tiles_5_0"); //corruption tree bark 
@@ -155,12 +181,21 @@ namespace StarlightRiver.Content.NPCs.Corruption
 					{
 						Vector2 pos = npc.Center + Vector2.UnitY * k;
 						pos -= new Vector2(pos.X % 16, pos.Y % 16);
+
+						if (State == (int)States.Transforming && Timer > 60)
+						{
+							pos.Y += (Timer - 60) / 30f * Height;
+
+							if (pos.Y > npc.Center.Y + Height)
+								continue;
+						}
+
 						Rectangle source = new Rectangle(rand.Next(2) * 22, rand.Next(6) * 22, 20, 16);
 						Color color = Lighting.GetColor((int)pos.X / 16, (int)pos.Y / 16);
 
 						spriteBatch.Draw(barkTex, pos - Main.screenPosition, source, color);
 
-						if(rand.Next(6) == 0)
+						if(rand.Next(6) == 0 && k > 48 && k < Height - 48)
 						{
 							var branchTex = GetTexture(Texture + "Branches");
 
@@ -184,19 +219,18 @@ namespace StarlightRiver.Content.NPCs.Corruption
 					Vector2 topperPos = npc.Center - new Vector2(npc.Center.X % 16, npc.Center.Y % 16) + new Vector2(11, 8);
 					Rectangle topperSource = new Rectangle((int)Variant * 82, 0, 82, 82);
 
+					if (State == (int)States.Transforming)
+						topperSource.Y = (int)(Timer < 30 ? Timer / 30 * 5 : Timer % 10 < 5 ? 3 : 4) * 84;
+
 					spriteBatch.Draw(topperTex, topperPos - Main.screenPosition, topperSource, drawColor, 0, new Vector2(41, 41), 1, 0, 0);
-
-					break;
-
-				case (int)States.Transforming: //TODO: Add animation
-
-					spriteBatch.Draw(GetTexture(Texture), npc.Center - Main.screenPosition, npc.frame, drawColor, npc.rotation, npc.Size / 2, npc.scale, 0, 0);
 
 					break;
 
 				case (int)States.Attacking:
 
-					spriteBatch.Draw(GetTexture(Texture), npc.Center - Main.screenPosition, npc.frame, drawColor, npc.rotation, npc.Size / 2, npc.scale, 0, 0);
+					npc.frame = new Rectangle(0, (int)Timer / 7 % 3 * npc.height, npc.width, npc.height);
+
+					spriteBatch.Draw(GetTexture(Texture + Variant), npc.Center - Main.screenPosition, npc.frame, drawColor, npc.rotation, npc.Size / 2, npc.scale, 0, 0);
 
 					break;
 			}
