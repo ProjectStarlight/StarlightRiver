d833b46ecf708f1be753fe1087b4c5f54d7b757d
bobfishkins23 <bobfishkins23@yahoo.com>
2021-04-29 20:33:00 -0400

    I shit myself

--- Content/Tiles/Vitric/BasicVitricTileLoader.cs
+++ Content/Tiles/Vitric/BasicVitricTileLoader.cs
@@ -12,99 +12,117 @@ namespace StarlightRiver.Content.Tiles.Vitric
     {
         public override string AssetRoot => AssetDirectory.VitricTile;
 
-        public override void Load()
-        {
-            LoadTile(
-                "AncientSandstone",
-                "Ancient Sandstone",
-                new TileLoadData(
-                    minPick: 200,
-                    dustType: DustID.Copper,
-                    soundType: SoundID.Tink,
-                    mapColor: new Color(160, 115, 75)
-                    )
-                );
+		public override void Load()
+		{
+			LoadTile(
+				"AncientSandstone",
+				"Ancient Sandstone",
+				new TileLoadData(
+					minPick: 200,
+					dustType: DustID.Copper,
+					soundType: SoundID.Tink,
+					mapColor: new Color(160, 115, 75)
+					)
+				);
 
-            LoadTile(
-                "AncientSandstoneTile",
-                "Ancient Sandstone Tile",
-                new TileLoadData(
-                    minPick: 200,
-                    dustType: DustID.Copper,
-                    soundType: SoundID.Tink,
-                    mapColor: new Color(160, 115, 75)
-                    )
-                );
+			LoadTile(
+				"AncientSandstoneTile",
+				"Ancient Sandstone Tile",
+				new TileLoadData(
+					minPick: 200,
+					dustType: DustID.Copper,
+					soundType: SoundID.Tink,
+					mapColor: new Color(160, 115, 75)
+					)
+				);
 
-            LoadTile(
-                "VitricBrick",
-                "Vitric Bricks",
+			LoadTile(
+				"VitricBrick",
+				"Vitric Bricks",
+				new TileLoadData(
+					minPick: 0,
+					dustType: DustType<Dusts.GlassNoGravity>(),
+					soundType: SoundID.Shatter,
+					mapColor: new Color(190, 255, 245)
+				)
+			  );
+
+			LoadTile(
+				"VitricCactus",
+				"Crystal Cactus",
+				new TileLoadData(
+					minPick: 0,
+					dustType: DustType<Dusts.GlassNoGravity>(),
+					soundType: SoundID.Shatter,
+					mapColor: new Color(190, 255, 245)
+				)
+			  );
+
+			LoadTile(
+				"VitricGlass",
+				"Vitric Glass",
+				new TileLoadData(
+					minPick: 0,
+					dustType: DustType<Dusts.GlassNoGravity>(),
+					soundType: SoundID.Shatter,
+					mapColor: new Color(190, 255, 245)
+				)
+			  );
+
+			LoadTile(
+				"VitricSand",
+				"Glassy Sand",
+				new TileLoadData(
+					minPick: 0,
+					dustType: DustType<Content.Dusts.AirGravity>(),
+					soundType: SoundID.Dig,
+					mapColor: new Color(172, 131, 105)
+				)
+			  );
+
+			LoadTile(
+				"VitricSoftSand",
+				"Soft Glassy Sand",
+				new TileLoadData(
+					minPick: 0,
+					dustType: DustType<Content.Dusts.AirGravity>(),
+					soundType: SoundID.Dig,
+					mapColor: new Color(182, 141, 115)
+				)
+			  );
+
+			LoadTile(
+                "VitricSandPlain",
+                "Soft Sand",
                 new TileLoadData(
                     minPick: 0,
-                    dustType: DustType<Dusts.GlassNoGravity>(),
-                    soundType: SoundID.Shatter,
-                    mapColor: new Color(190, 255, 245)
-                )
-              );
-
-            LoadTile(
-                "VitricCactus",
-                "Crystal Cactus",
-                new TileLoadData(
-                    minPick: 0,
-                    dustType: DustType<Dusts.GlassNoGravity>(),
-                    soundType: SoundID.Shatter,
-                    mapColor: new Color(190, 255, 245)
-                )
-              );
-
-            LoadTile(
-                "VitricGlass",
-                "Vitric Glass",
-                new TileLoadData(
-                    minPick: 0,
-                    dustType: DustType<Dusts.GlassNoGravity>(),
-                    soundType: SoundID.Shatter,
-                    mapColor: new Color(190, 255, 245)
-                )
-              );
-
-            LoadTile(
-                "VitricSand",
-                "Glassy Sand",
-                new TileLoadData(
-                    minPick: 0,
-                    dustType: DustType<Content.Dusts.AirGravity>(),
+                    dustType: 32,
                     soundType: SoundID.Dig,
-                    mapColor: new Color(172, 131, 105)
+                    mapColor: new Color(192, 145, 110)
                 )
               );
-
-            LoadTile(
-                "VitricSoftSand",
-                "Soft Glassy Sand",
-                new TileLoadData(
-                    minPick: 0,
-                    dustType: DustType<Content.Dusts.AirGravity>(),
-                    soundType: SoundID.Dig,
-                    mapColor: new Color(182, 141, 115)
-                )
-              );
-        }
+		}
 
         public override void PostLoad()
         {
             AddMerge("VitricSoftSand", "VitricSand");
+			AddMerge("VitricSoftSand", "VitricSandPlain");
+			AddMerge("VitricSoftSand", "VitricSand");
 
-            var sands = new int[] { TileID.Sand, TileID.Ebonsand, TileID.Crimsand, TileID.Pearlsand,
+			var sands = new int[] { TileID.Sand, TileID.Ebonsand, TileID.Crimsand, TileID.Pearlsand,
                 TileID.Sandstone, TileID.CorruptSandstone, TileID.CrimsonSandstone, TileID.HallowSandstone,
                 TileID.HardenedSand, TileID.CorruptHardenedSand, TileID.CrimsonHardenedSand, TileID.HallowHardenedSand };
+
+			AddMerge("VitricSandPlain", sands);
 
             AddMerge("VitricSoftSand", sands);
 
             AddMerge("VitricSand", sands);
 
             AddMerge("AncientSandstone", "AncientSandstoneTile");
-        }
+			AddMerge("AncientSandstone", "VitricSoftSand");
+			AddMerge("AncientSandstone", "VitricSandPlain");
+			AddMerge("AncientSandstone", "VitricSand");
+		}
     }
 }
