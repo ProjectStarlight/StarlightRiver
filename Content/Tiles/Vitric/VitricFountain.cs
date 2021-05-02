cb256ba61713426d6d17fe098548714978fc2aed
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2021-05-02 06:21:00 -0500

    Vitric Fountain

---
+++ Content/Tiles/Vitric/VitricFountain.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using StarlightRiver.Core;
+
+namespace StarlightRiver.Content.Tiles.Vitric
+{
+    internal class VitricFountain : ModFountain
+    {
+        public VitricFountain() : base("VitricFountainItem", AssetDirectory.VitricTile) { }
+        public override void FountainActive(int i, int j, bool closer) =>
+            Main.LocalPlayer.GetModPlayer<BiomeHandler>().FountainVitric = true;
+    }
+
+    internal class VitricFountainItem : QuickTileItem
+    {
+        public VitricFountainItem() : base("Vitric Fountain", "Fruit salad.\nYummy yummy.", "VitricFountain", ItemRarityID.White, AssetDirectory.VitricTile) { }
+    }
+}
