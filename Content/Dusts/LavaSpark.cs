cb67cea171b96f8a2500c04ab776cbc31f7219de
bobfishkins23 <bobfishkins23@yahoo.com>
2021-06-24 14:27:36 -0400

    I forgot to port 1 dust. nice.

---
+++ Content/Dusts/LavaSpark.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+using StarlightRiver.Core;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace StarlightRiver.Content.Dusts
+{
+    public class LavaSpark : ModDust
+    {
+        public override bool Autoload(ref string name, ref string texture)
+        {
+            texture = "StarlightRiver/Assets/Keys/GlowSoft";
+            return true;
+        }
+
+        public override void OnSpawn(Dust dust)
+        {
+            dust.noGravity = true;
+            dust.noLight = false;
+            dust.frame = new Rectangle(0, 0, 64, 64);
+            dust.fadeIn = 0;
+
+            dust.shader = new Terraria.Graphics.Shaders.ArmorShaderData(new Ref<Effect>(StarlightRiver.Instance.GetEffect("Effects/GlowingDust")), "GlowingDustPass");
+            dust.shader.UseColor(Color.Transparent);
+        }
+
+        public override Color? GetAlpha(Dust dust, Color lightColor)
+        {
+            return dust.color * (float)Math.Sin(dust.fadeIn / 60f * 3.14f);
+        }
+
+        public override bool Update(Dust dust)
+        {
+            dust.fadeIn++;
+
+            if (dust.customData is null)
+            {
+                dust.position -= Vector2.One * 32 * dust.scale;
+                dust.customData = true;
+            }
+
+            dust.shader.UseColor(dust.color * (float)Math.Sin(dust.fadeIn / 60f * 3.14f));
+
+            dust.position += dust.velocity;
+
+            dust.velocity.X *= 0.995f;
+            dust.velocity.Y += 0.08f;
+            dust.scale *= 0.99f;
+
+            if (dust.fadeIn >= 60)
+                dust.active = false;
+
+            return false;
+        }
+    }
+}
