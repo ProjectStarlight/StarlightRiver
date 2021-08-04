4fae7830355390f1c8eb4e8dd42e245573cd8726
bobfishkins23 <bobfishkins23@yahoo.com>
2021-08-04 19:11:32 -0400

    aegis progress

--- Content/Items/Vitric/Accessories.CeirosExpert.cs
+++ Content/Items/Vitric/Accessories.CeirosExpert.cs
@@ -15,6 +15,8 @@ namespace StarlightRiver.Content.Items.Vitric
 {
 	class CeirosExpert : SmartAccessory
     {
+        public int cooldown = 0;
+
         public override string Texture => AssetDirectory.VitricItem + Name;
 
         public CeirosExpert() : base("Shattered Aegis", "Releases a burning ring when damaged\n'Meet your foes head-on, and give them a scorching embrace'") { }
@@ -33,11 +35,18 @@ namespace StarlightRiver.Content.Items.Vitric
             return true;
         }
 
-        private bool PreHurtKnockback(Player player, bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
+		public override void SafeUpdateEquip(Player player)
+		{
+            if (cooldown > 0)
+                cooldown--;
+		}
+
+		private bool PreHurtKnockback(Player player, bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
         {
-            if (Equipped(player))
+            if (Equipped(player) && cooldown <= 0)
             {
                 Projectile.NewProjectile(player.Center, Vector2.Zero, ModContent.ProjectileType<FireRing>(), 100, 0, player.whoAmI);
+                cooldown = 60;
             }
 
             return true;
@@ -49,8 +58,8 @@ namespace StarlightRiver.Content.Items.Vitric
         private List<Vector2> cache;
         private Trail trail;
 
-        public float TimeFade => 1 - projectile.timeLeft / 30f;
-        public float Radius => (30 - projectile.timeLeft) * 5;
+        public float TimeFade => 1 - projectile.timeLeft / 20f;
+        public float Radius => (20 - projectile.timeLeft) * 5;
 
         public override string Texture => AssetDirectory.Invisible;
 
@@ -60,18 +69,30 @@ namespace StarlightRiver.Content.Items.Vitric
             projectile.width = 1;
             projectile.height = 1;
             projectile.tileCollide = false;
-            projectile.timeLeft = 30;
+            projectile.timeLeft = 20;
+            projectile.penetrate = -1;
 		}
 
 		public override void AI()
 		{
             ManageCaches(ref cache);
-            ManageTrail(ref trail, cache, 80);
+            ManageTrail(ref trail, cache, 50);
+
+            for (int k = 0; k < 8; k++)
+            {
+                float rot = Main.rand.NextFloat(0, 6.28f);
+                Dust.NewDustPerfect(projectile.Center + Vector2.One.RotatedBy(rot) * (Radius + 15), ModContent.DustType<Dusts.Glow>(), Vector2.One.RotatedBy(rot + Main.rand.NextFloat(1.1f, 1.3f)) * 2, 0, new Color(255, 120 + (int)(100 * (float)Math.Sin(TimeFade * 3.14f)), 65), 0.4f);
+            }
         }
 
 		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
 		{
             return Helper.CheckCircularCollision(projectile.Center, (int)Radius, targetHitbox);
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+            target.velocity += Vector2.Normalize(target.Center - projectile.Center) * 2 * target.knockBackResist;
 		}
 
 		private void ManageCaches(ref List<Vector2> cache)
@@ -80,18 +101,18 @@ namespace StarlightRiver.Content.Items.Vitric
             {
                 cache = new List<Vector2>();
 
-                for (int i = 0; i < 20; i++)
+                for (int i = 0; i < 40; i++)
                 {
                     cache.Add(projectile.Center);
                 }
             }
 
-            for(int k = 0; k < 20; k++)
+            for(int k = 0; k < 40; k++)
 			{
-                cache.Add(projectile.Center + Vector2.One.RotatedBy(k / 20f * 6.28f) * Radius);
+                cache[k] = (projectile.Center + Vector2.One.RotatedBy(k / 19f * 6.28f) * (Radius + 20));
 			}
 
-            while (cache.Count > 20)
+            while (cache.Count > 40)
             {
                 cache.RemoveAt(0);
             }
@@ -99,13 +120,13 @@ namespace StarlightRiver.Content.Items.Vitric
 
         private void ManageTrail(ref Trail trail, List<Vector2> cache, int width)
         {
-            trail = trail ?? new Trail(Main.instance.GraphicsDevice, 20, new TriangularTip(40 * 4), factor => width * (float)Math.Sin(TimeFade * 3.14f), factor =>
+            trail = trail ?? new Trail(Main.instance.GraphicsDevice, 40, new TriangularTip(40 * 4), factor => width, factor =>
             {
-                return new Color(255, 150 + (int)(factor.X * 70), 135) * (float)Math.Sin(TimeFade * 3.14f);
+                return new Color(255, 100 + (int)(100 * (float)Math.Sin(TimeFade * 3.14f)), 65) * (float)Math.Sin(TimeFade * 3.14f) * 0.5f;
             });
 
             trail.Positions = cache.ToArray();
-            trail.NextPosition = projectile.Center + Vector2.One * Radius;
+            trail.NextPosition = cache[39];
         }
 
         public void DrawPrimitives()
@@ -116,10 +137,14 @@ namespace StarlightRiver.Content.Items.Vitric
             Matrix view = Main.GameViewMatrix.ZoomMatrix;
             Matrix projection = Matrix.CreateOrthographicOffCenter(0, Main.screenWidth, Main.screenHeight, 0, -1, 1);
 
-            effect.Parameters["time"].SetValue(Main.GameUpdateCount);
-            effect.Parameters["repeats"].SetValue(2f);
+            effect.Parameters["time"].SetValue(projectile.timeLeft * 0.01f);
+            effect.Parameters["repeats"].SetValue(6);
             effect.Parameters["transformMatrix"].SetValue(world * view * projection);
             effect.Parameters["sampleTexture"].SetValue(ModContent.GetTexture("StarlightRiver/Assets/EnergyTrail"));
+
+            trail?.Render(effect);
+
+            effect.Parameters["sampleTexture"].SetValue(ModContent.GetTexture("StarlightRiver/Assets/FireTrail"));
 
             trail?.Render(effect);
         }
