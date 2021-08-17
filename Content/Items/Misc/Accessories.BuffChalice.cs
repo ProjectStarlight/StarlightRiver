32b99907f6f5b136a04abf3ddfd55b2f32a90745
bobfishkins23 <bobfishkins23@yahoo.com>
2021-08-17 01:51:01 -0400

    buff tracker

---
+++ Content/Items/Misc/Accessories.BuffChalice.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Xna.Framework;
+using StarlightRiver.Content.Items.BaseTypes;
+using StarlightRiver.Core;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Content.Items.Misc
+{
+	class BuffChalice : SmartAccessory
+	{
+		public int rage = 0;
+
+		public override string Texture => AssetDirectory.MiscItem + Name;
+
+		public BuffChalice() : base("Plexus Chalice", "Inflicting debuffs grants innoculation") { }
+
+		public override bool Autoload(ref string name)
+		{
+			StatusTrackingNPC.buffCompareEffects += ChaliceEffects;
+			return base.Autoload(ref name);
+		}
+
+		private void ChaliceEffects(Player player, NPC npc, int[] oldTypes, int[] newTypes, int[] oldTimes, int[] newTimes)
+		{
+			if (Equipped(player))
+			{
+				for(int k = 0; k < 5; k++)
+				{
+					if (oldTypes[k] != newTypes[k] || newTimes[k] > oldTimes[k])
+						player.AddBuff(ModContent.BuffType<Potions.InnoculationPotionBuff>(), 120);
+				}
+			}
+		}
+	}
+}
---
+++ Core/StatusTrackingNPC.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StarlightRiver.Content.Tiles.Vitric;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+
+namespace StarlightRiver.Core
+{
+	class StatusTrackingNPC : GlobalNPC
+	{
+		private Player attacker;
+		private int[] storedBuffs = new int[5];
+		private int[] storedTimes = new int[5];
+		private bool compareBuffs;
+
+		public static event Action<Player, NPC, int[], int[], int[], int[]> buffCompareEffects;
+
+		public override bool InstancePerEntity => true;
+
+		public StatusTrackingNPC Tracker(NPC npc) => npc.GetGlobalNPC<StatusTrackingNPC>();
+
+		public override bool PreAI(NPC npc)
+		{
+			return base.PreAI(npc);
+		}
+
+		public override void ModifyHitByItem(NPC npc, Player player, Item item, ref int damage, ref float knockback, ref bool crit)
+		{
+			Tracker(npc).attacker = player;
+			Tracker(npc).compareBuffs = true;
+		}
+
+		public override void ModifyHitByProjectile(NPC npc, Projectile projectile, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+		{
+			Tracker(npc).attacker = Main.player[projectile.owner];
+			Tracker(npc).compareBuffs = true;
+		}
+
+		public override void ResetEffects(NPC npc)
+		{
+			if (Tracker(npc).compareBuffs)
+			{
+				buffCompareEffects.Invoke(attacker, npc, Tracker(npc).storedBuffs, npc.buffType, Tracker(npc).storedTimes, npc.buffTime);
+				Tracker(npc).compareBuffs = false;
+			}
+
+			npc.buffType.CopyTo(Tracker(npc).storedBuffs, 0);
+			npc.buffTime.CopyTo(Tracker(npc).storedTimes, 0);
+		}
+	}
+}
