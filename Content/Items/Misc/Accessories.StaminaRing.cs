92dccc1510170ac001d521bf7b1ef4ff13c4169c
bobfishkins23 <bobfishkins23@yahoo.com>
2021-09-04 01:51:25 -0400

    changed stamina ring

--- Content/Items/Misc/Accessories.StaminaRing.cs
+++ Content/Items/Misc/Accessories.StaminaRing.cs
@@ -13,25 +13,14 @@ namespace StarlightRiver.Content.Items.Misc
     {
         public override string Texture => AssetDirectory.MiscItem + Name;
 
-        public StaminaRing() : base("Band of Endurance", "Increased max stamina and stamina regeneration") { }
+        public StaminaRing() : base("Band of Endurance", "Slowly regenerates stamina") { }
 
-        public override void SafeSetDefaults() => item.rare = ItemRarityID.Blue;
-        public override void AddRecipes()
-        {
-            ModRecipe recipe = new ModRecipe(mod);
+        public override void SafeSetDefaults() => item.rare = ItemRarityID.Green;
 
-            recipe.AddIngredient(ItemType<StaminaUp>());
-            recipe.AddIngredient(ItemType<MossSalve>());
-            recipe.AddTile(TileID.TinkerersWorkbench);
-
-            recipe.SetResult(ItemType<StaminaRing>());
-
-            recipe.AddRecipe();
-        }
         public override void SafeUpdateEquip(Player player)
         {
             AbilityHandler mp = player.GetHandler();
-            mp.StaminaMaxBonus += 1;
+            mp.StaminaRegenRate += 0.05f;
         }
     }
 }
