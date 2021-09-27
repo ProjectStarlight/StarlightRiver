37119248265fb4eab40c64567f39e899f0b6bcab
bobfishkins23 <bobfishkins23@yahoo.com>
2021-09-26 23:29:59 -0400

    Added tutorial text

--- Content/CustomHooks/Visuals.CutawayHandler.cs
+++ Content/CustomHooks/Visuals.CutawayHandler.cs
@@ -47,6 +47,9 @@ namespace StarlightRiver.Content.CustomHooks
 
 		private void DrawPositive(On.Terraria.Main.orig_DrawDust orig, Main self)
 		{
+			orig(self);
+			return;
+
 			for (int k = 0; k < cutaways.Count; k++)
 				cutaways[k].Draw();
 
