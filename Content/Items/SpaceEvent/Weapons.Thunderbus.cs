7e956a1b765311430968e8b8b9b11c16e7edd91a
bobfishkins23 <bobfishkins23@yahoo.com>
2021-10-10 02:54:46 -0400

    Thunderbus Orb Prototype

--- Content/Items/SpaceEvent/Weapons.Thunderbus.cs
+++ Content/Items/SpaceEvent/Weapons.Thunderbus.cs
@@ -14,7 +14,11 @@ namespace StarlightRiver.Content.Items.SpaceEvent
 {
 	class Thunderbuss : ModItem
 	{
+        public Projectile ball;
+
         public override string Texture => "StarlightRiver/Assets/Items/SpaceEvent/Thunderbuss";
+
+        public override bool AltFunctionUse(Player player) => true;
 
 		public override void SetDefaults()
 		{
@@ -30,9 +34,37 @@ namespace StarlightRiver.Content.Items.SpaceEvent
 
 		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
 		{
+            float aim = (player.Center - Main.MouseWorld).ToRotation();
+
+            if (ball != null && (!ball.active || ball.type != ModContent.ProjectileType<ThunderbussBall>()))
+                ball = null;
+
+            if(player.altFunctionUse == 2)
+			{
+                int i = Projectile.NewProjectile(player.Center - new Vector2(64, 0).RotatedBy(aim), new Vector2(speedX, speedY) * 0.4f, ModContent.ProjectileType<ThunderbussBall>(), damage, 0, player.whoAmI);
+                ball = Main.projectile[i];
+
+                return false;
+			}
+
+            if(ball != null && player == Main.LocalPlayer && Vector2.Distance(ball.Center, Main.MouseWorld) < 128)
+			{
+                int i = Projectile.NewProjectile(player.Center, Vector2.Zero, type, damage, knockBack, player.whoAmI);
+
+                var mp = Main.projectile[i].modProjectile as ThunderbussShot;
+
+                mp.holdRot = (Main.MouseWorld - player.Center).ToRotation();
+                mp.projTarget = ball;
+                mp.power = 30;
+
+                ball.ai[1] = 1;
+
+                return false;
+			}
+
             List<NPC> targets = new List<NPC>();
 
-            foreach(NPC npc in Main.npc.Where(n => n.active && !n.immortal && !n.dontTakeDamage && Helpers.Helper.CheckConicalCollision(player.Center, 500, (player.Center - Main.MouseWorld).ToRotation(), 1, n.Hitbox)))
+            foreach(NPC npc in Main.npc.Where(n => n.active && !n.dontTakeDamage && Helpers.Helper.CheckConicalCollision(player.Center, 500, aim, 1, n.Hitbox)))
 			{
                 targets.Add(npc);
 			}
@@ -43,7 +75,7 @@ namespace StarlightRiver.Content.Items.SpaceEvent
 
                 for (int k = 0; k < 20; k++)
                 {
-                    float dustRot = (player.Center - Main.MouseWorld).ToRotation() + 1.57f * 1.5f + Main.rand.NextFloat(-0.2f, 0.2f);
+                    float dustRot = aim + 1.57f * 1.5f + Main.rand.NextFloat(-0.2f, 0.2f);
                     Dust.NewDustPerfect(player.Center + Vector2.One.RotatedBy(dustRot) * 80 + new Vector2(0, 32), ModContent.DustType<Dusts.GlowLine>(), Vector2.One.RotatedBy(dustRot) * Main.rand.NextFloat(5), 0, new Color(100, 200, 255), 0.6f);
                 }
 
@@ -58,7 +90,7 @@ namespace StarlightRiver.Content.Items.SpaceEvent
                 int i = Projectile.NewProjectile(player.Center, Vector2.Zero, type, damage, knockBack, player.whoAmI);
 
                 var mp = Main.projectile[i].modProjectile as ThunderbussShot;
-                mp.offset = Helpers.Helper.CompareAngle((player.Center - Main.MouseWorld).ToRotation(), (player.Center - targets[targetIndex].Center).ToRotation()) * -120;
+                mp.offset = Helpers.Helper.CompareAngle(aim, (player.Center - targets[targetIndex].Center).ToRotation()) * -120;
 
                 if(targets.Count == 1)
 				{
@@ -75,6 +107,7 @@ namespace StarlightRiver.Content.Items.SpaceEvent
 
                 mp.holdRot = (Main.MouseWorld - player.Center).ToRotation();
                 mp.target = targets[targetIndex];
+                mp.power = 20;
             }
 
             return false;
@@ -86,6 +119,10 @@ namespace StarlightRiver.Content.Items.SpaceEvent
         private Vector2 startPoint;
         public Vector2 endPoint;
         public Vector2 midPoint;
+
+        public int power;
+        public Projectile projOwner;
+        public Projectile projTarget;
 
         private float dist1;
         private float dist2;
@@ -119,7 +156,7 @@ namespace StarlightRiver.Content.Items.SpaceEvent
 
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Electro Arrow");
+            DisplayName.SetDefault("Electro Shock");
         }
 
         private Vector2 PointOnSpline(float progress) //I should really move this spline stuff somewhere central eventually. heh.
@@ -159,7 +196,7 @@ namespace StarlightRiver.Content.Items.SpaceEvent
 
             if (projectile.timeLeft == 60)
             {
-                Helpers.Helper.PlayPitched("Magic/LightningExplodeShallow", 0.4f, 0.5f, projectile.Center);
+                Helpers.Helper.PlayPitched("Magic/LightningExplodeShallow", 0.4f * (power / 20f), 0.5f, projectile.Center);
 
                 savedPos = projectile.Center;
                 startPoint = projectile.Center;       
@@ -168,8 +205,16 @@ namespace StarlightRiver.Content.Items.SpaceEvent
                 dist2 = ApproximateSplineLength(30, midPoint, endPoint - startPoint, endPoint, endPoint - midPoint);
             }
 
-            startPoint = Main.player[projectile.owner].Center + Vector2.UnitX.RotatedBy(holdRot) * 64;
-            endPoint = target.Center;
+            if (projOwner is null)
+                startPoint = Main.player[projectile.owner].Center + Vector2.UnitX.RotatedBy(holdRot) * 64;
+            else
+                startPoint = projOwner.Center;
+
+            if (projTarget is null)
+                endPoint = target.Center;
+            else
+                endPoint = projTarget.Center;
+
             midPoint = Vector2.Lerp(startPoint, endPoint, 0.5f) + Vector2.Normalize(endPoint - startPoint).RotatedBy(1.57f) * (offset + (float)Math.Sin(Main.GameUpdateCount * 0.2f) * 10);
 
             projectile.Center = endPoint;
@@ -209,8 +254,8 @@ namespace StarlightRiver.Content.Items.SpaceEvent
             {
                 Vector2 prevPos = k == 1 ? point1 : nodes[k - 1];
 
-                var target = new Rectangle((int)(prevPos.X - Main.screenPosition.X), (int)(prevPos.Y - Main.screenPosition.Y), (int)Vector2.Distance(nodes[k], prevPos) + 1, 20);
-                var target2 = new Rectangle((int)(prevPos.X - Main.screenPosition.X), (int)(prevPos.Y - Main.screenPosition.Y), (int)Vector2.Distance(nodes[k], prevPos) + 1, 100);
+                var target = new Rectangle((int)(prevPos.X - Main.screenPosition.X), (int)(prevPos.Y - Main.screenPosition.Y), (int)Vector2.Distance(nodes[k], prevPos) + 1, power);
+                var target2 = new Rectangle((int)(prevPos.X - Main.screenPosition.X), (int)(prevPos.Y - Main.screenPosition.Y), (int)Vector2.Distance(nodes[k], prevPos) + 1, power * 5);
                 var origin = new Vector2(0, tex.Height / 2);
                 var rot = (nodes[k] - prevPos).ToRotation();
                 var color = new Color(200, 230, 255) * (projectile.extraUpdates == 0 ? projectile.timeLeft / 15f : 1);
@@ -219,7 +264,7 @@ namespace StarlightRiver.Content.Items.SpaceEvent
                 sb.Draw(tex, target2, null, color * 0.5f, rot, origin, 0, 0);
 
                 if (Main.rand.Next(20) == 0)
-                    Dust.NewDustPerfect(prevPos + new Vector2(0, 50), ModContent.DustType<Dusts.GlowLine>(), Vector2.Normalize(nodes[k] - prevPos) * Main.rand.NextFloat(-6, -4), 0, new Color(100, 150, 200), 0.8f);
+                    Dust.NewDustPerfect(prevPos + new Vector2(0, 50), ModContent.DustType<Dusts.GlowLine>(), Vector2.Normalize(nodes[k] - prevPos) * Main.rand.NextFloat(-6, -4), 0, new Color(100, 150, 200) * (power / 20f), 0.8f);
             }
         }
 
@@ -233,7 +278,7 @@ namespace StarlightRiver.Content.Items.SpaceEvent
                 Dust.NewDustPerfect(target.Center + Vector2.One.RotatedBy(dustRot) * 24 + new Vector2(0, 32), ModContent.DustType<Dusts.GlowLine>(), Vector2.One.RotatedBy(dustRot) * Main.rand.NextFloat(1), 0, new Color(100, 200, 255), 0.5f);
             }
 
-            Main.LocalPlayer.GetModPlayer<StarlightPlayer>().Shake += 8;
+            Main.LocalPlayer.GetModPlayer<StarlightPlayer>().Shake += power / 4;
             projectile.damage = 0;
         }
 
@@ -263,4 +308,58 @@ namespace StarlightRiver.Content.Items.SpaceEvent
             return false;
         }
     }
+
+    internal class ThunderbussBall : ModProjectile, IDrawAdditive
+	{
+        public override string Texture => AssetDirectory.Invisible;
+
+        public ref float Stacks => ref projectile.ai[0];
+        public ref float ShouldFire => ref projectile.ai[1];
+
+        public override void SetDefaults()
+		{
+            projectile.width = 32;
+            projectile.height = 32;
+            projectile.timeLeft = 600;
+            projectile.magic = true;
+            projectile.penetrate = -1;
+            projectile.damage = 0;
+		}
+
+		public override void AI()
+		{
+            projectile.velocity.Y += 0.015f;
+
+            if(Stacks < 1.5f)
+                Stacks += 0.05f;
+
+            if(ShouldFire > 0)
+			{
+                for(int k = 0; k < Main.maxNPCs; k++)
+				{
+                    var npc = Main.npc[k];
+                    if(npc.active && Helpers.Helper.CheckCircularCollision(projectile.Center, (int)(150 * Stacks), npc.Hitbox))
+					{
+                        int i = Projectile.NewProjectile(projectile.Center, Vector2.Zero, ModContent.ProjectileType<ThunderbussShot>(), 24, 0, projectile.owner);
+                        var proj = Main.projectile[i].modProjectile as ThunderbussShot;
+
+                        proj.target = npc;
+                        proj.projOwner = projectile;
+                        proj.power = 10;
+					}
+				}
+
+                ShouldFire = 0;
+			}
+		}
+
+		public void DrawAdditive(SpriteBatch spriteBatch)
+		{
+			var tex = ModContent.GetTexture("StarlightRiver/Assets/Keys/GlowSoft");
+			spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, null, new Color(200, 230, 255), 0, tex.Size() / 2, 1, 0, 0);
+
+            var tex2 = ModContent.GetTexture("StarlightRiver/Assets/Bosses/VitricBoss/BombTell");
+            spriteBatch.Draw(tex2, projectile.Center - Main.screenPosition, null, new Color(200, 230, 255) * 0.3f, 0, tex2.Size() / 2, 0.75f * Stacks, 0, 0);
+        }
+	}
 }
