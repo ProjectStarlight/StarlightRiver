fec2f0b949c9e8cbf5b5a079f9b6790bdf8eea25
eggshellsv <angelvortexgaming@gmail.com>
2021-10-12 20:11:36 -0400

    demo text

--- Content/Tiles/Vitric/Temple/TutorialText.cs
+++ Content/Tiles/Vitric/Temple/TutorialText.cs
@@ -26,14 +26,14 @@ namespace StarlightRiver.Content.Tiles.Vitric.Temple
         {
             var pos = (new Vector2(i + 0.5f, j + 0.5f) + Helper.TileAdj) * 16 - Main.screenPosition;
 
-            Utils.DrawBorderString(spriteBatch, Helper.WrapString("Welcome to our demo blah blah blah this is filler test blah blah filler text this is to make sure whatever egshels writes will fit blah blah blah blah blah blah", 250, Main.fontItemStack, 0.8f), pos, Color.White, 0.8f);
+            Utils.DrawBorderString(spriteBatch, Helper.WrapString("Welcome to Starlight River! This is a demo version of Starlight River with a focus on Ceiros, the third (and currently most complete) boss in the mod. Try out some of the gear available at this progression point and tell us what you think!", 250, Main.fontItemStack, 0.8f), pos, Color.White, 0.8f);
 
             Utils.DrawBorderString(spriteBatch, Helper.WrapString("Melee", 300, Main.fontItemStack, 0.8f), pos + new Vector2(-650, 150), Color.White, 1f);
             Utils.DrawBorderString(spriteBatch, Helper.WrapString("Ranged", 300, Main.fontItemStack, 0.8f), pos + new Vector2(-500, 150), Color.White, 1f);
             Utils.DrawBorderString(spriteBatch, Helper.WrapString("Magic", 300, Main.fontItemStack, 0.8f), pos + new Vector2(-350, 150), Color.White, 1f);
             Utils.DrawBorderString(spriteBatch, Helper.WrapString("Consumables", 300, Main.fontItemStack, 0.8f), pos + new Vector2(-210, 150), Color.White, 1f);
 
-            Utils.DrawBorderString(spriteBatch, Helper.WrapString("The forbidden winds ability shall make you cum big globs of the goopy white stuff haha filler text filler text filler text filler text filler text filler text", 300, Main.fontItemStack, 0.8f), pos + new Vector2(-570, -370), Color.White, 0.8f);
+            Utils.DrawBorderString(spriteBatch, Helper.WrapString("Forbidden Winds is the first obtainable ability, granting a dash that allows you to maneuver with ease and interact with objects. Like all abilities, it's regularly used throughout progression at all stages of the game past obtainment.", 300, Main.fontItemStack, 0.8f), pos + new Vector2(-570, -370), Color.White, 0.8f);
         }
     }
 
