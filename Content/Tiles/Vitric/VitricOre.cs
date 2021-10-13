8784c6c352fa9009b68bf40bb8b849d3c557dbd6
bobfishkins23 <bobfishkins23@yahoo.com>
2021-10-13 19:19:54 -0400

    Added anchor and draw offset to vitric ore

--- Content/Tiles/Vitric/VitricOre.cs
+++ Content/Tiles/Vitric/VitricOre.cs
@@ -12,41 +12,55 @@ namespace StarlightRiver.Content.Tiles.Vitric
 {
 	internal class VitricOre : DummyTile
     {
+        public override int DummyType => ProjectileType<VitricOreDummy>();
+
         public override bool Autoload(ref string name, ref string texture)
         {
             texture = AssetDirectory.VitricTile + name;
             return base.Autoload(ref name, ref texture);
         }
-
-        public override int DummyType => ProjectileType<VitricOreDummy>();
+    
         public override void SetDefaults()
         {
             TileObjectData.newTile.DrawYOffset = 2;
-            (this).QuickSetFurniture(2, 3, DustType<Dusts.Air>(), SoundID.Shatter, new Color(200, 255, 230), 16, false, false, "Vitric Ore");
             minPick = int.MaxValue;
+
+            var bottomAnchor = new Terraria.DataStructures.AnchorData(Terraria.Enums.AnchorType.SolidTile, 2, 0);
+            (this).QuickSetFurniture(2, 3, DustType<Dusts.Air>(), SoundID.Shatter, new Color(200, 255, 230), 16, false, false, "Vitric Ore", bottomAnchor);        
         }
-        public override void KillMultiTile(int i, int j, int frameX, int frameY) => Item.NewItem(new Vector2(i, j) * 16, ItemType<Items.Vitric.VitricOre>(), 12);
-    }
+
+		public override void KillMultiTile(int i, int j, int frameX, int frameY)
+		{
+			Item.NewItem(new Vector2(i, j) * 16, ItemType<Items.Vitric.VitricOre>(), 12);
+		}
+	}
 
     internal class VitricOreFloat : DummyTile
     {
+        public override int DummyType => ProjectileType<VitricOreFloatDummy>();
+
         public override bool Autoload(ref string name, ref string texture)
         {
             texture = AssetDirectory.VitricTile + name;
             return base.Autoload(ref name, ref texture);
         }
 
-        public override int DummyType => ProjectileType<VitricOreFloatDummy>();
         public override void SetDefaults()
         {
             (this).QuickSetFurniture(2, 2, DustType<Content.Dusts.Air>(), SoundID.Shatter, false, new Color(200, 255, 230), false, false, "Vitric Ore");
             minPick = int.MaxValue;
         }
-        public override void KillMultiTile(int i, int j, int frameX, int frameY) => Item.NewItem(new Vector2(i, j) * 16, ItemType<Items.Vitric.VitricOre>(), 6);
+
+        public override void KillMultiTile(int i, int j, int frameX, int frameY)
+        {
+            Item.NewItem(new Vector2(i, j) * 16, ItemType<Items.Vitric.VitricOre>(), 6);
+        }
     }
 
     internal class VitricOreDummy : Dummy
     {
+        public override string Texture => AssetDirectory.VitricTile + "VitricOreGlow";
+
         public VitricOreDummy() : base(TileType<VitricOre>(), 32, 48) { }
 
         public override void Collision(Player player)
@@ -64,19 +78,19 @@ namespace StarlightRiver.Content.Tiles.Vitric
             }
         }
 
-        public override string Texture => AssetDirectory.VitricTile + "VitricOreGlow";
-
         public override void PostDraw(SpriteBatch spriteBatch, Color lightColor)
         {
             Texture2D tex = GetTexture(AssetDirectory.VitricTile + "VitricOreGlow");
             Color color = Helper.IndicatorColorProximity(150, 300, projectile.Center);
 
-            spriteBatch.Draw(tex, projectile.position - new Vector2(1, -1) - Main.screenPosition, color);
+            spriteBatch.Draw(tex, projectile.position - new Vector2(1, -3) - Main.screenPosition, color);
         }
     }
 
     internal class VitricOreFloatDummy : Dummy
     {
+        public override string Texture => AssetDirectory.VitricTile + "VitricOreFloatGlow";
+
         public VitricOreFloatDummy() : base(TileType<VitricOreFloat>(), 32, 32) { }
 
         public override void Collision(Player player)
@@ -94,8 +108,6 @@ namespace StarlightRiver.Content.Tiles.Vitric
             }
         }
 
-        public override string Texture => AssetDirectory.VitricTile + "VitricOreFloatGlow";
-
         public override void PostDraw(SpriteBatch spriteBatch, Color lightColor)
         {
             Texture2D tex = GetTexture(AssetDirectory.VitricTile + "VitricOreFloatGlow");
@@ -104,10 +116,12 @@ namespace StarlightRiver.Content.Tiles.Vitric
             spriteBatch.Draw(tex, projectile.position - Vector2.One - Main.screenPosition, color);
         }
     }
+
     class VitricOreItem : QuickTileItem
     {
         public VitricOreItem() : base("Vitric Ore Crystal Item", "", TileType<VitricOre>(), 1, AssetDirectory.Debug, true) { }
     }
+
     class VitricOreFloatItem : QuickTileItem
     {
         public VitricOreFloatItem() : base("Floating Vitric Ore Crystal Item", "", TileType<VitricOreFloat>(), 1, AssetDirectory.Debug, true) { }
