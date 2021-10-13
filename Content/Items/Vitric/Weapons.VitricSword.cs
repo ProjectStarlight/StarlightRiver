cff6e53cccaa328d6921a20357a269a332fcb783
bobfishkins23 <bobfishkins23@yahoo.com>
2021-10-13 18:07:05 -0400

    buffed proejectiles of ancient vitric blade

--- Content/Items/Vitric/Weapons.VitricSword.cs
+++ Content/Items/Vitric/Weapons.VitricSword.cs
@@ -41,9 +41,9 @@ namespace StarlightRiver.Content.Items.Vitric
             if (!Broken)
             {
                 Main.PlaySound(SoundID.Item107);
-                Projectile.NewProjectile(target.Center, Vector2.Normalize(player.Center - target.Center) * -32, mod.ProjectileType("VitricSwordProjectile"), 15, 0, player.whoAmI);
-                Projectile.NewProjectile(target.Center, Vector2.Normalize(player.Center - target.Center).RotatedBy(0.3) * -16, mod.ProjectileType("VitricSwordProjectile"), 15, 0, player.whoAmI);
-                Projectile.NewProjectile(target.Center, Vector2.Normalize(player.Center - target.Center).RotatedBy(-0.25) * -24, mod.ProjectileType("VitricSwordProjectile"), 15, 0, player.whoAmI);
+                Projectile.NewProjectile(target.Center, Vector2.Normalize(player.Center - target.Center) * -32, mod.ProjectileType("VitricSwordProjectile"), 24, 0, player.whoAmI);
+                Projectile.NewProjectile(target.Center, Vector2.Normalize(player.Center - target.Center).RotatedBy(0.3) * -16, mod.ProjectileType("VitricSwordProjectile"), 24, 0, player.whoAmI);
+                Projectile.NewProjectile(target.Center, Vector2.Normalize(player.Center - target.Center).RotatedBy(-0.25) * -24, mod.ProjectileType("VitricSwordProjectile"), 24, 0, player.whoAmI);
 
                 for (int k = 0; k <= 20; k++)
                 {
