d3c6a3316411fce6c96a05c7b8710a67678e63c9
eggshellsv <angelvortexgaming@gmail.com>
2021-10-15 01:26:17 -0400

    breacher sound tweaks

--- Content/Items/Breacher/Weapons.FlareBreacher.cs
+++ Content/Items/Breacher/Weapons.FlareBreacher.cs
@@ -64,7 +64,7 @@ namespace StarlightRiver.Content.Items.Breacher
                 dust.noGravity = true;
             }
 
-            Helper.PlayPitched("Guns/FlareFire", 0.6f, Main.rand.NextFloat(-0.1f,0.1f));
+            Helper.PlayPitched("Guns/FlareFire", 0.5f, Main.rand.NextFloat(-0.1f,0.1f));
             return base.Shoot(player, ref position, ref speedX, ref speedY, ref type, ref damage, ref knockBack);
         }
     }
@@ -121,7 +121,7 @@ namespace StarlightRiver.Content.Items.Breacher
                     if (!red)
                     {
                         red = true;
-                        Helper.PlayPitched("Effects/Bleep", 0.6f, 1 - (explosionTimer / 100f));
+                        Helper.PlayPitched("Effects/Bleep", 0.7f, 1 - (explosionTimer / 100f));
                         blinkCounter = 0;
                     }
                     else
@@ -185,7 +185,7 @@ namespace StarlightRiver.Content.Items.Breacher
 
         private void Explode(NPC target)
         {
-            Helper.PlayPitched("Guns/FlareBoom", 0.6f, Main.rand.NextFloat(-0.1f, 0.1f));
+            Helper.PlayPitched("Guns/FlareBoom", 0.4f, Main.rand.NextFloat(-0.1f, 0.1f));
 
             if (!target.immortal && !target.dontTakeDamage)
                 target.StrikeNPC(projectile.damage, 0f, 0);
