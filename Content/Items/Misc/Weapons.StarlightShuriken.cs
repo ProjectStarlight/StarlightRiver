b24b070ec51539b50557907f5a5ae4ab5fb903ea
bobfishkins23 <bobfishkins23@yahoo.com>
2021-10-15 20:40:13 -0400

    Added missing item rarities

--- Content/Items/Misc/Weapons.StarlightShuriken.cs
+++ Content/Items/Misc/Weapons.StarlightShuriken.cs
@@ -41,8 +41,9 @@ namespace StarlightRiver.Content.Items.Misc
             item.shootSpeed = 15f;
             item.noMelee = true;
 			item.noUseGraphic = true;
-            item.autoReuse = true;
-        }
+			item.autoReuse = true;
+			item.rare = ItemRarityID.Green;
+		}
 
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
