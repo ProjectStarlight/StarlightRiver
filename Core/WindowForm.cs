0db6415200e064e4512581a4e00f1040d66f9f8b
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2021-10-25 03:11:54 -0500

    Window icon fixed

--- Core/WindowForm.cs
+++ Core/WindowForm.cs
@@ -6,6 +6,7 @@ using System;
 using Microsoft.Xna.Framework.Graphics;
 using System.IO;
 using Terraria.ModLoader;
+using System.Reflection;
 
 namespace StarlightRiver.Core.Loaders
 {
@@ -15,15 +16,9 @@ namespace StarlightRiver.Core.Loaders
 
         public float Priority => 1.3f;
 
-        const string DefaultTitle = "Starlight River Dev Build";
+        const string DefaultTitle = "Starlight River Test Build";
 
         const int iconSize = 48; //16, 32, 48
-
-        private void GameFormChanges()
-        {
-            GameForm.Icon = Texture2Icon(ModContent.GetTexture("StarlightRiver/Assets/Misc/StarlightTree"));
-            GameForm.Text = DefaultTitle;//TODO: make random.
-        }
 
         public void Load()
         {
@@ -31,16 +26,43 @@ namespace StarlightRiver.Core.Loaders
             {
                 using (Control threadControl = new Control())
                 {
-                    //_threadControl.Enabled = true;
-                    //_threadControl.Visible = true;
                     IntPtr ptr = threadControl.Handle;//this needs to be accessed for it to be generated
                     if (threadControl.IsHandleCreated)//if the icon doesn't show its its likely because this check failed
                         threadControl.Invoke((Action)(() => 
                         { GameForm = (Form)Control.FromHandle(Main.instance.Window.Handle); }));
                 }
 
-                InvokeOnForm(GameFormChanges);
+                TryFormChanges();
             }
+        }
+
+        private Func<Form, bool> IsFormActive;
+
+        private void TryFormChanges()//check if the window is active
+        {
+            PropertyInfo IsActiveInfo = typeof(Form).GetProperty("Active", BindingFlags.Instance | BindingFlags.NonPublic);//grab 'Active' property info (Unsure why this is private)
+            IsFormActive = (Func<Form, bool>)Delegate.CreateDelegate( typeof(Func<Form, bool>), IsActiveInfo.GetGetMethod(true));//create delegate for property getter
+
+            if (!IsFormActive(GameForm))//if inactive detour Main.DoUpdate and wait for it to become active
+                On.Terraria.Main.DoUpdate += WaitForWindowAvailible;
+            else
+                InvokeOnForm(WindowFormChanges);//if active change icon
+        }
+
+        private void WaitForWindowAvailible(On.Terraria.Main.orig_DoUpdate orig, Main self, GameTime gameTime)
+        {
+            orig(self, gameTime);
+            if (IsFormActive(GameForm))//if form becomes active change icon and un-detour Main.DoUpdate
+            {
+                InvokeOnForm(WindowFormChanges);
+                On.Terraria.Main.DoUpdate -= WaitForWindowAvailible;
+            }
+        }
+
+        private void WindowFormChanges()//generates a icon from a png and changes the window text
+        {
+            GameForm.Icon = Texture2Icon(ModContent.GetTexture("StarlightRiver/Assets/Misc/StarlightTree"));
+            GameForm.Text = DefaultTitle;//TODO: make random.
         }
 
         //public static Image Texture2Image(Texture2D texture)
@@ -57,7 +79,7 @@ namespace StarlightRiver.Core.Loaders
         //    return img;
         //}
         public static Icon Texture2Icon(Texture2D texture)
-        {
+        {//this seems to break if the window is minimized in fullscreen... or might be setting it breaking
             Icon icon;
             using (MemoryStream MS = new MemoryStream())
             {
@@ -77,7 +99,7 @@ namespace StarlightRiver.Core.Loaders
         }
         public void Unload()
         {
-
+            On.Terraria.Main.DoUpdate -= WaitForWindowAvailible;
         }
     }
 }
