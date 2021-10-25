0f744070d76c42314268e81b7bbcc99aaa5084a9
bobfishkins23 <bobfishkins23@yahoo.com>
2021-10-25 18:12:08 -0400

    Added Convenient Excuse

---
+++ Content/Items/ConvenientExcuse.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StarlightRiver.Core;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace StarlightRiver.Content.Items
+{
+	class ConvenientExcuse : ModItem
+	{
+		public static string sike = "Lol you thought you were gonna find something in here";
+
+        public override string Texture => "StarlightRiver/Assets/Items/ConvenientExcuse";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("The rest of the mod's content");
+			Tooltip.SetDefault("Tightly packed into a conveniently indecipherable ball");
+		}
+
+		public override bool UseItem(Player player)
+		{
+			Main.NewText("It's far too tightly compacted to open without breaking everything inside...", new Color(255, 255, 200));
+			return true;
+		}
+	}
+}
