b4ebe985361af830ae988d1dec4fb9cd7f95c4eb
bobfishkins23 <bobfishkins23@yahoo.com>
2021-10-25 17:54:25 -0400

    fix

--- Content/Items/Vitric/Placeholder.cs
+++ Content/Items/Vitric/Placeholder.cs
@@ -10,12 +10,18 @@ namespace StarlightRiver.Content.Items.Vitric
     public class VitricSummonOrb
     {
         public static Color MoltenGlow(float time)
+        {
             Color MoltenGlowc = Color.White;
             if (time > 30 && time < 60)
                 MoltenGlowc = Color.Lerp(Color.White, Color.Orange, Math.Min((time - 30f) / 20f, 1f));
             else if (time >= 60)
                 MoltenGlowc = Color.Lerp(Color.Orange, Color.Lerp(Color.Red, Color.Transparent, Math.Min((time - 60f) / 50f, 1f)), Math.Min((time - 60f) / 30f, 1f));
             return MoltenGlowc;
+        }
+
+        public static Rectangle WhiteFrame(Rectangle tex, bool white)
+        {
+            return new Rectangle(white ? tex.Width / 2 : 0, tex.Y, tex.Width / 2, tex.Height);
         }
     }
 }
