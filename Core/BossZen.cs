390d3af27c6bfa178a00951c3ec78ceb50f868bf
bobfishkins23 <bobfishkins23@yahoo.com>
2021-10-26 15:41:10 -0400

    boss zen

---
+++ Core/BossZen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Core
+{
+    class BossZen : GlobalNPC
+    {
+		public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
+		{
+			if (StarlightWorld.VitricBossArena.Contains((player.Center / 16).ToPoint()))
+			{
+				spawnRate = int.MaxValue;
+				maxSpawns = 1;
+			}
+		}
+	}
+}
