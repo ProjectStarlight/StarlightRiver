30edf181f6c579afc8cdcbd67a43b3474eb97bca
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2021-11-21 02:43:46 -0600

    Hotspring bug fixes

--- Content/CustomHooks/Visuals.HotspringMapTarget.cs
+++ Content/CustomHooks/Visuals.HotspringMapTarget.cs
@@ -8,6 +8,7 @@ using Microsoft.Xna.Framework.Graphics;
 using StarlightRiver.Content.Bosses.SquidBoss;
 using StarlightRiver.Content.Tiles.Underground;
 using Terraria;
+using static Terraria.Utils;
 
 namespace StarlightRiver.Content.CustomHooks
 {
@@ -52,7 +53,7 @@ namespace StarlightRiver.Content.CustomHooks
             graphics.SetRenderTarget(hotspringMapTarget);
 
             graphics.Clear(Color.Transparent);
-            Main.spriteBatch.Begin(default, BlendState.Additive, default, default, default, default, Main.GameViewMatrix.ZoomMatrix);
+            Main.spriteBatch.Begin(default, BlendState.Additive, default, default, default, default);
 
             for (int k = 0; k < Main.maxProjectiles; k++)
             {
@@ -65,7 +66,8 @@ namespace StarlightRiver.Content.CustomHooks
             graphics.SetRenderTarget(null);
 
 
-
+            //if (Main.renderCount == 3)//
+            //{
             Main.spriteBatch.Begin(default, BlendState.Additive, SamplerState.PointWrap, default, default);
 
             Main.graphics.GraphicsDevice.SetRenderTarget(hotspringShineTarget);
@@ -73,14 +75,18 @@ namespace StarlightRiver.Content.CustomHooks
 
             Texture2D tex2 = Terraria.ModLoader.ModContent.GetTexture("StarlightRiver/Assets/Misc/HotspringWaterMap");
 
+            //The seam issue is not in this file, See StarlightRiver.cs and enable the commented out PostDrawInterface hook to view RTs
             for (int i = -tex2.Width; i <= Main.screenWidth + tex2.Width; i += tex2.Width)
                 for (int j = -tex2.Height; j <= Main.screenHeight + tex2.Height; j += tex2.Height)
                 {
-                    var pos = (new Vector2(i, j) - new Vector2(Main.screenPosition.X % tex2.Width, Main.screenPosition.Y % tex2.Height));
+                    //the divide by 1.3 and 1.5 are what keep the tile tied to the world location, seems to be tied to the 2 magic numbers in HotspringAddon.cs
+                    Vector2 pos = (new Vector2(i, j) - new Vector2((Main.screenPosition.X / 1.3f) % tex2.Width, (Main.screenPosition.Y / 1.5f) % tex2.Height));
                     Main.spriteBatch.Draw(tex2, pos, null, Color.White);
                 }
 
             Main.spriteBatch.End();
+            //}
+
             Main.graphics.GraphicsDevice.SetRenderTarget(null);
         }
     }
