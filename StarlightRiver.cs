518bd45799838d0ea4fa71cf48a517cc9d9708cf
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2021-11-23 23:42:35 -0600

    Rice improvements and debug item

--- StarlightRiver.cs
+++ StarlightRiver.cs
@@ -32,11 +32,11 @@ namespace StarlightRiver
         public bool HasLoaded;
 
         //debug hook to view RTs
-        public override void PostDrawInterface(SpriteBatch spriteBatch)
-        {
-            spriteBatch.Draw(Content.CustomHooks.HotspringMapTarget.hotspringMapTarget, new Rectangle(0, 0, Main.screenWidth, Main.screenHeight), Color.Purple * 0.5f);
-            spriteBatch.Draw(Content.CustomHooks.HotspringMapTarget.hotspringShineTarget, new Rectangle(Main.screenWidth - (Main.screenWidth / 4), 0, Main.screenWidth / 4, Main.screenHeight / 4), Color.White * 0.5f);
-        }
+        //public override void PostDrawInterface(SpriteBatch spriteBatch)
+        //{
+        //    spriteBatch.Draw(Content.CustomHooks.HotspringMapTarget.hotspringMapTarget, new Rectangle(0, 0, Main.screenWidth, Main.screenHeight), Color.Purple * 0.5f);
+        //    spriteBatch.Draw(Content.CustomHooks.HotspringMapTarget.hotspringShineTarget, new Rectangle(Main.screenWidth - (Main.screenWidth / 4), 0, Main.screenWidth / 4, Main.screenHeight / 4), Color.White * 0.5f);
+        //}
 
         public static StarlightRiver Instance { get; set; }
 
