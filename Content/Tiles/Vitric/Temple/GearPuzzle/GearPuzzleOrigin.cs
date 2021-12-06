f7ac2feb77cb0885be6f81c9bab559279222aed0
bobfishkins23 <bobfishkins23@yahoo.com>
2021-12-06 00:08:04 -0500

    Updated visuals for gear puzzle origin

--- Content/Tiles/Vitric/Temple/GearPuzzle/GearPuzzleOrigin.cs
+++ Content/Tiles/Vitric/Temple/GearPuzzle/GearPuzzleOrigin.cs
@@ -45,16 +45,24 @@ namespace StarlightRiver.Content.Tiles.Vitric.Temple.GearPuzzle
 			Size = 3;
 			Engaged = true;
 			RotationVelocity = 2;
+
+			Lighting.AddLight(projectile.Center, new Vector3(1, 0.7f, 0.4f) * 0.5f);
 		}
 
 		public override void PostDraw(SpriteBatch spriteBatch, Color lightColor)
 		{
-			Texture2D pegTex = ModContent.GetTexture(AssetDirectory.VitricTile + "GearPeg");
-			spriteBatch.Draw(pegTex, projectile.Center - Main.screenPosition, null, lightColor, 0, pegTex.Size() / 2, 1, 0, 0);
+			Texture2D bgTex = ModContent.GetTexture(AssetDirectory.VitricTile + "OriginGearBase");
+			spriteBatch.Draw(bgTex, projectile.Center - Main.screenPosition, null, lightColor, 0, new Vector2(bgTex.Width / 2, 4), 1, 0, 0);
 
 			var tex = ModContent.GetTexture(AssetDirectory.VitricTile + "OriginGear"); 
 
 			spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, null, lightColor, Rotation, tex.Size() / 2, 1, 0, 0);
+
+			var magmiteTex = ModContent.GetTexture(AssetDirectory.VitricNpc + "MagmitePassive");
+			var sinTimer = Main.GameUpdateCount / 20f;
+			var frame = new Rectangle(42, sinTimer % 6.28f < 1.57f ? 0 : (int)(Main.GameUpdateCount / 3f) % 5 * 40, 42, 40);
+
+			spriteBatch.Draw(magmiteTex, projectile.Center - Main.screenPosition, frame, Color.White, (float)Math.Sin(sinTimer), new Vector2(21, 0), 1, SpriteEffects.FlipHorizontally, 0);
 		}
 	}
 
