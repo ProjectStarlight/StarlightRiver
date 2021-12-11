f3ae774f226f33110e877d59364409c08a46c2aa
TripleFate <snowxdragon@gmail.com>
2021-12-10 23:28:14 -0800

    fix for players falling through fast upwards moving platforms in mp

--- Content/NPCs/BaseTypes/MovingPlatform.cs
+++ Content/NPCs/BaseTypes/MovingPlatform.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StarlightRiver.Core;
+using System;
 using System.IO;
 using System.Linq;
 using Terraria;
@@ -48,9 +51,42 @@ namespace StarlightRiver.Content.NPCs.BaseTypes
             SafeReceiveExtraAI();
         }
 
+        Vector2 prevPos;
+
         public sealed override void AI()
         {
             SafeAI();
+
+
+            float yDistTraveled = npc.position.Y - prevPos.Y;
+
+            if (npc.velocity != Vector2.Zero && Math.Abs(npc.velocity.Y) > 1f && (npc.velocity.Y < 0 && yDistTraveled < npc.velocity.Y * 1.5 && yDistTraveled > npc.velocity.Y * 6))
+            {
+                //this loop outside of the normal moving platform loop in Mechanics is mainly for multiplayer with some potential for extreme lag situations on fast platforms
+                //what is happening is that when terraria skips frames (or lags in mp) they add the npc velocity multiplied by the skipped frames up to 5x a normal frame until caught up, but only run the ai once
+                //so we can end up with frames where the platform skips 5x its normal velocity likely clipping through players since the platform is thin.
+                //to solve this, the collision code takes into account the previous platform position accessed by this AI for the hitbox to cover the whole travel from previous fully processed frame.
+                //only handling big upwards y movements since the horizontal skips don't seem as jarring to the user since platforms tend to be wide, and vertical down skips aren't jarring since player drops onto platform anyway instead of clipping through.
+                foreach (Player player in Main.player)
+                {
+                    if (!player.active || player.dead || player.GoingDownWithGrapple || player.GetModPlayer<StarlightPlayer>().platformTimer > 0)
+                        continue;
+
+                    Rectangle playerRect = new Rectangle((int)player.position.X, (int)player.position.Y + (player.height), player.width, 1);
+                    Rectangle npcRect = new Rectangle((int)npc.position.X, (int)npc.position.Y, npc.width, 8 + (player.velocity.Y > 0 ? (int)player.velocity.Y : 0) + (int)Math.Abs(yDistTraveled));
+
+                    if (playerRect.Intersects(npcRect) && player.position.Y <= npc.position.Y)
+                    {
+                        if (!player.justJumped && player.velocity.Y >= 0)
+                        {
+                            player.velocity.Y = 0;
+                            player.position.Y = npc.position.Y - player.height + 4;
+                            player.position += npc.velocity;
+                        }
+                    }
+                }
+            }
+            
 
             for (int k = 0; k < Main.maxProjectiles; k++)
 			{
@@ -65,6 +101,8 @@ namespace StarlightRiver.Content.NPCs.BaseTypes
                     proj.netUpdate = true;
                 }
 			}
+
+            prevPos = npc.position;
         }
     }
 }
