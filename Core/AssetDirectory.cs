184b17f6df73df55a223da54a39d271eca686a26
GraydeeSi <wiseguy0002@gmail.com>
2022-03-01 15:04:44 -0500

    Geomancer assets

--- Core/AssetDirectory.cs
+++ Core/AssetDirectory.cs
@@ -34,7 +34,8 @@ namespace StarlightRiver.Core
         public const string CaveTempleItem =        Assets + "Items/UndergroundTemple/";
         public const string DungeonItem =           Assets + "Items/Dungeon/";
         public const string GravediggerItem =       Assets + "Items/Gravedigger/";
-        public const string DemonItem =       Assets + "Items/Demon/";
+        public const string DemonItem =             Assets + "Items/Demon/";
+        public const string GeomancerItem =         Assets + "Items/Geomancer/";
 
         public const string SpaceEventItem =        Assets + "Items/SpaceEvent/";
         public const string SpaceEventNPC =         Assets + "NPCs/SpaceEvent/";
