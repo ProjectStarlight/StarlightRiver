cf962de467ee2b68f488a35d737d8cdead64d4b1
GraydeeSi <wiseguy0002@gmail.com>
2022-03-06 12:04:08 -0500

    Geomancer armor - Funny descriptions of gem effects

--- Content/Items/Geomancer/Armors.GeomancerArmor.cs
+++ Content/Items/Geomancer/Armors.GeomancerArmor.cs
@@ -25,6 +25,14 @@ namespace StarlightRiver.Content.Items.Geomancer
     {
         public override string Texture => AssetDirectory.GeomancerItem + Name;
 
+        internal static Item dummyItem = new Item();
+
+        public override bool Autoload(ref string name)
+        {
+            On.Terraria.Main.MouseText_DrawItemTooltip += SpoofMouseItem;
+            return true;
+        }
+
         public override void SetStaticDefaults()
         {
             DisplayName.SetDefault("Geomancer hood");
@@ -44,6 +52,35 @@ namespace StarlightRiver.Content.Items.Geomancer
 		{
             player.GetModPlayer<CritMultiPlayer>().RangedCritMult += 0.15f;
 		}*/
+
+        private void SpoofMouseItem(On.Terraria.Main.orig_MouseText_DrawItemTooltip orig, Main self, int rare, byte diff, int X, int Y)
+        {
+            var player = Main.LocalPlayer;
+
+            if (dummyItem.IsAir)
+                dummyItem.SetDefaults(ModContent.ItemType<GeomancerItemDummy>());
+
+            if (IsGeomancerArmor(Main.HoverItem) && IsArmorSet(player) && player.controlUp)
+            {
+                Main.HoverItem = dummyItem.Clone();
+                Main.hoverItemName = dummyItem.Name;
+            }
+
+            orig(self, rare, diff, X, Y);
+        }
+
+        public bool IsGeomancerArmor(Item item)
+        {
+            return item.type == ModContent.ItemType<GeomancerHood>() ||
+                item.type == ModContent.ItemType<GeomancerRobe>() ||
+                item.type == ModContent.ItemType<GeomancerPants>();
+        }
+
+        public bool IsArmorSet(Player player)
+        {
+            return player.armor[0].type == ModContent.ItemType<GeomancerHood>() && player.armor[1].type == ModContent.ItemType<GeomancerRobe>() && player.armor[2].type == ModContent.ItemType<GeomancerPants>();
+        }
+
     }
 
     [AutoloadEquip(EquipType.Body)]
@@ -105,5 +142,26 @@ namespace StarlightRiver.Content.Items.Geomancer
         {
             player.GetModPlayer<CritMultiPlayer>().RangedCritMult += Math.Min(0.2f, player.velocity.Length() / 16f * 0.2f);
         }*/
+    }
+    public class GeomancerItemDummy : ModItem
+    {
+        public override string Texture => AssetDirectory.GeomancerItem + "GeoDiamond";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Effects of different gems:");
+            Tooltip.SetDefault(
+            "Diamond does a funny \n" +
+            "Topaz does a funny \n" + 
+            "Emerald does a funny \n" +
+            "Sapphire does a funny \n" + 
+            "Amethyst does a funny \n" + 
+            "Ruby does a funny");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 20;
+        }
     }
 }
