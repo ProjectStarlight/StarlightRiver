e2742d33fd1c6d424ee2b1eb4460e1e2b420d66e
bobfishkins23 <bobfishkins23@yahoo.com>
2022-04-02 16:26:07 -0400

    Old codex changes that never got pushed

--- Content/Codex/Entries/RelicEntries.cs
+++ Content/Codex/Entries/RelicEntries.cs
@@ -9,7 +9,8 @@ namespace StarlightRiver.Codex.Entries
         {
             Category = Categories.Relics;
             Title = "Placeholder Entry";
-            Body = "Huh. Well this is awkward."; 
+            Body = "Huh. Well this is awkward.";
+            Hint = "Some things you just cant know in a demo...";
             Image = GetTexture("StarlightRiver/Assets/Codex/AbilityImageLore");
             Icon = GetTexture("StarlightRiver/Assets/Codex/StarlightWaterIcon");
         }
