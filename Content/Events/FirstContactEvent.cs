6d6e8243a60494a2262996c6069d12c1fd676dc4
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2022-04-06 22:28:18 -0500

    first contact mod system

--- Content/Events/FirstContactEvent.cs
+++ Content/Events/FirstContactEvent.cs
@@ -1,5 +1,6 @@
 using Microsoft.Xna.Framework;
 using StarlightRiver.Codex.Entries;
+using StarlightRiver.Content.CustomHooks;
 using StarlightRiver.Core;
 using StarlightRiver.Helpers;
 using System;
@@ -28,5 +29,20 @@ namespace StarlightRiver.Content.Biomes
         {
 			return spaceEventActive;//this is here for now to stick with the old style
 		}
+    }
+
+    public class FirstContactSystem : ModSystem
+    {
+        public override void ModifySunLightColor(ref Color tileColor, ref Color backgroundColor)
+        {
+            TargetHost.Maps?.OrderedShaderPass();
+            TargetHost.Maps?.OrderedRenderPassBatched(Main.spriteBatch, Main.graphics.GraphicsDevice);
+
+            if (FirstContactEvent.spaceEventFade > 0) //PORTTODO: Move this to a ModSceneEffect
+            {
+                tileColor = Color.Lerp(Color.White, new Color(70, 60, 120), FirstContactEvent.spaceEventFade);
+                backgroundColor = Color.Lerp(Color.White, new Color(17, 15, 30), FirstContactEvent.spaceEventFade);
+            }
+        }
     }
 }
--- Core/BiomeHandler.cs
+++ Core/BiomeHandler.cs
@@ -37,16 +37,5 @@ namespace StarlightRiver
         {
             return (int)Math.Floor(((Main.screenPosition.Y + (Main.screenHeight * (1f / Core.ZoomHandler.ClampedExtraZoomTarget))) / 16f) + 2 - (((Main.screenPosition.Y + Main.screenHeight) / 16f) + 2));
         }
-        public override void ModifySunLightColor(ref Color tileColor, ref Color backgroundColor) //PORTTODO: Figure out what the heck these expansion things are for? Were they for zoom?
-        {
-            TargetHost.Maps?.OrderedShaderPass();
-            TargetHost.Maps?.OrderedRenderPassBatched(Main.spriteBatch, Main.graphics.GraphicsDevice);
-
-            if(FirstContactEvent.spaceEventFade > 0) //PORTTODO: Move this to a ModSceneEffect
-			{
-                tileColor = Color.Lerp(Color.White, new Color(70, 60, 120), FirstContactEvent.spaceEventFade);
-                backgroundColor = Color.Lerp(Color.White, new Color(17, 15, 30), FirstContactEvent.spaceEventFade);
-            }
-        }
     }
 }
