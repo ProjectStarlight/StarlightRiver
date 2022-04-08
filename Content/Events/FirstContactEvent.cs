3fce1532fa050a781e8b50acc55f5c6af4bed803
bobfishkins23 <bobfishkins23@yahoo.com>
2022-04-07 21:17:45 -0400

    Ported more of first contact

--- Content/Events/FirstContactEvent.cs
+++ Content/Events/FirstContactEvent.cs
@@ -16,32 +16,41 @@ namespace StarlightRiver.Content.Biomes
 {
 	public class FirstContactEvent : ModSceneEffect
 	{
-		//moved here from starlight world
-		//TODO: figure out what do to with these
-		public static bool spaceEventActive = false;
-		public static float spaceEventFade = 0;
-
 		public override int Music => MusicLoader.GetMusicSlot("Sounds/WhipAndNaenae");
 
 		public override SceneEffectPriority Priority => SceneEffectPriority.Event;
 
         public override bool IsSceneEffectActive(Player player)
         {
-			return spaceEventActive;//this is here for now to stick with the old style
+			return FirstContactSystem.FirstContactActive;//this is here for now to stick with the old style
 		}
     }
 
     public class FirstContactSystem : ModSystem
     {
-        public override void ModifySunLightColor(ref Color tileColor, ref Color backgroundColor)
+        public static bool FirstContactActive = false;
+        public static float FirstContactFade = 0;
+
+        public override void PostUpdateEverything()
+		{
+            if (FirstContactActive)
+            {
+                if (FirstContactFade <= 1)
+                    FirstContactFade += 0.01f;
+            }
+            else if (FirstContactFade > 0)
+                FirstContactFade -= 0.01f;
+        }
+
+		public override void ModifySunLightColor(ref Color tileColor, ref Color backgroundColor)
         {
             TargetHost.Maps?.OrderedShaderPass();
             TargetHost.Maps?.OrderedRenderPassBatched(Main.spriteBatch, Main.graphics.GraphicsDevice);
 
-            if (FirstContactEvent.spaceEventFade > 0) //PORTTODO: Move this to a ModSceneEffect
+            if (FirstContactFade > 0) //PORTTODO: Move this to a ModSceneEffect
             {
-                tileColor = Color.Lerp(Color.White, new Color(70, 60, 120), FirstContactEvent.spaceEventFade);
-                backgroundColor = Color.Lerp(Color.White, new Color(17, 15, 30), FirstContactEvent.spaceEventFade);
+                tileColor = Color.Lerp(Color.White, new Color(70, 60, 120), FirstContactFade);
+                backgroundColor = Color.Lerp(Color.White, new Color(17, 15, 30), FirstContactFade);
             }
         }
     }
