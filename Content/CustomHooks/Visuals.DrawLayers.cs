cbfd259e00e62d1170e9dacdf880b767ee3cad20
bobfishkins23 <bobfishkins23@yahoo.com>
2022-04-10 01:46:53 -0400

    Fixed player drawing

--- Content/CustomHooks/Visuals.DrawLayers.cs
+++ Content/CustomHooks/Visuals.DrawLayers.cs
@@ -26,11 +26,39 @@ namespace StarlightRiver.Content.CustomHooks
             On.Terraria.Main.DrawItems += DrawKeys;
             //Tile draws infront of the Player
             On.Terraria.Graphics.Renderers.LegacyPlayerRenderer.DrawPlayer += PostDrawPlayer;
+            On.Terraria.Graphics.Renderers.ReturnGatePlayerRenderer.DrawPlayer += PostDrawPlayerGate;
+
+            On.Terraria.DataStructures.PlayerDrawLayers.DrawPlayer_RenderAllLayers += PostDrawPlayerLayer;
+        }
+
+		private void PostDrawPlayerLayer(On.Terraria.DataStructures.PlayerDrawLayers.orig_DrawPlayer_RenderAllLayers orig, ref Terraria.DataStructures.PlayerDrawSet drawinfo)
+		{
+            var drawPlayer = drawinfo.drawPlayer;
+            var shadow = drawinfo.shadow;
+
+            if (!Main.gameMenu && shadow == 0) 
+                drawPlayer.GetModPlayer<StarlightPlayer>().PreDraw(drawPlayer, Main.spriteBatch);
+
+            orig(ref drawinfo);
+
+            if (!Main.gameMenu && shadow == 0)
+                drawPlayer.GetModPlayer<StarlightPlayer>().PostDraw(drawPlayer, Main.spriteBatch);
         }
 
 		private void PostDrawPlayer(On.Terraria.Graphics.Renderers.LegacyPlayerRenderer.orig_DrawPlayer orig, Terraria.Graphics.Renderers.LegacyPlayerRenderer self, Camera camera, Player drawPlayer, Vector2 position, float rotation, Vector2 rotationOrigin, float shadow, float scale)
 		{
-            if (!Main.gameMenu && shadow == 0) //PORTTODO: Maybe pass through more of these params to these methods?
+            if (!Main.gameMenu && shadow == 0) 
+                drawPlayer.GetModPlayer<StarlightPlayer>().PreDraw(drawPlayer, Main.spriteBatch);
+
+            orig(self, camera, drawPlayer, position, rotation, rotationOrigin, shadow, scale);
+
+            if (!Main.gameMenu && shadow == 0)
+                drawPlayer.GetModPlayer<StarlightPlayer>().PostDraw(drawPlayer, Main.spriteBatch);
+        }
+
+        private void PostDrawPlayerGate(On.Terraria.Graphics.Renderers.ReturnGatePlayerRenderer.orig_DrawPlayer orig, object self, Camera camera, Player drawPlayer, Vector2 position, float rotation, Vector2 rotationOrigin, float shadow, float scale)
+        {
+            if (!Main.gameMenu && shadow == 0)
                 drawPlayer.GetModPlayer<StarlightPlayer>().PreDraw(drawPlayer, Main.spriteBatch);
 
             orig(self, camera, drawPlayer, position, rotation, rotationOrigin, shadow, scale);
