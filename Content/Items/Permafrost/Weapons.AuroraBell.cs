66464c48219b05709439f280ba3a0168d324f93e
GraydeeSi <wiseguy0002@gmail.com>
2022-04-11 13:09:22 -0400

    Aurora bell progress

--- Content/Items/Permafrost/Weapons.AuroraBell.cs
+++ Content/Items/Permafrost/Weapons.AuroraBell.cs
@@ -2,13 +2,16 @@
 using Microsoft.Xna.Framework.Graphics;
 
 using StarlightRiver.Core;
+using StarlightRiver.Helpers;
 
 using System;
+using System.Collections.Generic;
 
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Graphics.Effects;
 
 namespace StarlightRiver.Content.Items.Permafrost
 {
@@ -105,16 +108,20 @@ namespace StarlightRiver.Content.Items.Permafrost
 			for (int i = 0; i < Main.projectile.Length; i++)
             {
 				Projectile proj = Main.projectile[i];
-				if (proj == null || !proj.active || !ProjectileID.Sets.IsAWhip[proj.type])
+				if (proj == null || !proj.active || !(ProjectileID.Sets.IsAWhip[proj.type] || (proj.type == ModContent.ProjectileType<AuroraBellRing>())))
 					continue;
 
 				ModProjectile modProj = proj.ModProjectile;
+                if (modProj is AuroraBellRing ringer && ringer.cantHit.Contains(Projectile))
+                    continue;
 
 				bool colliding = false;
-				if (modProj != null && modProj.Colliding(proj.Hitbox, Projectile.Hitbox) == true)
+				if (modProj != null && modProj.Colliding(proj.Hitbox, Projectile.Hitbox) != false)
 				{
 					if (modProj.Colliding(proj.Hitbox, Projectile.Hitbox) == null && Projectile.Colliding(proj.Hitbox, Projectile.Hitbox))
 						colliding = true;
+                    if (modProj.Colliding(proj.Hitbox, Projectile.Hitbox) == true)
+                        colliding = true;
 				}
 
 				else
@@ -134,13 +141,140 @@ namespace StarlightRiver.Content.Items.Permafrost
 					}
 				}
 
-				if (colliding)
+				if (colliding) 
 				{
-					Main.NewText("Collision");
+                    Vector2 offset = 8 * new Vector2((float)Math.Cos(counter * 0.05f), (float)Math.Sin(counter * 0.05f));
+                    Projectile newProj = Projectile.NewProjectileDirect(Projectile.GetProjectileSource_FromThis(), Projectile.Center + offset, Vector2.Zero, ModContent.ProjectileType<AuroraBellRing>(), Projectile.damage, Projectile.knockBack, owner.whoAmI);
+					newProj.originalDamage = Projectile.damage;
+
+                    if (modProj is AuroraBellRing oldRinger)
+                    {
+                        var newProjMP = newProj.ModProjectile as AuroraBellRing;
+                        oldRinger.cantHit.Add(Projectile);
+                        newProjMP.cantHit = oldRinger.cantHit;
+                    }
 					chargeCounter = 0;
 					break;
 				}
             }
+        }
+    }
+    internal class AuroraBellRing : ModProjectile, IDrawPrimitive
+    {
+        public override string Texture => AssetDirectory.Assets + "Invisible";
+
+        private List<Vector2> cache;
+
+        private Trail trail;
+        private Trail trail2;
+
+        private float Progress => 1 - (Projectile.timeLeft / 16f);
+
+        private float Radius => 180 * (float)Math.Sqrt(Math.Sqrt(Progress));
+
+        public List<Projectile> cantHit = new List<Projectile>();
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 80;
+            Projectile.height = 80;
+            Projectile.DamageType = DamageClass.Summon;
+            Projectile.friendly = true;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+            Projectile.timeLeft = 16;
+        }
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Aurora Bell");
+        }
+
+        public override void AI()
+        {
+            if (Main.netMode != NetmodeID.Server)
+            {
+                ManageCaches();
+                ManageTrail();
+            }
+        }
+
+        public override bool PreDraw(ref Color lightColor) => false;
+
+        public override bool? CanHitNPC(NPC target)
+        {
+            if (target.whoAmI == (int)Projectile.ai[0])
+                return false;
+            return base.CanHitNPC(target);
+        }
+
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            Vector2 line = targetHitbox.Center.ToVector2() - Projectile.Center;
+            line.Normalize();
+            line *= Radius;
+            if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Projectile.Center, Projectile.Center + line))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void ManageCaches()
+        {
+            cache = new List<Vector2>();
+            float radius = Radius;
+            for (int i = 0; i < 33; i++) //TODO: Cache offsets, to improve performance
+            {
+                double rad = (i / 32f) * 6.28f;
+                Vector2 offset = new Vector2((float)Math.Sin(rad), (float)Math.Cos(rad));
+                offset *= radius;
+                cache.Add(Projectile.Center + offset);
+            }
+
+            while (cache.Count > 33)
+            {
+                cache.RemoveAt(0);
+            }
+        }
+
+        private void ManageTrail()
+        {
+
+            trail = trail ?? new Trail(Main.instance.GraphicsDevice, 33, new TriangularTip(1), factor => 38 * (1 - Progress), factor =>
+            {
+                return Color.Magenta;
+            });
+
+            trail2 = trail2 ?? new Trail(Main.instance.GraphicsDevice, 33, new TriangularTip(1), factor => 20 * (1 - Progress), factor =>
+            {
+                return Color.White;
+            });
+            float nextplace = 33f / 32f;
+            Vector2 offset = new Vector2((float)Math.Sin(nextplace), (float)Math.Cos(nextplace));
+            offset *= Radius;
+
+            trail.Positions = cache.ToArray();
+            trail.NextPosition = Projectile.Center + offset;
+
+            trail2.Positions = cache.ToArray();
+            trail2.NextPosition = Projectile.Center + offset;
+        }
+
+        public void DrawPrimitives()
+        {
+            Effect effect = Filters.Scene["OrbitalStrikeTrail"].GetShader().Shader;
+
+            Matrix world = Matrix.CreateTranslation(-Main.screenPosition.Vec3());
+            Matrix view = Main.GameViewMatrix.ZoomMatrix;
+            Matrix projection = Matrix.CreateOrthographicOffCenter(0, Main.screenWidth, Main.screenHeight, 0, -1, 1);
+
+            effect.Parameters["transformMatrix"].SetValue(world * view * projection);
+            effect.Parameters["sampleTexture"].SetValue(ModContent.Request<Texture2D>("StarlightRiver/Assets/GlowTrail").Value);
+            effect.Parameters["alpha"].SetValue(1);
+
+            trail?.Render(effect);
+            trail2?.Render(effect);
         }
     }
 }
