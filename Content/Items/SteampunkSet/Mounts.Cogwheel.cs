6a64f55096e3dc97f9d261a70a1cc11604475d95
GraydeeSi <wiseguy0002@gmail.com>
2022-05-08 12:35:33 -0400

    Now immune to spikes on cogwheel

--- Content/Items/SteampunkSet/Mounts.Cogwheel.cs
+++ Content/Items/SteampunkSet/Mounts.Cogwheel.cs
@@ -168,6 +168,13 @@ namespace StarlightRiver.Content.Items.SteampunkSet
 			mounted = false;
         }
 
+        public override bool PreHurt(bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource)
+        {
+			if (damageSource.SourceOtherIndex == 3 && mounted)
+				return false;
+			return base.PreHurt(pvp, quiet, ref damage, ref hitDirection, ref crit, ref customDamage, ref playSound, ref genGore, ref damageSource);
+        }
+
         public override void PostUpdate()
         {
 			if (!mounted)
