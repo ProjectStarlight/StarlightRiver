a2b17c13f691c7038be0d3e3be267914047d22ab
GraydeeSi <wiseguy0002@gmail.com>
2022-05-20 11:06:15 -0400

    Basically finished slitherring

--- Content/Items/Misc/Accessories.Slitherring.cs
+++ Content/Items/Misc/Accessories.Slitherring.cs
@@ -4,26 +4,34 @@ using StarlightRiver.Content.WorldGeneration;
 using StarlightRiver.Core;
 using StarlightRiver.NPCs;
 using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.Audio;
 using Terraria.GameContent.ItemDropRules;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria.DataStructures;
+using Terraria.GameContent;
+using ReLogic.Content;
 
 namespace StarlightRiver.Content.Items.Misc
 {
-    public class Slitherring : SmartAccessory
+    public class Slitherring : SmartAccessory, IChestItem
     {
         public override string Texture => AssetDirectory.MiscItem + Name;
+
+        public int Stack => 1;
+
+        public ChestRegionFlags Regions => ChestRegionFlags.Jungle;
 
         public Slitherring() : base("Slitherring", "Whips have a chance to shoot out a smaller, snake whip") { }
 
         public override void SafeSetDefaults()
         {
-            Item.value = 1;
-            Item.rare = ItemRarityID.LightRed;
+            Item.value = Item.sellPrice(0,0,40,0);
+            Item.rare = ItemRarityID.Orange;
         }
 
         public override void SafeUpdateEquip(Player Player)
@@ -51,7 +59,7 @@ namespace StarlightRiver.Content.Items.Misc
             {
                 Projectile proj = Projectile.NewProjectileDirect(source, position, velocity * 0.75f, ModContent.ProjectileType<SlitherringWhip>(), (int)(damage * 0.5f), knockback, player.whoAmI, -1);
                 proj.originalDamage = damage / 2;
-            }    
+            }
             return base.Shoot(item, player, source, position, velocity, type, damage, knockback);
         }
     }
@@ -88,9 +96,21 @@ namespace StarlightRiver.Content.Items.Misc
 
         public override bool ShouldDrawSegment(int segment) => true;// segment % 2 == 0;
 
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            List<Vector2> points = Projectile.WhipPointsForCollision;
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                float collisionPoint = 0f;
+                if (Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), points[i], points[i + 1], 8, ref collisionPoint))
+                    return true;
+            }
+            return false;
+        }
+
         public override bool PreAI()
         {
-
+            ProjectileID.Sets.IsAWhip[Projectile.type] = false;
             Player player = Main.player[Projectile.owner];
             _flyTime = player.itemAnimationMax * Projectile.MaxUpdates * 2;
             if (Projectile.ai[0] == -1)
@@ -105,18 +125,67 @@ namespace StarlightRiver.Content.Items.Misc
                 Projectile.Kill();
                 return false;
             }
-
-            if (Projectile.ai[0] == (int)(_flyTime / 2f))
-            {
-                Projectile.WhipPointsForCollision.Clear();
-                SetPoints(Projectile.WhipPointsForCollision);
-            }
-            if (Utils.GetLerpValue(0.1f, 0.7f, Projectile.ai[0] / _flyTime, true) * Utils.GetLerpValue(0.9f, 0.7f, Projectile.ai[0] / _flyTime, true) > 0.5f)
-            {
-                Projectile.WhipPointsForCollision.Clear();
-                SetPoints(Projectile.WhipPointsForCollision);
-            }
+            Projectile.WhipPointsForCollision.Clear();
+            SetPoints(Projectile.WhipPointsForCollision);
             ArcAI();
+            return false;
+        }
+
+        public override void PostAI()
+        {
+
+            base.PostAI();
+        }
+
+        public override bool PreDraw(ref Color lightColor)
+        {
+            DrawBehindWhip(ref lightColor);
+            List<Vector2> points = new List<Vector2>();
+            points.Clear();
+            SetPoints(points);
+
+            //points = Projectile.WhipPointsForCollision;
+
+            //string
+            Vector2 stringPoint = points[0];
+            for (int i = 0; i < points.Count - 2; i++)
+            {
+                Vector2 nextPoint = points[i + 1] - points[i];
+                Color color = _stringColor.MultiplyRGBA(Projectile.GetAlpha(Lighting.GetColor(points[i].ToTileCoordinates())));
+                Vector2 scale = new Vector2(1f, (nextPoint.Length() + 2f) / (float)TextureAssets.FishingLine.Height());
+                Main.EntitySpriteDraw(TextureAssets.FishingLine.Value, stringPoint - Main.screenPosition, null, color, nextPoint.ToRotation() - MathHelper.PiOver2, new Vector2(TextureAssets.FishingLine.Width() * 0.5f, 2f), scale, SpriteEffects.None, 0);
+                stringPoint += nextPoint;
+            }
+
+            //whip
+            Asset<Texture2D> texture = ModContent.Request<Texture2D>(Texture);
+            Rectangle whipFrame = texture.Frame(1, 5, 0, 0);
+            int height = whipFrame.Height;
+            Vector2 firstPoint = points[0];
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                Vector2 origin = whipFrame.Size() * 0.5f;
+                bool draw = true;
+                if (i == 0)
+                    origin.Y += _handleOffset;
+                else if (i == points.Count - 2)
+                    whipFrame.Y = height * 4;
+                else
+                {
+                    whipFrame.Y = height * SegmentVariant(i);
+                    draw = ShouldDrawSegment(i);
+                }
+
+                Vector2 difference = points[i + 1] - points[i];
+                if (draw)
+                {
+                    Color alpha = Projectile.GetAlpha(Lighting.GetColor(points[i].ToTileCoordinates()));
+                    float rotation = difference.ToRotation() - MathHelper.PiOver2;
+                    Main.EntitySpriteDraw(texture.Value, points[i] - Main.screenPosition, whipFrame, alpha, rotation, origin, Projectile.scale, SpriteEffects.None, 0);
+                }
+                firstPoint += difference;
+            }
+
             return false;
         }
 
