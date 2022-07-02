09e2c811af66807b027c77ab4d971d146774657b
bobfishkins23 <bobfishkins23@yahoo.com>
2022-07-01 22:10:08 -0400

    Added basic functionality for wolf mount

---
+++ Content/Items/Forest/FeralWolfMount.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.Xna.Framework;
+using StarlightRiver.Core;
+using StarlightRiver.Core.Systems.CombatMountSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Content.Items.Forest
+{
+	internal class FeralWolfMount : CombatMount
+	{
+		public override string PrimaryIconTexture => "StarlightRiver/Assets/Items/Example/RocketAbility";
+		public override string SecondaryIconTexture => "StarlightRiver/Assets/Items/Example/RingAbility";
+
+		public override void SetDefaults()
+		{
+			primarySpeedCoefficient = 20;
+			primaryCooldownCoefficient = 20;
+			secondaryCooldownCoefficient = 600;
+			secondarySpeedCoefficient = 30;
+			damageCoefficient = 16;
+			autoReuse = false;
+		}
+
+		public override void UpdatePhysics(Player player)
+		{
+
+		}
+
+		public override void OnStartPrimaryAction(Player player)
+		{
+			int i = Projectile.NewProjectile(player.GetSource_FromThis(), player.Center, Vector2.Zero, ModContent.ProjectileType<FeralWolfMountBite>(), damageCoefficient, 0, player.whoAmI, MaxPrimaryTime);
+			Main.projectile[i].rotation = player.direction == 1 ? 0 : 3.14f;
+		}
+
+		public override void PrimaryAction(int timer, Player player)
+		{
+
+		}
+
+		public override void OnStartSecondaryAction(Player player)
+		{
+			foreach (Projectile proj in Main.projectile.Where(n => n.active && n.owner == player.whoAmI && n.minion))
+			{
+				proj.extraUpdates++;
+			}
+		}
+
+		public override void SecondaryAction(int timer, Player player)
+		{
+			if (timer == 1)
+			{
+				foreach (Projectile proj in Main.projectile.Where(n => n.active && n.owner == player.whoAmI && n.minion))
+				{
+					proj.extraUpdates--;
+				}
+			}
+		}
+	}
+
+	internal class FeralWolfMountData : ModMount
+	{
+		public override string Texture => "StarlightRiver/Assets/Items/Example/ExampleCombatMount";
+
+		public override void SetStaticDefaults()
+		{
+			MountData.jumpHeight = 5;
+			MountData.acceleration = 0.14f;
+			MountData.jumpSpeed = 8f; 
+			MountData.blockExtraJumps = false; 
+			MountData.heightBoost = 20; 
+			MountData.runSpeed = 8f; 
+
+			// Frame data and player offsets
+			MountData.totalFrames = 1;
+			MountData.playerYOffsets = Enumerable.Repeat(20, MountData.totalFrames).ToArray();
+			MountData.xOffset = 13;
+			MountData.yOffset = -12;
+			MountData.playerHeadOffset = 22;
+			MountData.bodyFrame = 3;
+			// Standing
+			MountData.standingFrameCount = 1;
+			MountData.standingFrameDelay = 12;
+			MountData.standingFrameStart = 0;
+			// Running
+			MountData.runningFrameCount = 1;
+			MountData.runningFrameDelay = 12;
+			MountData.runningFrameStart = 0;
+			// Flying
+			MountData.flyingFrameCount = 0;
+			MountData.flyingFrameDelay = 0;
+			MountData.flyingFrameStart = 0;
+			// In-air
+			MountData.inAirFrameCount = 1;
+			MountData.inAirFrameDelay = 12;
+			MountData.inAirFrameStart = 0;
+			// Idle
+			MountData.idleFrameCount = 1;
+			MountData.idleFrameDelay = 12;
+			MountData.idleFrameStart = 0;
+			MountData.idleFrameLoop = true;
+			// Swim
+			MountData.swimFrameCount = MountData.inAirFrameCount;
+			MountData.swimFrameDelay = MountData.inAirFrameDelay;
+			MountData.swimFrameStart = MountData.inAirFrameStart;
+
+			if (!Main.dedServ)
+			{
+				MountData.textureWidth = MountData.backTexture.Width() + 20;
+				MountData.textureHeight = MountData.backTexture.Height();
+			}
+		}
+	}
+
+	internal class FeralWolfMountItem : CombatMountItem
+	{
+		public override int MountType => ModContent.MountType<FeralWolfMountData>();
+
+		public override Type CombatMountType => typeof(FeralWolfMount);
+
+		public override string Texture => AssetDirectory.Debug;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Juicy Steak");
+			Tooltip.SetDefault("Summons a feral wolf combat mount\nInflicts bleeding with powerful bites\nRight click to howl and boost the speed of minions");
+		}
+	}
+}
---
+++ Content/Items/Forest/FeralWolfMountBite.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StarlightRiver.Core;
+using StarlightRiver.Core.Systems.CombatMountSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Content.Items.Forest
+{
+	internal class FeralWolfMountBite : ModProjectile
+	{
+		public float Progress => 1 - (Projectile.timeLeft / Projectile.ai[0]);
+
+		public override string Texture => AssetDirectory.Invisible;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Crushing Jaws");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.timeLeft = (int)Projectile.ai[0];
+			Projectile.DamageType = DamageClass.Summon;
+			Projectile.friendly = true;
+		}
+
+		public override void AI()
+		{
+			var owner = Main.player[Projectile.owner];
+			Projectile.Center = owner.MountedCenter + Vector2.UnitX.RotatedBy(Projectile.rotation) * 50;
+		}
+
+		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+		{
+			return Helpers.Helper.CheckConicalCollision(Projectile.Center, 50, Projectile.rotation, 1.57f, targetHitbox);
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			var topTex = ModContent.Request<Texture2D>(AssetDirectory.ForestItem + "FeralWolfMountBiteTop").Value;
+			var botTex = ModContent.Request<Texture2D>(AssetDirectory.ForestItem + "FeralWolfMountBiteBot").Value;
+			var glowTex = ModContent.Request<Texture2D>("StarlightRiver/Assets/Misc/TriTell").Value;
+
+			var glowColor = new Color(255, 100, 100) * (float)Math.Sin(Progress * 3.14f);
+			glowColor.A = 0;
+
+			Main.EntitySpriteDraw(glowTex, Projectile.Center - Main.screenPosition, null, glowColor, Projectile.rotation + 1.57f / 2, new Vector2(0, glowTex.Height), 1, 0, 0);
+
+			float rotOff = 0.5f - Helpers.Helper.SwoopEase(Progress) * 0.5f;
+
+			Main.EntitySpriteDraw(topTex, Projectile.Center - Main.screenPosition, null, glowColor, Projectile.rotation + 1.57f / 2 - rotOff, Vector2.Zero, 1, 0, 0);
+			Main.EntitySpriteDraw(botTex, Projectile.Center - Main.screenPosition, null, glowColor, Projectile.rotation + 1.57f / 2 + rotOff, new Vector2(0, botTex.Height), 1, 0, 0);
+
+			return false;
+		}
+	}
+}
