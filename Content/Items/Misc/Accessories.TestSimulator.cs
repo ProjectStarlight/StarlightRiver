b08b69753584b810c734303763fc7fe9d1d680a5
bobfishkins23 <bobfishkins23@yahoo.com>
2022-07-16 14:11:39 -0400

    Updated examples

--- Content/Items/Misc/Accessories.TestSimulator.cs
+++ Content/Items/Misc/Accessories.TestSimulator.cs
@@ -18,7 +18,7 @@ namespace StarlightRiver.Content.Items.Misc
             ModContent.ItemType<Guillotine>()
         };
 
-		public TestSimulator() : base("Testificate Charm", "debug item\nCombines the effects of some accessories\nAdditionally, boosts life by 100") { }
+		public TestSimulator() : base("Testificate Charm", "debug item\nCombines the effects of Mantis Technique and Golden Guillotine\nAdditionally, boosts life by 100") { }
 
         public override void SafeSetDefaults()
         {
@@ -36,6 +36,34 @@ namespace StarlightRiver.Content.Items.Misc
         }
     }
 
+    public class TestSimulator3 : SmartAccessory
+    {
+        public override string Texture => AssetDirectory.MiscItem + "StarGlaive";
+
+        public override List<int> ChildTypes => new List<int>()
+        {
+            ModContent.ItemType<SwordBook>(),
+            ModContent.ItemType<StaminaUp>()
+        };
+
+        public TestSimulator3() : base("Testificate Charm 3", "debug item\nCombines the effects of Mantis Technique and Stamina Vessel\nAdditionally, boosts life by 100") { }
+
+        public override void SafeSetDefaults()
+        {
+            Item.rare = ItemRarityID.Orange;
+        }
+
+        public override void OnEquip(Player player, Item item)
+        {
+            Main.NewText("We just equipped!");
+        }
+
+        public override void SafeUpdateEquip(Player Player)
+        {
+            Player.statLifeMax2 += 100;
+        }
+    }
+
     public class TestSimulator2 : SmartAccessory
     {
         public override string Texture => AssetDirectory.MiscItem + "StarGlaive";
@@ -46,7 +74,7 @@ namespace StarlightRiver.Content.Items.Misc
             ModContent.ItemType<PulseBoots>()
         };
 
-        public TestSimulator2() : base("Testificate Charm 2", "debug item\nCombines the effects of some accessories in a chain") { }
+        public TestSimulator2() : base("Testificate Charm 2", "debug item\nCombines the effects of Testificate Charm and Pulse Boots") { }
 
         public override void SafeSetDefaults()
         {
