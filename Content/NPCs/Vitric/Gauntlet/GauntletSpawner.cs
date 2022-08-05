ba6760f13996d18aac5646185d4ab495dd181131
bobfishkins23 <bobfishkins23@yahoo.com>
2022-08-05 11:19:45 -0400

    Added rest period to NPC spawners, refined NPC spawner visuals

--- Content/NPCs/Vitric/Gauntlet/GauntletSpawner.cs
+++ Content/NPCs/Vitric/Gauntlet/GauntletSpawner.cs
@@ -9,11 +9,15 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 using System.Collections.Generic;
+using Terraria.Graphics.Effects;
 
 namespace StarlightRiver.Content.NPCs.Vitric.Gauntlet
 {
     public class GauntletSpawner : ModProjectile
     {
+        private List<Vector2> cache;
+        private Trail trail;
+
         public ref float NPCType => ref Projectile.ai[0];
         public ref float Timer => ref Projectile.ai[1];
 
@@ -23,7 +27,7 @@ namespace StarlightRiver.Content.NPCs.Vitric.Gauntlet
         {
             Projectile.width = 16;
             Projectile.height = 16;
-            Projectile.tileCollide = false;
+            Projectile.tileCollide = true;
             Projectile.ignoreWater = true;
             Projectile.penetrate = -1;
             Projectile.aiStyle = -1;
@@ -31,12 +35,26 @@ namespace StarlightRiver.Content.NPCs.Vitric.Gauntlet
 
         public override void AI()
         {
-            Timer++;
+            ManageCaches();
+            ManageTrail();
+
+            if (Timer > 1)
+                Timer++;
+            else //While being thrown out
+            {
+                Projectile.velocity.Y += 0.1f;
+
+                if(Main.rand.NextBool(2))
+                    Dust.NewDustPerfect(Projectile.Center + Main.rand.NextVector2Circular(10, 10), ModContent.DustType<Cinder>(), Vector2.Zero, 0, new Color(255, 170, 100), 0.65f);
+
+                return;
+            }
 
             if (Timer < 50)
             {
-                Vector2 cinderPos = Projectile.Top + Main.rand.NextVector2Circular(95, 95);
-                Dust cinder = Dust.NewDustPerfect(cinderPos, ModContent.DustType<Dusts.Cinder>(), -Vector2.UnitY.RotatedBy(cinderPos.AngleTo(Projectile.Center)) * Main.rand.NextFloat(-2, 2), 0, Bosses.GlassMiniboss.Glassweaver.GlowDustOrange, 1f);
+                Vector2 cinderPos = Projectile.Top + Main.rand.NextVector2Circular(40, 40);
+                var vel = -Vector2.UnitY.RotatedBy(cinderPos.AngleTo(Projectile.Center)) * Main.rand.NextFloat(-2, 2);
+                Dust cinder = Dust.NewDustPerfect(cinderPos, ModContent.DustType<Cinder>(), vel, 0, Bosses.GlassMiniboss.Glassweaver.GlowDustOrange, 0.8f);
                 cinder.customData = Projectile.Center + Main.rand.NextVector2Circular(40, 40);
             }
 
@@ -47,14 +65,43 @@ namespace StarlightRiver.Content.NPCs.Vitric.Gauntlet
             }
         }
 
-        public override bool PreDraw(ref Color lightColor)
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+            Projectile.velocity *= 0;
+            Timer = 2;
+            return false;
+		}
+
+		public override bool PreDraw(ref Color lightColor)
         {
             var tex = Terraria.GameContent.TextureAssets.Npc[(int)NPCType].Value;
             var fakeNPC = new NPC();
             fakeNPC.SetDefaults((int)NPCType);
             fakeNPC.FindFrame();
 
-            var effect = Terraria.Graphics.Effects.Filters.Scene["MoltenForm"].GetShader().Shader;
+            Effect trailEffect = Filters.Scene["CeirosRing"].GetShader().Shader;
+
+            Matrix world = Matrix.CreateTranslation(-Main.screenPosition.Vec3());
+            Matrix view = Main.GameViewMatrix.ZoomMatrix;
+            Matrix projection = Matrix.CreateOrthographicOffCenter(0, Main.screenWidth, Main.screenHeight, 0, -1, 1);
+
+            trailEffect.Parameters["time"].SetValue(Projectile.timeLeft * -0.04f);
+            trailEffect.Parameters["repeats"].SetValue(1);
+            trailEffect.Parameters["transformMatrix"].SetValue(world * view * projection);
+            trailEffect.Parameters["sampleTexture"].SetValue(ModContent.Request<Texture2D>("StarlightRiver/Assets/EnergyTrail").Value);
+
+            trail?.Render(trailEffect);
+
+            var glowTex = ModContent.Request<Texture2D>("StarlightRiver/Assets/Keys/GlowAlpha").Value;
+            var color = new Color(255, 160, 100);
+            color.A = 0;
+            Main.spriteBatch.Draw(glowTex, Projectile.Center - Main.screenPosition, null, color * (1 - (Timer / 25f)), 0, glowTex.Size() / 2, Timer * 0.1f, 0, 0);
+            Main.spriteBatch.Draw(glowTex, Projectile.Center - Main.screenPosition, null, color * (1 - (Timer / 25f)), 0, glowTex.Size() / 2, Timer * 0.05f, 0, 0);
+
+            Main.spriteBatch.Draw(glowTex, Projectile.Center - Main.screenPosition, null, color * ((25 - Timer) / 25f), 0, glowTex.Size() / 2, 0.75f, 0, 0);
+            Main.spriteBatch.Draw(glowTex, Projectile.Center - Main.screenPosition, null, color * 2 * ((25 - Timer) / 25f), 0, glowTex.Size() / 2, 0.25f, 0, 0);
+
+            var effect = Filters.Scene["MoltenForm"].GetShader().Shader;
             effect.Parameters["sampleTexture2"].SetValue(ModContent.Request<Texture2D>("StarlightRiver/Assets/Bosses/VitricBoss/ShieldMap").Value);
             effect.Parameters["uTime"].SetValue(Timer / 70f * 2);
             effect.Parameters["sourceFrame"].SetValue(new Vector4(0, 0, fakeNPC.frame.Width, fakeNPC.frame.Height));
@@ -69,6 +116,37 @@ namespace StarlightRiver.Content.NPCs.Vitric.Gauntlet
             Main.spriteBatch.Begin(default, default, default, default, default, default, Main.GameViewMatrix.ZoomMatrix);
 
             return false;
+        }
+
+        private void ManageCaches()
+        {
+            if (cache is null)
+            {
+                cache = new List<Vector2>();
+                for (int i = 0; i < 30; i++)
+                {
+                    cache.Add(Projectile.Center);
+                }
+            }
+
+            cache.Add(Projectile.Center);
+
+            while (cache.Count > 30)
+            {
+                cache.RemoveAt(0);
+            }
+
+        }
+
+        private void ManageTrail()
+        {
+            trail ??= new Trail(Main.instance.GraphicsDevice, 30, new TriangularTip(4), factor => 24, factor =>
+            {
+                return new Color(255, 200, 165) * factor.X;
+            });
+
+            trail.Positions = cache.ToArray();
+            trail.NextPosition = Projectile.Center + Projectile.velocity;
         }
     }
 }
--- Content/Tiles/Vitric/Temple/NPCSpawner.cs
+++ Content/Tiles/Vitric/Temple/NPCSpawner.cs
@@ -9,6 +9,7 @@ using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria.ID;
+using StarlightRiver.Content.NPCs.Vitric.Gauntlet;
 
 namespace StarlightRiver.Content.Tiles.Vitric
 {
@@ -34,29 +35,66 @@ namespace StarlightRiver.Content.Tiles.Vitric
 		public bool active;
 
 		public ref float timer => ref Projectile.ai[0];
+		public ref float spawned => ref Projectile.ai[1];
 
 		public NPCSpawnerDummy() : base(ModContent.TileType<NPCSpawner>(), 32, 48) { }
 
 		public override void Update()
 		{
+			if (spawned >= 600)
+				active = false;
+
 			if (active && timer < 60)
 				timer++;
 
-			if (!active && timer > 0)
-				timer--;
-
-			if (Main.player.Any(n => Vector2.Distance(n.Center, Projectile.Center) < 400))
+			if (!active)
 			{
+				if (timer > 0)
+					timer--;
+
+				if (spawned > 0)
+					spawned--;
+			}
+
+			if (Main.player.Any(n => Vector2.Distance(n.Center, Projectile.Center) < 600) && (active || spawned <= 0))
+			{
+				var nearby = Main.npc.Count(n => n.active && Vector2.Distance(n.Center, Projectile.Center) < 600);
+
+				if (nearby > 4)
+				{
+					spawned = 300;
+					return;
+				}
+
 				active = true;
-				SpawnEnemies((int)timer);
+				SpawnEnemies((int)Main.GameUpdateCount);
 			}
 			else
 				active = false;
+
+			Color color = Color.Lerp(new Color(255, 100, 40), new Color(255, 160, 100), timer / 60f);
+			var glowOpacity = (timer / 60f) * 0.75f + (0.2f + (float)(Math.Sin(3.14f + Main.GameUpdateCount / 60f * 6.28f)) * 0.2f);
+
+			Lighting.AddLight(Projectile.Center, (color * glowOpacity).ToVector3());
 		}
 
 		public void SpawnEnemies(int time)
 		{
+			if (time % 60 == 0)
+			{
+				int monster;
+				switch(Main.rand.Next(3))
+				{
+					case 0: monster = ModContent.NPCType<GruntConstruct>(); break;
+					case 1: monster = ModContent.NPCType<PelterConstruct>(); break;
+					case 2: monster = ModContent.NPCType<ShieldConstruct>(); break;
+					default: monster = ModContent.NPCType<GruntConstruct>(); break;
+				}
 
+				Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Vector2.UnitY.RotatedByRandom(1) * -5, ModContent.ProjectileType<GauntletSpawner>(), 1, 0, Main.myPlayer, monster);
+
+				spawned += 150;
+			}
 		}
 
 		public override void PostDraw(Color lightColor)
@@ -64,10 +102,19 @@ namespace StarlightRiver.Content.Tiles.Vitric
 			var spriteBatch = Main.spriteBatch;
 			var tex = ModContent.Request<Texture2D>(AssetDirectory.VitricTile + "NPCSpawnerGlow").Value;
 			var frame = new Rectangle(0, (int)(1 + Helpers.Helper.SwoopEase(timer / 60f) * 18f) % 8 * 48, 22, 48);
+			var pos = Projectile.Center - Main.screenPosition + new Vector2(0, -12 * timer / 60f);
 
 			Color color = Color.Lerp(lightColor, Color.White, timer / 60f);
 
-			spriteBatch.Draw(tex, Projectile.Center - Main.screenPosition + new Vector2(0, -12 * timer / 60f), frame, color, 0, new Vector2(11, 24), 1, 0, 0);
+			spriteBatch.Draw(tex, pos, frame, color, 0, new Vector2(11, 24), 1, 0, 0);
+
+			var glowTex = ModContent.Request<Texture2D>("StarlightRiver/Assets/Keys/GlowAlpha").Value;
+			var glowColor = new Color(255, 160, 100);
+			glowColor.A = 0;
+
+			var glowOpacity = (timer / 60f) * 0.5f + (float)(Math.Sin(3.14f + Main.GameUpdateCount / 60f * 6.28f)) * 0.5f;
+
+			Main.spriteBatch.Draw(glowTex, pos, null, glowColor * glowOpacity, 0, glowTex.Size() / 2, 0.8f, 0, 0);
 		}
 
 	}
