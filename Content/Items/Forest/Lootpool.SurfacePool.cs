b44a7d63d2627c052e2c987185007bda90aa8319
GraydeeSi <wiseguy0002@gmail.com>
2022-08-20 16:06:51 -0400

    Tweaked loot pools

--- Content/Items/Forest/Lootpool.SurfacePool.cs
+++ Content/Items/Forest/Lootpool.SurfacePool.cs
@@ -9,10 +9,10 @@ namespace StarlightRiver.Content.Items.Forest
     {
         public override void AddLoot()
         {
-            AddItem(ModContent.ItemType<AcornSprout>(), ChestRegionFlags.Surface, default, 1, true, 1);
-            AddItem(ModContent.ItemType<DustyAmulet>(), ChestRegionFlags.Surface, default, 1, true, 1);
-            AddItem(ModContent.ItemType<OldWhetstone>(), ChestRegionFlags.Surface, default, 1, true, 1);
-            AddItem(ModContent.ItemType<Trowel>(), ChestRegionFlags.Surface, default, 1, true, 1);
+            AddItem(ModContent.ItemType<AcornSprout>(), ChestRegionFlags.Surface, 0.5f, 1, true, 1);
+            AddItem(ModContent.ItemType<DustyAmulet>(), ChestRegionFlags.Surface, 0.5f, 1, true, 1);
+            AddItem(ModContent.ItemType<OldWhetstone>(), ChestRegionFlags.Surface, 0.5f, 1, true, 1);
+            AddItem(ModContent.ItemType<Trowel>(), ChestRegionFlags.Surface, 0.5f, 1, true, 1);
         }
     }
 }
--- Content/Items/Misc/LootPool.MiscPool.cs
+++ Content/Items/Misc/LootPool.MiscPool.cs
@@ -13,11 +13,10 @@ namespace StarlightRiver.Content.Items.Misc
             AddItem(ModContent.ItemType<Cheapskates>(), ChestRegionFlags.Ice);
             AddItem(ModContent.ItemType<Slitherring>(), ChestRegionFlags.Jungle, 0.20f);
             AddItem(ModContent.ItemType<CutlassBus>(), ChestRegionFlags.Underwater);
-
             AddItem(ModContent.ItemType<SojournersScarf>(), ChestRegionFlags.Underground | ChestRegionFlags.Surface | ChestRegionFlags.Granite | ChestRegionFlags.Marble, 0.1f);
-            AddItem(ModContent.ItemType<CoughDrops>(), ChestRegionFlags.Underground, default, 1, true, -1);
-            AddItem(ModContent.ItemType<DisinfectantWipes>(), ChestRegionFlags.Underground, default, 1, true, -1);
-            AddItem(ModContent.ItemType<SanitizerSpray>(), ChestRegionFlags.Underground, default, 1, true, -1);
+            AddItem(ModContent.ItemType<CoughDrops>(), ChestRegionFlags.Underground, 0.5f, 1, true, 2);
+            AddItem(ModContent.ItemType<DisinfectantWipes>(), ChestRegionFlags.Underground, 0.5f, 1, true, 2);
+            AddItem(ModContent.ItemType<SanitizerSpray>(), ChestRegionFlags.Underground, 0.5f, 1, true, 2);
         }
     }
 }
