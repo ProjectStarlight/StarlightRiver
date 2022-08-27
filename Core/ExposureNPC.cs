b29482e99fd5fc9c38b237f4aca25bf7d1a79575
SkippZzMods <pumpkinboo2k6@gmail.com>
2022-08-26 21:47:26 -0400

    Added Exposure System
    also made refractive blade use said system

---
+++ Core/ExposureNPC.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Core
+{
+    public class ExposureNPC : GlobalNPC
+    {
+        public int ExposureAdd;
+        public int ExposureAddMelee;
+        public int ExposureAddRanged;
+        public int ExposureAddMagic;
+        public int ExposureAddSummon; //literally just summon tag without the requirement of being the players minion target
+
+        public float ExposureMult;
+        public float ExposureMultMelee;
+        public float ExposureMultRanged;
+        public float ExposureMultMagic;
+        public float ExposureMultSummon;
+
+        public override bool InstancePerEntity => true;
+        public override void ResetEffects(NPC npc)
+        {
+            ExposureAdd = 0;
+            ExposureAddMelee = 0;
+            ExposureAddRanged = 0;
+            ExposureAddMagic = 0;
+            ExposureAddSummon = 0;
+
+            ExposureMult = 0f;
+            ExposureMultMelee = 0f;
+            ExposureMultRanged = 0f;
+            ExposureMultMagic = 0f;
+            ExposureMultSummon = 0f;
+        }
+
+        public override void ModifyHitByItem(NPC npc, Player player, Item item, ref int damage, ref float knockback, ref bool crit)
+        {
+            if (item.CountsAsClass(DamageClass.Melee))
+                damage = (int)(damage * (1f + ExposureMultMelee)) + ExposureAddMelee;
+
+            if (item.CountsAsClass(DamageClass.Ranged))
+                damage = (int)(damage * (1f + ExposureMultRanged)) + ExposureAddRanged;
+
+            if (item.CountsAsClass(DamageClass.Magic))
+                damage = (int)(damage * (1f + ExposureMultMagic)) + ExposureAddMagic;
+
+            if (item.CountsAsClass(DamageClass.Summon))
+                damage = (int)(damage * (1f + ExposureMultSummon)) + ExposureAddSummon;
+
+            damage = (int)(damage * (1f + ExposureMult)) + ExposureAdd;
+        }
+
+        public override void ModifyHitByProjectile(NPC npc, Projectile projectile, ref int damage, ref float knockback, ref bool crit, ref int hitDirection)
+        {
+            if (projectile.CountsAsClass(DamageClass.Melee))
+                damage = (int)(damage * (1f + ExposureMultMelee)) + ExposureAddMelee;
+
+            if (projectile.CountsAsClass(DamageClass.Ranged))
+                damage = (int)(damage * (1f + ExposureMultRanged)) + ExposureAddRanged;
+
+            if (projectile.CountsAsClass(DamageClass.Magic))
+                damage = (int)(damage * (1f + ExposureMultMagic)) + ExposureAddMagic;
+
+            if (projectile.CountsAsClass(DamageClass.Summon))
+                damage = (int)(damage * (1f + ExposureMultSummon)) + ExposureAddSummon;
+
+            damage = (int)(damage * (1f + ExposureMult)) + ExposureAdd;
+        }
+    }
+}
