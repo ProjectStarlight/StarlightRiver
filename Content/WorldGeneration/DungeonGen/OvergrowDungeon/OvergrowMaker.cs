bbfd1a5329ebc86ef701fbe68ec50898ba1e5e3e
bobfishkins23 <bobfishkins23@yahoo.com>
2022-08-27 23:13:01 -0400

    added constraints to overgrowth dungeon generator example

--- Content/WorldGeneration/DungeonGen/OvergrowDungeon/OvergrowMaker.cs
+++ Content/WorldGeneration/DungeonGen/OvergrowDungeon/OvergrowMaker.cs
@@ -1,10 +1,16 @@
-﻿using StarlightRiver.Content.WorldGeneration.DungeonGen.OvergrowDungeon.Rooms;
+﻿using Microsoft.Xna.Framework;
+using StarlightRiver.Content.Tiles.Permafrost;
+using StarlightRiver.Content.WorldGeneration.DungeonGen.OvergrowDungeon.Rooms;
+using StarlightRiver.Core;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
 using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace StarlightRiver.Content.WorldGeneration.DungeonGen.OvergrowDungeon
 {
@@ -21,6 +27,27 @@ namespace StarlightRiver.Content.WorldGeneration.DungeonGen.OvergrowDungeon
 		public override void Initialize()
 		{
 			dungeon = new DungeonRoom.secType[60, 60];
+		}
+
+		public override bool TileBlacklistCondition(Tile tile, int x, int y)
+		{
+			var t = tile.TileType;
+			if (t == TileID.BlueDungeonBrick ||
+				t == TileID.GreenDungeonBrick ||
+				t == TileID.PinkDungeonBrick ||
+				t == ModContent.TileType<AuroraBrick>())
+				return true;
+
+			if (StarlightWorld.VitricBiome.Contains(new Point(x, y)) ||
+				StarlightWorld.SquidBossArena.Contains(new Point(x, y)))
+				return true;
+
+			return false;
+		}
+
+		public override bool IsDungeonValid()
+		{
+			return rooms.Count >= 10;
 		}
 	}
 }
