8c762e8de8371546be1c277b53d67a90f809504e
bobfishkins23 <bobfishkins23@yahoo.com>
2022-08-28 14:00:41 -0400

    Fixed generation crash

--- Content/WorldGeneration/PermafrostGen.cs
+++ Content/WorldGeneration/PermafrostGen.cs
@@ -113,7 +113,7 @@ namespace StarlightRiver.Core
                 oldPos = new Vector2(xTarget + 11, yTarget + 9) * 16;
             }
 
-            for (int y = 0; y < Main.maxTilesY - 200; y++)
+            for (int y = 14; y < Main.maxTilesY - 200; y++)
             {             
                 if (Main.tile[center, y].TileType == TileID.SnowBlock || Main.tile[center, y].TileType == TileID.IceBlock)
                 {
