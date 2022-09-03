9a97bd6013084fd8fb89fdc2100305b91339cf02
bobfishkins23 <bobfishkins23@yahoo.com>
2022-09-03 11:43:17 -0400

    added custom whippable support, basic framework for noxious node, removed whip autoreuse

--- Content/Dusts/Gas.cs
+++ Content/Dusts/Gas.cs
@@ -32,5 +32,29 @@ namespace StarlightRiver.Content.Dusts
         }
     }
 
-    public class GasGreen : Gas { }
+    public class NoxiousGas : Gas 
+    {
+        public override string Texture => AssetDirectory.Dust + "NeedlerDust";
+
+        public override void OnSpawn(Dust dust)
+        {
+            dust.noGravity = true;
+            dust.noLight = true;
+            dust.rotation = Main.rand.NextFloat(6.28f);
+            dust.frame = new Rectangle(0, 0, 34, 36);
+        }
+       
+        public override bool Update(Dust dust)
+        {
+            dust.fadeIn++;
+            dust.position += dust.velocity;
+            dust.scale *= 1.02f;
+            dust.velocity *= 0.92f;
+            dust.rotation += 0.01f;
+            dust.color = new Color(20, 255, 200) * (float)System.Math.Sin(dust.fadeIn / 60f * 3.14f) * 0.02f;
+
+            if (dust.fadeIn >= 60) dust.active = false;
+            return false;
+        }
+    }
 }
