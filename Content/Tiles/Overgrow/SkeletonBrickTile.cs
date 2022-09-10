2bb6db0a5802629f4804ab4e7d7383c75cabca1d
SkippZzMods <pumpkinboo2k6@gmail.com>
2022-09-10 19:28:27 -0400

    Tile finished, needs item sprite

---
+++ Content/Tiles/Overgrow/SkeletonBrickTile.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework;
+using StarlightRiver.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Content.Tiles.Overgrow
+{
+    public class SkeletonBrickTile : ModTile
+    {
+        public override string Texture => AssetDirectory.OvergrowTile + Name;
+        public override void SetStaticDefaults()
+        {
+            Main.tileSolid[Type] = true;
+			Main.tileBrick[Type] = true;
+            HitSound = SoundID.Tink;
+            Main.tileBlockLight[Type] = true;
+            AddMapEntry(Color.Black);
+
+            ItemDrop = ModContent.ItemType<SkeletonBrickItem>();
+
+            DustType = DustID.DungeonBlue;
+
+            ModTranslation name = CreateMapEntryName();
+            name.SetDefault("Skeleton Brick");
+        }
+		public override void AnimateIndividualTile(int type, int i, int j, ref int frameXOffset, ref int frameYOffset)
+		{
+			int frame = 0;
+			int xPosition = i % 4;
+			int yPosition = j % 4;
+			switch (xPosition)
+			{
+				case 0:
+					switch (yPosition)
+					{
+						case 0: frame = 1; break;
+						case 1:	frame = 2; break;
+						case 2: frame = 0; break;
+						case 3: frame = 2; break;
+						default: frame = 0; break;
+					}
+					break;
+				case 1:
+					switch (yPosition)
+					{
+						case 0: frame = 2; break;
+						case 1: frame = 0; break;
+						case 2: frame = 2; break;
+						case 3: frame = 1; break;
+						default: frame = 0; break;
+					}
+					break;
+				case 2:
+					switch (yPosition)
+					{
+						case 0: frame = 2; break;
+						case 1: frame = 1; break;
+						case 2: frame = 0; break;
+						case 3: frame = 2; break;
+						default: frame = 2; break;
+					}
+					break;
+				case 3:
+					switch (yPosition)
+					{
+						case 0: frame = 2; break;
+						case 1: frame = 0; break;
+						case 2: frame = 0; break;
+						case 3: frame = 2; break;
+						default: frame = 1; break;
+					}
+					break;
+			}
+
+			frameXOffset = frame * 288; //width of texture divided by 3, the amount of "frames" for the tile
+		}
+    }
+
+    public class SkeletonBrickItem : QuickTileItem
+    {
+        public override string Texture => AssetDirectory.Debug;
+        public SkeletonBrickItem() : base("Skeletal Brick", "", "SkeletonBrickTile", texturePath: "Debug Item") { }
+    }
+}
