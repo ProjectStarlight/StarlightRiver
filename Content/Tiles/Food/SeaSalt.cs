b7cedce1e800dd9492114ea6b27abfa14efbe44a
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2022-09-18 05:34:58 -0500

    Falling sand

--- Content/Tiles/Food/SeaSalt.cs
+++ Content/Tiles/Food/SeaSalt.cs
@@ -13,12 +13,23 @@ namespace StarlightRiver.Content.Tiles.Food
 
         public override void SetStaticDefaults()
         {
-            //Main.tileMerge[Type][TileID.Sand] = true;
-            //Main.tileMerge[TileID.Sand][Type] = true;
-            this.QuickSet(0, 2, SoundID.Dig, new Color(1f, 0.8f, 0.8f), ItemType<Items.Food.SeaSalt>());
+            Main.tileMerge[Type][ModContent.TileType<Salt>()] = true;
+            Main.tileMerge[ModContent.TileType<Salt>()][Type] = true;
+            this.QuickSet(0, DustID.Web, SoundID.Dig, new Color(1f, 0.8f, 0.8f), ItemType<Items.Food.SeaSalt>());
             Main.tileSolid[Type] = false;
+            //Main.tileBlockLight[Type] = true;
+            TileID.Sets.ForAdvancedCollision.ForSandshark[Type] = true; // Allows Sandshark enemies to "swim" in this sand.
+            TileID.Sets.Falling[Type] = true;
         }
 
+        public override bool TileFrame(int i, int j, ref bool resetFrame, ref bool noBreak) => Salt.SandFall(i, j, ModContent.ProjectileType<SeaSaltProjectile>());
+
         public override bool IsTileSpelunkable(int i, int j) => true;
+    }
+
+    public class SeaSaltProjectile : SaltProjectile
+    {
+        protected override int GetTileType => ModContent.TileType<SeaSalt>();
+        protected override string GetName => "Sea Salt Ball";
     }
 }
