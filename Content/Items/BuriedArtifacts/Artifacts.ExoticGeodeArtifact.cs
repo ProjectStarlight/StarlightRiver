2573ffd35148876277f089514cd655ea86ef74f0
GraydeeSi <wiseguy0002@gmail.com>
2022-09-21 13:18:23 -0400

    Exotic geode now gives gems at extractinator

--- Content/Items/BuriedArtifacts/Artifacts.ExoticGeodeArtifact.cs
+++ Content/Items/BuriedArtifacts/Artifacts.ExoticGeodeArtifact.cs
@@ -1,5 +1,9 @@
 ﻿using StarlightRiver.Core;
+using System;
 using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Utilities;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -13,6 +17,7 @@ namespace StarlightRiver.Content.Items.BuriedArtifacts
 		{
 			DisplayName.SetDefault("Exotic Geode");
 			Tooltip.SetDefault("'Incredibly shiny'");
+			ItemID.Sets.ExtractinatorMode[Item.type] = Item.type;
 		}
 
 		public override void SetDefaults()
@@ -23,6 +28,25 @@ namespace StarlightRiver.Content.Items.BuriedArtifacts
 			Item.rare = ItemRarityID.Blue;
 			Item.maxStack = 30;
 			Item.autoReuse = true;
+			Item.consumable = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+		}
+
+		public override void ExtractinatorUse(ref int resultType, ref int resultStack)
+		{
+			int[] gems = new int[]
+			{
+				ItemID.Topaz,
+				ItemID.Amethyst,
+				ItemID.Sapphire,
+				ItemID.Emerald,
+				ItemID.Diamond,
+				ItemID.Ruby
+			};
+			resultStack = Main.rand.Next(7, 15);
+			resultType = gems[Main.rand.Next(gems.Length)];
 		}
 	}
 }
