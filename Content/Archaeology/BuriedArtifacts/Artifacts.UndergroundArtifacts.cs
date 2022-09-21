662c184b57ef76df19aafce66e02ad426154990c
GraydeeSi <wiseguy0002@gmail.com>
2022-09-21 14:24:42 -0400

    Perfectly generic object

--- Content/Archaeology/BuriedArtifacts/Artifacts.UndergroundArtifacts.cs
+++ Content/Archaeology/BuriedArtifacts/Artifacts.UndergroundArtifacts.cs
@@ -27,4 +27,19 @@ namespace StarlightRiver.Content.Archaeology.BuriedArtifacts
 
         public override int ItemType => ModContent.ItemType<ExoticGeodeArtifactItem>();
     }
+
+    public class PerfectlyGenericArtifact : UndergroundArtifact
+    {
+        public override Vector2 Size => new Vector2(32, 32);
+
+        public override float SpawnChance => 0.01f;
+
+        public override int SparkleDust => ModContent.DustType<Dusts.ArtifactSparkles.WhiteArtifactSparkle>();
+
+        public override int SparkleRate => 40;
+
+        public override Color BeamColor => Color.White;
+
+        public override int ItemType => ModContent.ItemType<PerfectlyGenericArtifactItem>();
+    }
 }
