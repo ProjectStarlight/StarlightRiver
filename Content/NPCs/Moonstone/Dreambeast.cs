876c355ebfc1b55a065af6a40b568c28310ac9c4
bobfishkins23 <bobfishkins23@yahoo.com>
2022-09-21 22:45:40 -0400

    dream beast shader

---
+++ Content/NPCs/Moonstone/Dreambeast.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StarlightRiver.Core;
+using StarlightRiver.Physics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace StarlightRiver.Content.NPCs.Moonstone
+{
+	internal class Dreambeast : ModNPC
+	{
+		public VerletChain[] chains = new VerletChain[5];
+
+		public Vector2 homePos;
+
+		private bool AppearVisible => Main.LocalPlayer.HasBuff(ModContent.BuffType<Buffs.Overcharge>());
+
+		public ref float Phase => ref NPC.ai[0];
+		public ref float Timer => ref NPC.ai[1];
+
+		public override string Texture => AssetDirectory.Debug;
+
+		public override void SetDefaults()
+		{
+			NPC.width = 64;
+			NPC.height = 64;
+			NPC.noGravity = true;
+			NPC.noTileCollide = true;
+			NPC.lifeMax = 666666;
+			NPC.damage = 66;
+			NPC.dontTakeDamage = true;
+			NPC.immortal = true;
+		}
+
+		public override void AI()
+		{
+			Timer++;
+
+			if (homePos == default)
+				homePos = NPC.Center;
+
+			/*
+			for (int k = 0; k < chains.Length; k++)
+			{
+				var chain = chains[k];
+
+				if (chain is null)
+					chain = new VerletChain(50, false, NPC.Center, 5, false);
+
+				chain.UpdateChain(NPC.Center);
+			}*/
+
+			//Temp
+			NPC.Center = homePos + Vector2.One.RotatedBy(Timer * 0.005f) * 350;
+		}
+
+		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+		{
+			var tex = ModContent.Request<Texture2D>("StarlightRiver/Assets/Bosses/VitricBoss/VitricBossGodrayHead").Value;
+
+			if (!AppearVisible)
+			{
+				var effect = Terraria.Graphics.Effects.Filters.Scene["MoonstoneBeastEffect"].GetShader().Shader;
+				effect.Parameters["baseTexture"].SetValue(tex);
+				effect.Parameters["distortTexture"].SetValue(ModContent.Request<Texture2D>("StarlightRiver/Assets/Noise/MiscNoise2").Value);
+				effect.Parameters["size"].SetValue(tex.Size());
+				effect.Parameters["time"].SetValue(Main.GameUpdateCount * 0.005f);
+
+				spriteBatch.End();
+				spriteBatch.Begin(default, BlendState.Additive, default, default, default, effect, Main.GameViewMatrix.TransformationMatrix);
+			}
+
+			spriteBatch.Draw(tex, NPC.Center - Main.screenPosition, Color.White);
+
+			spriteBatch.End();
+			spriteBatch.Begin(default, default, default, default, default, default, Main.GameViewMatrix.TransformationMatrix);
+
+			return false;
+		}
+	}
+}
---
+++ Effects/Compile/MoonstoneBeastEffect.fx
@@ -0,0 +1,58 @@
+﻿sampler uImage0 : register(s0);
+sampler uImage1 : register(s1);
+
+texture baseTexture;
+sampler2D baseSampler = sampler_state
+{
+    texture = <baseTexture>;
+    magfilter = LINEAR;
+    minfilter = LINEAR;
+    mipfilter = LINEAR;
+    AddressU = wrap;
+    AddressV = wrap;
+};
+
+texture distortTexture;
+sampler2D distortSampler = sampler_state
+{
+    texture = <distortTexture>;
+    magfilter = LINEAR;
+    minfilter = LINEAR;
+    mipfilter = LINEAR;
+    AddressU = wrap;
+    AddressV = wrap;
+};
+
+float2 size;
+float time;
+
+float4 Main(float2 uv : TEXCOORD) : COLOR
+{
+    float noise0 = tex2D(distortSampler, uv / 8.0 + time * 0.05).r;
+    float noise1 = tex2D(distortSampler, uv / 8.0 - time * 0.1).r;
+    
+    uv += noise0 * 0.025 + noise1 * 0.025;
+    
+    float4 color = tex2D(baseSampler, uv);
+        color.a *= 2.5
+        - tex2D(baseSampler, uv + 2.0 / size.x).a
+        - tex2D(baseSampler, uv - 2.0 / size.x).a;
+    
+    color.r *= color.a;
+    color.g *= color.a * 0.9;
+    color.b *= color.a * 2.0;
+    
+    color += color.a * 0.2;
+    
+    color.a *= 0.25 + noise0 * noise1;
+    
+    return color;
+}
+
+technique SpriteDrawing
+{
+    pass P0
+    {
+        PixelShader = compile ps_3_0 Main();
+    }
+};
