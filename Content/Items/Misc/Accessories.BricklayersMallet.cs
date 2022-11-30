ae65e7f322b03527c219ad6b81d07e80523e35f3
SkippZzMods <pumpkinboo2k6@gmail.com>
2022-11-30 00:18:31 -0500

    fixed bricklayers mallet

--- Content/Items/Misc/Accessories.BricklayersMallet.cs
+++ Content/Items/Misc/Accessories.BricklayersMallet.cs
@@ -14,25 +14,34 @@ namespace StarlightRiver.Content.Items.Misc
         public override string Texture => AssetDirectory.MiscItem + Name;
         public BricklayersMallet() : base("Bricklayer's Mallet", "Doubles block placement and tool range\nDecreases mining speed by 50% for blocks outside your original range") { }
 
-        public override void SafeSetDefaults()
+		public override void Load()
+		{
+			StarlightPlayer.PostUpdateEquipsEvent += UpdateBricklayersMallet;
+		}
+
+		private void UpdateBricklayersMallet(StarlightPlayer player)
+		{
+			if (!Equipped(player.Player))
+				return;
+
+			Player Player = player.Player;
+			Player.blockRange *= 2;
+			if (Main.myPlayer == Player.whoAmI)
+			{
+				bool outsideXRange = Main.MouseWorld.X < Player.Center.X ? Math.Abs(Player.Left.X - Main.MouseWorld.X) > (Player.tileRangeX - 1) * 16f : Math.Abs(Player.Right.X - Main.MouseWorld.X) > (Player.tileRangeX - 1) * 16f;
+				bool outsideYRange = Main.MouseWorld.Y > Player.Center.Y ? Math.Abs(Player.Bottom.Y - Main.MouseWorld.Y) > (Player.tileRangeY - 2) * 16f : Math.Abs(Player.Top.Y - Main.MouseWorld.Y) > (Player.tileRangeY - 2) * 16f;
+				if (outsideXRange || outsideYRange)
+					Player.pickSpeed *= 1.5f;
+
+				Player.tileRangeX *= 2;
+				Player.tileRangeY *= 2;
+			}
+		}
+
+		public override void SafeSetDefaults()
         {
             Item.rare = ItemRarityID.Green;
             Item.value = Item.sellPrice(silver: 25);
-        }
-
-        public override void SafeUpdateEquip(Player Player)
-        {
-            Player.blockRange *= 2;
-            if (Main.myPlayer == Player.whoAmI)
-            {
-                bool outsideXRange = Main.MouseWorld.X < Player.Center.X ? Math.Abs(Player.Left.X - Main.MouseWorld.X) > 80f + (Player.tileRangeX * 16f) : Math.Abs(Player.Right.X - Main.MouseWorld.X) > 80f + (Player.tileRangeX * 16f);
-                bool outsideYRange = Main.MouseWorld.Y > Player.Center.Y ? Math.Abs(Player.Bottom.Y - Main.MouseWorld.Y) > 48f + (Player.tileRangeY * 16f) : Math.Abs(Player.Top.Y - Main.MouseWorld.Y) > 48f + (Player.tileRangeY * 16f);
-                if (outsideXRange || outsideYRange)
-                    Player.pickSpeed *= 1.5f;
-
-                Player.tileRangeX *= 2;
-                Player.tileRangeY *= 2;
-            }
         }
     }
 
