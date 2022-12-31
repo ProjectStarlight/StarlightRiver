cd8c231f0053f3425510228c8360bf5df455ab7b
GraydeeSi <wiseguy0002@gmail.com>
2022-12-30 19:48:57 -0500

    Steampunk artifacts

--- Content/Archaeology/BuriedArtifacts/Artifacts.UndergroundArtifacts.cs
+++ Content/Archaeology/BuriedArtifacts/Artifacts.UndergroundArtifacts.cs
@@ -30,5 +30,46 @@ namespace StarlightRiver.Content.Archaeology.BuriedArtifacts
 		public override Color BeamColor => Color.White;
 
 		public override int ItemType => ModContent.ItemType<PerfectlyGenericArtifactItem>();
+
+
+	}
+
+	public abstract class SteampunkArtifact : UndergroundArtifact
+	{
+		public override float SpawnChance => 0.5f;
+
+		public override Color BeamColor => Color.Gold;
+
+		public override int SparkleRate => 30;
+
+		public override int SparkleDust => ModContent.DustType<Dusts.ArtifactSparkles.GoldArtifactSparkle>();
+	}
+
+	public class CopperCogArtifact : SteampunkArtifact
+	{
+		public override Vector2 Size => new(32, 32);
+
+		public override int ItemType => ModContent.ItemType<CopperCogArtifactItem>();
+	}
+
+	public class ImportantScrewArtifact : SteampunkArtifact
+	{
+		public override Vector2 Size => new(26, 19);
+
+		public override int ItemType => ModContent.ItemType<ImportantScrewArtifactItem>();
+	}
+
+	public class SuspiciouslyStrangeBrewArtifact : SteampunkArtifact
+	{
+		public override Vector2 Size => new(18, 38);
+
+		public override int ItemType => ModContent.ItemType<SuspiciouslyStrangeBrewArtifactItem>();
+	}
+
+	public class InconspicuousPlatingArtifact : SteampunkArtifact
+	{
+		public override Vector2 Size => new(30, 28);
+
+		public override int ItemType => ModContent.ItemType<InconspicuousPlatingArtifactItem>();
 	}
 }
---
+++ Content/Items/BuriedArtifacts/Artifacts.SteampunkArtifacts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Terraria.Utilities;
+using Terraria.ID;
+
+namespace StarlightRiver.Content.Items.BuriedArtifacts
+{
+	public abstract class SteampunkArtifactItem : ModItem
+	{
+		public string name;
+
+		public string description;
+		public SteampunkArtifactItem(string _name, string _description)
+		{
+			name = _name;
+			description = _description;
+		}
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault(name);
+			Tooltip.SetDefault("Can be used at the extractinator \n" + description);
+			ItemID.Sets.ExtractinatorMode[Item.type] = Item.type;
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 20;
+			Item.height = 20;
+			Item.useTurn = true;
+			Item.autoReuse = true;
+			Item.useAnimation = 15;
+			Item.useTime = 10;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.consumable = true;
+			Item.maxStack = 999;
+			Item.value = Item.sellPrice(0,0,25,0);
+			Item.rare = ItemRarityID.Blue;
+		}
+
+		public override void ExtractinatorUse(ref int resultType, ref int resultStack)
+		{
+			resultType = ModContent.ItemType<Items.SteampunkSet.AncientGear>();
+			resultStack = Main.rand.Next(1, 4);
+		}
+	}
+	public class CopperCogArtifactItem : SteampunkArtifactItem
+	{
+		public override string Texture => AssetDirectory.Archaeology + "CopperCogArtifact";
+		public CopperCogArtifactItem() : base("Copper Cog", "'Our editors work hard to keep our work polished!'") { }
+	}
+
+	public class ImportantScrewArtifactItem : SteampunkArtifactItem
+	{
+		public override string Texture => AssetDirectory.Archaeology + "ImportantScrewArtifact";
+		public ImportantScrewArtifactItem() : base("Important Screw", "'No machine would be complete (Or work...) Without it!'") { }
+	}
+
+	public class SuspiciouslyStrangeBrewArtifactItem : SteampunkArtifactItem
+	{
+		public override string Texture => AssetDirectory.Archaeology + "SuspiciouslyStrangeBrewArtifact";
+		public SuspiciouslyStrangeBrewArtifactItem() : base("Suspiciously Strange Brew", "'1,500 standard proof!'") { }
+	}
+
+	public class InconspicuousPlatingArtifactItem : SteampunkArtifactItem
+	{
+		public override string Texture => AssetDirectory.Archaeology + "InconspicuousPlatingArtifact";
+		public InconspicuousPlatingArtifactItem() : base("Inconspicuous Plating", "'Stronger than diamonds! Kinda...'") { }
+	}
+}
