40cd7366a370dbbfc1e29f45bd42d92bd48000a5
GraydeeSi <wiseguy0002@gmail.com>
2023-01-02 12:10:23 -0500

    Blover pushes enemies now

--- Content/NPCs/Forest/Blover.cs
+++ Content/NPCs/Forest/Blover.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Terraria.GameContent.Bestiary;
 using Terraria.ID;
 using Terraria.ModLoader.Utilities;
@@ -150,6 +151,15 @@ namespace StarlightRiver.Content.NPCs.Forest
 
 			Vector2 dustPos = NPC.Center + new Vector2(60 * Math.Sign(Target.Center.X - NPC.Center.X), Main.rand.Next(-15, 15));
 			Dust.NewDustPerfect(dustPos, ModContent.DustType<Dusts.GlowLine>(), 7 * new Vector2(Math.Sign(Target.Center.X - NPC.Center.X), 0), 0, Color.White * 0.3f, 1.25f);
+
+			var npcTarget = Main.npc.Where(n => n.active && n.knockBackResist > 0 && Math.Abs(n.Center.X - NPC.Center.X) < 300 && Math.Abs(n.Center.Y - NPC.Center.Y) < 30 && Math.Sign(n.Center.X - NPC.Center.X) == NPC.direction).OrderBy(n => n.Distance(NPC.Center)).FirstOrDefault();
+			if (npcTarget != default)
+			{
+				float npcTargetAcceleration = Math.Sign(npcTarget.Center.X - NPC.Center.X) * (float)((300 - Math.Abs(npcTarget.Center.X - NPC.Center.X)) / 300f) * 0.55f * npcTarget.knockBackResist;
+
+				if ((Math.Abs(npcTarget.velocity.X) < 10 || Math.Sign(npcTarget.velocity.X) != Math.Sign(npcTargetAcceleration)))
+					npcTarget.velocity.X += npcTargetAcceleration;
+			}
 		}
 
 		private void IdleBehavior()
