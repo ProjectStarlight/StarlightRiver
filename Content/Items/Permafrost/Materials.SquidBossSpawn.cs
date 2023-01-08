8ac301c829516c0f1b6676fcf7329189e73b56a1
bobfishkins23 <bobfishkins23@yahoo.com>
2023-01-08 18:38:18 -0500

    added name to boss spawn item

--- Content/Items/Permafrost/Materials.SquidBossSpawn.cs
+++ Content/Items/Permafrost/Materials.SquidBossSpawn.cs
@@ -10,6 +10,12 @@ namespace StarlightRiver.Content.Items.Permafrost
 
 		public override string Texture => AssetDirectory.PermafrostItem + Name;
 
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Suspicious Looking Offering");
+			Tooltip.SetDefault("Drop in prismatic waters to summon the one the Squiddites worship");
+		}
+
 		public override void SetDefaults()
 		{
 			Item.rare = ItemRarityID.Green;
