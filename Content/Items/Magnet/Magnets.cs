2a1c175ded848d5b13fe3ba2f3e78f26174bdc19
GraydeeSi <wiseguy0002@gmail.com>
2023-01-16 12:35:13 -0500

    Magnet enemies WIP

---
+++ Content/Items/Magnet/Magnets.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace StarlightRiver.Content.Items.Magnet
+{
+	class UnchargedMagnet : ModItem
+	{
+		public override string Texture => AssetDirectory.Debug;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Uncharged Magnet");
+			Tooltip.SetDefault("May attract charged enemies");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(0, 0, 5, 0);
+			Item.width = 32;
+			Item.height = 32;
+			Item.maxStack = 999;
+		}
+	}
+
+	class ChargedMagnet : ModItem
+	{
+		public override string Texture => AssetDirectory.Debug;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Charged Magnet");
+			Tooltip.SetDefault("'Pulsing with magnetic power'");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.rare = ItemRarityID.Blue;
+			Item.value = Item.sellPrice(0, 0, 15, 0);
+			Item.width = 32;
+			Item.height = 32;
+			Item.maxStack = 999;
+		}
+	}
+}
---
+++ Content/NPCs/Misc/MagnetizedEnemies.cs
@@ -0,0 +1,72 @@
+﻿//TODO:
+//Negative attack
+//Positive attack
+//Good Sparks
+//Make stuff craftable with said magnets
+//Make uncharged magnets drop from crescent casters
+//Magnet display above head
+
+using StarlightRiver.Content.Items.Magnet;
+using System.Linq;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace StarlightRiver.Content.NPCs.Misc
+{
+	public class MagnetizedEnemies : GlobalNPC
+	{
+		public int charge = 0;
+
+		public Player chargedPlayer = default;
+
+		public override bool InstancePerEntity => true;
+
+		public override void OnSpawn(NPC npc, IEntitySource source)
+		{
+			if (npc.boss)
+				return;
+
+			if (source is EntitySource_SpawnNPC spawnSource)
+			{
+				if (Main.npc.Any(n => n.active && n.GetGlobalNPC<MagnetizedEnemies>().charge != 0))
+					return;
+
+				Player player = Main.player.Where(n => n.active && !n.dead).OrderBy(n => n.DistanceSQ(npc.Center)).FirstOrDefault();
+				if (player != default && player.HasItem(ModContent.ItemType<Items.Magnet.UnchargedMagnet>()))
+				{
+					chargedPlayer = player;
+					charge = Main.rand.NextBool() ? 1 : -1;
+					npc.lifeMax *= 4;
+					npc.life = npc.lifeMax;
+					npc.scale *= 1.3f;
+				}
+			}
+		}
+
+		public override void AI(NPC npc)
+		{
+			if (charge != 0 && Main.rand.NextBool(6))
+				Dust.NewDust(npc.position, npc.width, npc.height, DustID.Electric);
+		}
+
+		public override void OnKill(NPC npc)
+		{
+			if (chargedPlayer == default)
+				return;
+			for (int index = 0; index < 54; ++index)
+			{
+				if (chargedPlayer.inventory[index].type == ModContent.ItemType<UnchargedMagnet>() && chargedPlayer.inventory[index].stack > 0)
+				{
+					chargedPlayer.inventory[index].stack--;
+					if (chargedPlayer.inventory[index].stack <= 0)
+						chargedPlayer.inventory[index].TurnToAir();
+
+					Item.NewItem(npc.GetSource_Death(), chargedPlayer.Center, ModContent.ItemType<ChargedMagnet>());
+
+					break;
+				}
+			}
+		}
+	}
+}
