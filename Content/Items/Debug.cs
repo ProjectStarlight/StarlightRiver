75f249e6826c9d18c5397329b77d063140951d95
GraydeeSi <wiseguy0002@gmail.com>
2023-01-16 18:43:29 -0500

    Reintroduced debug stick

---
+++ Content/Items/Debug.cs
@@ -0,0 +1,114 @@
+﻿using StarlightRiver.Content.Tiles.Vitric;
+using StarlightRiver.Helpers;
+using System.Drawing;
+using System.Linq;
+using Terraria.DataStructures;
+using Terraria.ID;
+
+namespace StarlightRiver.Content.Items
+{
+	class DebugStick : ModItem
+	{
+		public override string Texture => AssetDirectory.Assets + "Items/DebugStick";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Debug Stick");
+			Tooltip.SetDefault("Developer item");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 10;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 38;
+			Item.height = 40;
+			Item.useTime = 18;
+
+			Item.useAnimation = 18;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 5f;
+			Item.value = 1000;
+			Item.rare = ItemRarityID.LightRed;
+			Item.autoReuse = true;
+			Item.UseSound = SoundID.Item18;
+			Item.useTurn = true;
+			Item.accessory = true;
+
+			//Item.createTile = ModContent.TileType<Tiles.CrashTech.CrashPod>();
+		}
+
+		public override bool? UseItem(Player player)
+		{
+			int cX = (int)Main.MouseWorld.X / 16;
+			int cY = (int)Main.MouseWorld.Y / 16;
+
+            StructureHelper.Generator.GenerateStructure("Structures/EvasionShrine", new Point16(cX, cY), StarlightRiver.Instance);
+
+            return true;
+		}
+	}
+
+	class DebugModerEnabler : ModItem
+	{
+		public override string Texture => AssetDirectory.Assets + "Items/DebugStick";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Debug Mode");
+			Tooltip.SetDefault("Enables debug mode which does... stuff!\nHold Y to make bosses go at ludicrous speed.");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 10;
+			Item.width = 38;
+			Item.height = 38;
+			Item.useTime = 15;
+			Item.useAnimation = 15;
+			Item.useStyle = ItemUseStyleID.Swing;
+		}
+
+		public override bool? UseItem(Player Player)
+		{
+			StarlightRiver.debugMode = !StarlightRiver.debugMode;
+			return true;
+		}
+	}
+
+	class Eraser : ModItem
+	{
+		public override string Texture => AssetDirectory.Assets + "Items/DebugStick";
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Eraser");
+			Tooltip.SetDefault("Death");
+		}
+
+		public override void SetDefaults()
+		{
+			Item.damage = 10;
+			Item.DamageType = DamageClass.Melee;
+			Item.width = 38;
+			Item.height = 38;
+			Item.useTime = 2;
+			Item.useAnimation = 2;
+			Item.useStyle = ItemUseStyleID.Swing;
+			Item.knockBack = 5f;
+			Item.value = 1000;
+			Item.rare = ItemRarityID.LightRed;
+			Item.autoReuse = true;
+			Item.UseSound = SoundID.Item18;
+			Item.useTurn = true;
+			Item.accessory = true;
+		}
+
+		public override bool? UseItem(Player Player)
+		{
+			foreach (NPC NPC in Main.npc.Where(n => Vector2.Distance(n.Center, Main.MouseWorld) < 100))
+				NPC.StrikeNPC(99999, 0, 0, false, false, false);
+			return true;
+		}
+	}
+}
