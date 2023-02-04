a60be121ca055065e9bcd791546b62870307ad5e
bobfishkins23 <bobfishkins23@yahoo.com>
2023-02-04 15:55:42 -0500

    ScreenTarget system prototype

---
+++ Core/Systems/ScreenTargetSystem/ScreenTarget.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StarlightRiver.Core.Systems.ScreenTargetSystem
+{
+	internal class ScreenTarget
+	{
+		/// <summary>
+		/// What gets rendered to this screen target. Spritebatch is automatically started and RT automatically set, you only need to write the code for what you are rendering.
+		/// </summary>
+		public Action<SpriteBatch> drawFunct;
+
+		/// <summary>
+		/// If this render target should be rendered. Make sure this it as restrictive as possible to prevent uneccisary rendering work.
+		/// </summary>
+		public Func<bool> activeFunct;
+
+		/// <summary>
+		/// Optional function that runs when the screen is resized. Returns the size the render target should be.
+		/// </summary>
+		public Func<Vector2, Vector2> onResize;
+
+		/// <summary>
+		/// Where this render target should fall in the order of rendering. Important if you want to render something to chain into another RT.
+		/// </summary>
+		public float order;
+
+		public RenderTarget2D RenderTarget { get; set; }
+
+		public ScreenTarget(Action<SpriteBatch> draw, Func<bool> active, float order, Func<Vector2, Vector2> onResize = null)
+		{
+			drawFunct = draw;
+			activeFunct = active;
+			this.order = order;
+			this.onResize = onResize;
+
+			ScreenTargetHandler.AddTarget(this);
+		}
+	}
+}
---
+++ Core/Systems/ScreenTargetSystem/ScreenTargetHandler.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+
+namespace StarlightRiver.Core.Systems.ScreenTargetSystem
+{
+	internal class ScreenTargetHandler : ModSystem
+	{
+		public static List<ScreenTarget> targets = new();
+
+		public override void Load()
+		{
+			On.Terraria.Main.CheckMonoliths += RenderScreens;
+			Main.OnResolutionChanged += ResizeScreens;
+		}
+
+		/// <summary>
+		/// Registers a new screen target and orders it into the list. Called automatically by the constructor of ScreenTarget!
+		/// </summary>
+		/// <param name="toAdd"></param>
+		public static void AddTarget(ScreenTarget toAdd)
+		{
+			targets.Add(toAdd);
+			targets.Sort((a, b) => a.order - b.order > 0 ? 1 : -1);
+		}
+
+		/// <summary>
+		/// Removes a screen target from the targets list. Should not normally need to be used.
+		/// </summary>
+		/// <param name="toRemove"></param>
+		public static void RemoveTarget(ScreenTarget toRemove)
+		{
+			targets.Remove(toRemove);
+			targets.Sort((a, b) => a.order - b.order > 0 ? 1 : -1);
+		}
+
+		private void ResizeScreens(Vector2 obj)
+		{
+			targets.ForEach(n =>
+			{
+				Vector2 size = n.onResize != null ? obj : n.onResize(obj);
+
+				n.RenderTarget.Dispose();
+				n.RenderTarget = new RenderTarget2D(Main.instance.GraphicsDevice, (int)size.X, (int)size.Y, false, SurfaceFormat.Color, DepthFormat.None, 0, RenderTargetUsage.PreserveContents);
+			});
+		}
+
+		private void RenderScreens(On.Terraria.Main.orig_CheckMonoliths orig)
+		{
+			Main.spriteBatch.Begin();
+
+			foreach (ScreenTarget target in targets)
+			{
+				Main.graphics.GraphicsDevice.SetRenderTarget(target.RenderTarget);
+				Main.graphics.GraphicsDevice.Clear(Color.Transparent);
+
+				if (target.activeFunct())
+					target.drawFunct(Main.spriteBatch);
+
+				Main.graphics.GraphicsDevice.SetRenderTarget(null);
+			}
+
+			Main.spriteBatch.End();
+		}
+
+		public override void Unload()
+		{
+			targets.ForEach(n => n.RenderTarget.Dispose());
+			targets.Clear();
+			targets = null;
+		}
+	}
+}
