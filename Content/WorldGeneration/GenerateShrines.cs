fae480974a024a2ed9a8a36579b959796220e795
bobfishkins23 <bobfishkins23@yahoo.com>
2023-02-20 01:01:44 -0500

    Spawning restrictions

--- Content/WorldGeneration/GenerateShrines.cs
+++ Content/WorldGeneration/GenerateShrines.cs
@@ -1,5 +1,7 @@
 ﻿using StarlightRiver.Helpers;
+using System.Collections.Generic;
 using Terraria.DataStructures;
+using Terraria.ID;
 using Terraria.IO;
 using Terraria.WorldBuilding;
 
@@ -7,6 +9,19 @@ namespace StarlightRiver.Core
 {
 	public partial class StarlightWorld
 	{
+		public static readonly List<int> InvalidShrineTiles = new()
+		{
+			TileID.Sand,
+			TileID.Ebonstone,
+			TileID.Crimstone,
+			TileID.IceBlock,
+			TileID.SnowBlock,
+			TileID.Sandstone,
+			TileID.JungleGrass,
+			TileID.Mud,
+			TileID.MushroomGrass
+		};
+
 		public static void ShrineGen(GenerationProgress progress, GameConfiguration configuration)
 		{
 			for (int x = 0; x < Main.maxTilesX - 200; x += WorldGen.genRand.Next(200, 300))
@@ -16,7 +31,7 @@ namespace StarlightRiver.Core
 				Point16 dims = new();
 				StructureHelper.Generator.GetDimensions("Structures/CombatShrine", StarlightRiver.Instance, ref dims);
 
-				if (WorldGen.InWorld(x, y) && WorldGenHelper.IsRectangleSafe(new Rectangle(x, y, dims.X, dims.Y)))
+				if (WorldGen.InWorld(x, y) && WorldGenHelper.IsRectangleSafe(new Rectangle(x, y, dims.X, dims.Y), (a, b) => !InvalidShrineTiles.Contains(a.TileType)))
 					StructureHelper.Generator.GenerateStructure("Structures/CombatShrine", new Point16(x, y), StarlightRiver.Instance);
 			}
 		}
