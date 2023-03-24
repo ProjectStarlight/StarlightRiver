1913aacd39a5ecab0f391f83789f6256db55a32b
bobfishkins23 <bobfishkins23@yahoo.com>
2023-03-23 22:15:02 -0400

    Menu improvements

--- Core/Systems/BossRushSystem/BossRushGUIHack.cs
+++ Core/Systems/BossRushSystem/BossRushGUIHack.cs
@@ -1,5 +1,6 @@
 ﻿using StarlightRiver.Content.GUI;
 using StarlightRiver.Core.Loaders.UILoading;
+using Terraria.GameContent.UI.States;
 
 namespace StarlightRiver.Core.Systems.BossRushSystem
 {
@@ -17,45 +18,26 @@ namespace StarlightRiver.Core.Systems.BossRushSystem
 		{
 			if (inMenu && Main.keyState.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
 				inMenu = false;
-
-			orig();
 		}
 
 		private void DrawBossMenu(On.Terraria.Main.orig_DrawMenu orig, Main self, GameTime gameTime)
 		{
-			int a = Main.menuMode;
-
-			if (!inMenu)
-			{
-				orig(self, gameTime);
-
-				if (Main.menuMode == 888 && Main.ActivePlayerFileData.Path != null)
-				{
-					Main.spriteBatch.Begin(default, default, default, default, default, default, Main.UIScaleMatrix);
-
-					UILoader.GetUIState<BossRushButton>().UserInterface.Update(gameTime);
-					UILoader.GetUIState<BossRushButton>().Draw(Main.spriteBatch);
-
-					Main.DrawCursor(Main.DrawThickCursor());
-
-					Main.spriteBatch.End();
-				}
-
-				return;
-			}
-
-			Main.menuMode = -1;
+			if (inMenu)
+				Main.MenuUI.SetState(UILoader.GetUIState<BossRushMenu>());
 
 			orig(self, gameTime);
 
-			Main.spriteBatch.Begin(default, default, default, default, default, default, Main.UIScaleMatrix);
+			if (Main.MenuUI.CurrentState is UIWorldSelect)
+			{
+				Main.spriteBatch.Begin(default, default, default, default, default, default, Main.UIScaleMatrix);
 
-			UILoader.GetUIState<BossRushMenu>().UserInterface.Update(gameTime);
-			UILoader.GetUIState<BossRushMenu>().Draw(Main.spriteBatch);
+				UILoader.GetUIState<BossRushButton>().UserInterface.Update(gameTime);
+				UILoader.GetUIState<BossRushButton>().Draw(Main.spriteBatch);
 
-			Main.DrawCursor(Main.DrawThickCursor());
+				Main.DrawCursor(Main.DrawThickCursor());
 
-			Main.spriteBatch.End();
+				Main.spriteBatch.End();
+			}
 		}
 	}
 }
