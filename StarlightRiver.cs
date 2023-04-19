96c6ec0d548a3f1f2d7632ed9e2db1cb6719835c
Setnour6 <set.nour6@gmail.com>
2023-04-19 13:16:24 -0400

    Fix errors
    I kept the port note in StarlightRiver.cs line 123, AddRecipeGroups. I found that it would be better to leave it there until further notice

--- StarlightRiver.cs
+++ StarlightRiver.cs
@@ -120,7 +120,7 @@ namespace StarlightRiver
 			}
 		}
 
-		public override void AddRecipeGroups()
+		public override void AddRecipeGroups()/* tModPorter Note: Removed. Use ModSystem.AddRecipeGroups */
 		{
 			foreach (IRecipeGroup group in recipeGroupCache)
 			{
