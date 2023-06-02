ebc1fab7b403573e192439daa2b37f7ca91707f2
bobfishkins23 <bobfishkins23@yahoo.com>
2023-06-02 00:33:06 -0400

    Key fix

--- Content/Items/Vitric/Misc.TempleEntranceKey.cs
+++ Content/Items/Vitric/Misc.TempleEntranceKey.cs
@@ -4,6 +4,6 @@
 	{
 		public override string Texture => AssetDirectory.VitricItem + Name;
 
-		public TempleEntranceKey() : base("Forge Key", "Opens a door to the Vitric Forge.", Terraria.ID.ItemRarityID.Quest, 0, 1) { }
+		public TempleEntranceKey() : base("Forge Key", "Opens a door to the Vitric Forge.", 1, 0, Terraria.ID.ItemRarityID.Quest) { }
 	}
 }
