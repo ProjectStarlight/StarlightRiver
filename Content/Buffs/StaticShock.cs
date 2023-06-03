fe79436786c417f967b8ab4c9751ca06df7e5b0e
SkippZzMods <pumpkinboo2k6@gmail.com>
2023-06-03 19:01:44 -0400

    copper & tin coil fix

--- Content/Buffs/StaticShock.cs
+++ Content/Buffs/StaticShock.cs
@@ -11,7 +11,9 @@ namespace StarlightRiver.Content.Buffs
 
 		public override void Update(NPC npc, ref int buffIndex)
 		{
-			npc.defense -= 4 + Math.Min((int)(npc.buffTime[buffIndex] * 0.1f), 5);
+			int decrease = 4 + Utils.Clamp(Math.Min((int)(npc.buffTime[buffIndex] * 0.1f), 5), 0, 5);
+
+			npc.defense = Utils.Clamp(npc.defense - decrease, 0, npc.defense); // i think this should work (you usually dont actually decrease defense for these types of things iirc)
 			if (Main.rand.NextBool(9))
 			{
 				Vector2 around = npc.Center + Main.rand.NextVector2Circular(npc.width, npc.height) * 1.1f;
@@ -22,7 +24,7 @@ namespace StarlightRiver.Content.Buffs
 
 		public override void Update(Player player, ref int buffIndex)
 		{
-			player.statDefense -= 4 + Math.Min(player.buffTime[buffIndex] * 1, 5);
+			player.statDefense -= 4 + Utils.Clamp(Math.Min(player.buffTime[buffIndex] * 1, 5), 0, 5);
 			if (Main.rand.NextBool(9))
 			{
 				Vector2 around = player.Center + Main.rand.NextVector2Circular(player.width, player.height) * 1.1f;
