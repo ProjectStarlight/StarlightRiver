2a995ef84a332c896d2c83798fe0390c043fc3b0
bobfishkins23 <bobfishkins23@yahoo.com>
2023-06-19 21:40:47 -0400

    Vitric arrow head

--- Content/Items/Vitric/Accessories.GlassArrowhead.cs
+++ Content/Items/Vitric/Accessories.GlassArrowhead.cs
@@ -27,8 +27,8 @@ namespace StarlightRiver.Content.Items.Vitric
 				{
 					Vector2 velocity = proj.velocity.RotatedByRandom(MathHelper.Pi / 6f);
 					velocity *= Main.rand.NextFloat(0.5f, 0.75f);
-					//Projectile.NewProjectile(Player.GetSource_Accessory(Item), proj.Center, velocity, ModContent.ProjectileType<Vitric.VitricArrowShattered>(), (int)(damage * 0.2f), knockback * 0.15f, Player.whoAmI);
-					//TODO: Replace with new projectile
+
+					Projectile.NewProjectile(Player.GetSource_Accessory(Item), proj.Center, velocity * 2, ModContent.ProjectileType<GlassShard>(), (int)(damageDone * 0.1f), 0.1f, Player.whoAmI);
 				}
 			}
 		}
@@ -46,6 +46,99 @@ namespace StarlightRiver.Content.Items.Vitric
 			recipe.AddIngredient(ModContent.ItemType<VitricOre>(), 5);
 			recipe.AddTile(TileID.Furnaces);
 			recipe.Register();
+		}
+	}
+
+	public class GlassShard : ModProjectile, IDrawAdditive
+	{
+		Vector2 savedVelocity;
+
+		public override string Texture => AssetDirectory.VitricBoss + "GlassSpike";
+
+		public override void SetDefaults()
+		{
+			Projectile.friendly = true;
+			Projectile.width = 22;
+			Projectile.height = 22;
+			Projectile.penetrate = 2;
+			Projectile.timeLeft = 60;
+			Projectile.tileCollide = true;
+			Projectile.ignoreWater = true;
+
+			Projectile.scale = 0.5f;
+		}
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Glass Spike");
+		}
+
+		public override void AI()
+		{
+			if (Projectile.timeLeft == 60)
+			{
+				savedVelocity = Projectile.velocity;
+				Projectile.velocity *= 0;
+			}
+
+			if (Projectile.timeLeft > 50)
+				Projectile.velocity = Vector2.SmoothStep(Vector2.Zero, savedVelocity, (30 - (Projectile.timeLeft - 50)) / 30f);
+
+			Color color = Helpers.Helper.MoltenVitricGlow(MathHelper.Min(200 - Projectile.timeLeft, 120));
+
+			if (Projectile.timeLeft < 45)
+			{
+				for (int k = 0; k <= 1; k++)
+				{
+					if (Main.rand.NextBool(3))
+					{
+						Vector2 pos = Projectile.Center + Vector2.Normalize(Projectile.velocity).RotatedBy(1.57f) * (k == 0 ? 10f : -10f);
+						var d = Dust.NewDustPerfect(pos, ModContent.DustType<Dusts.GlowLine>(), (Projectile.velocity * Main.rand.NextFloat(-0.5f, -0.02f)).RotatedBy(k == 0 ? -0.4f : 0.4f), 0, color, 1f);
+						d.customData = 0.85f;
+					}
+				}
+			}
+
+			Projectile.rotation = Projectile.velocity.ToRotation() + 3.14f / 4;
+		}
+
+		public override void ModifyHitPlayer(Player target, ref Player.HurtModifiers modifiers)
+		{
+			target.AddBuff(BuffID.Bleeding, 300);
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			Color color = Helpers.Helper.MoltenVitricGlow(MathHelper.Min(200 - Projectile.timeLeft, 120));
+
+			for (int k = 0; k <= 10; k++)
+			{
+				Dust.NewDust(Projectile.position, 22, 22, ModContent.DustType<Dusts.GlassGravity>(), Projectile.velocity.X * 0.5f, Projectile.velocity.Y * 0.5f);
+				Dust.NewDust(Projectile.position, 22, 22, ModContent.DustType<Dusts.Glow>(), 0, 0, 0, color, 0.3f);
+			}
+
+			Terraria.Audio.SoundEngine.PlaySound(SoundID.Shatter, Projectile.Center);
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			SpriteBatch spriteBatch = Main.spriteBatch;
+			Color color = Helpers.Helper.MoltenVitricGlow(MathHelper.Min(200 - Projectile.timeLeft, 120));
+
+			spriteBatch.Draw(ModContent.Request<Texture2D>(Texture).Value, Projectile.Center - Main.screenPosition, new Rectangle(0, 0, 22, 22), lightColor, Projectile.rotation, Vector2.One * 11, Projectile.scale, 0, 0);
+			spriteBatch.Draw(ModContent.Request<Texture2D>(Texture).Value, Projectile.Center - Main.screenPosition, new Rectangle(0, 22, 22, 22), color, Projectile.rotation, Vector2.One * 11, Projectile.scale, 0, 0);
+
+			return false;
+		}
+
+		public void DrawAdditive(SpriteBatch spriteBatch)
+		{
+			Texture2D tex = ModContent.Request<Texture2D>(Texture + "Glow").Value;
+			float alpha = Projectile.timeLeft > 160 ? 1 - (Projectile.timeLeft - 160) / 20f : 1;
+			Color color = Helpers.Helper.MoltenVitricGlow(MathHelper.Min(200 - Projectile.timeLeft, 120)) * alpha;
+
+			spriteBatch.Draw(tex, Projectile.Center + Vector2.Normalize(Projectile.velocity) * -40 - Main.screenPosition, tex.Frame(),
+				color * (Projectile.timeLeft / 140f), Projectile.rotation + 3.14f, tex.Size() / 2, 1.8f, 0, 0);
 		}
 	}
 }
