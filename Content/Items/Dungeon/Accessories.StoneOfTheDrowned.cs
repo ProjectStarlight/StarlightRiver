5f907de7b464e79a69779c8bac6730586acf79bb
SkippZzMods <pumpkinboo2k6@gmail.com>
2023-06-21 16:50:33 -0400

    finished

--- Content/Items/Dungeon/Accessories.StoneOfTheDrowned.cs
+++ Content/Items/Dungeon/Accessories.StoneOfTheDrowned.cs
@@ -1,5 +1,7 @@
 ﻿using ReLogic.Content;
 using StarlightRiver.Content.Items.BaseTypes;
+using StarlightRiver.Content.Items.Jungle;
+using StarlightRiver.Core.Systems.BarrierSystem;
 using System;
 using System.Collections.Generic;
 using System.Reflection;
@@ -7,9 +9,9 @@ using Terraria.DataStructures;
 using Terraria.GameContent;
 using Terraria.ID;
 
-namespace StarlightRiver.Content.Items.Jungle
+namespace StarlightRiver.Content.Items.Dungeon
 {
-	public class HauntedAmethyst : SmartAccessory
+	public class StoneOfTheDrowned : SmartAccessory
 	{
 		internal float slotsUsed; // used so you only summon the amount of slots just gained. Otherwise would populate all of the players remaining slots
 		// i love private terraria methods which require reflection to access
@@ -18,8 +20,8 @@ namespace StarlightRiver.Content.Items.Jungle
 
 		public override string Texture => AssetDirectory.JungleItem + Name;
 
-		public HauntedAmethyst() : base("Haunted Amethyst", "Increases your number of max minions by 1 when below 50% life\nAutomatically summons a minion if there is a valid weapon in your inventory") { }
-		
+		public StoneOfTheDrowned() : base("Stone of the Drowned", "+30 barrier\nIncreases your number of max minions by 2 when you have no barrier\nAutomatically summons two slots worth of minions if there is a valid weapon in your inventory") { }
+
 		public override void Load()
 		{
 			playerItemCheckShoot_Info = typeof(Player).GetMethod("ItemCheck_Shoot", BindingFlags.NonPublic | BindingFlags.Instance);
@@ -31,17 +33,18 @@ namespace StarlightRiver.Content.Items.Jungle
 
 		public override void SafeSetDefaults()
 		{
-			Item.value = Item.sellPrice(gold: 1);
-			Item.rare = ItemRarityID.Green;
+			Item.value = Item.sellPrice(gold: 2);
+			Item.rare = ItemRarityID.Orange;
 		}
 
 		public override void SafeUpdateEquip(Player Player)
 		{
-			if (Player.statLife < Player.statLifeMax2 * 0.5f)
+			Player.GetModPlayer<BarrierPlayer>().maxBarrier += 30;
+			if (Player.GetModPlayer<BarrierPlayer>().barrier <= 0)
 			{
-				Player.maxMinions += 1;
+				Player.maxMinions += 2;
 
-				if (Player.slotsMinions < Player.maxMinions && slotsUsed < 1)
+				if (Player.slotsMinions < Player.maxMinions && slotsUsed < 2)
 				{
 					Item summoningItem = null;
 
@@ -53,7 +56,7 @@ namespace StarlightRiver.Content.Items.Jungle
 
 						dummyProj.SetDefaults(item.shoot);
 
-						if (item.CountsAsClass(DamageClass.Summon) && dummyProj.minion && dummyProj.minionSlots <= 1f)
+						if (item.CountsAsClass(DamageClass.Summon) && dummyProj.minion && dummyProj.minionSlots <= 2f)
 						{
 							summoningItem = item;
 							break;
@@ -66,7 +69,7 @@ namespace StarlightRiver.Content.Items.Jungle
 
 						dummyProj.SetDefaults(summoningItem.shoot);
 
-						for (int i = 0; i < 1 / dummyProj.minionSlots; i++)
+						for (int i = 0; i < 2 / dummyProj.minionSlots; i++)
 						{
 							playerItemCheckShoot(Player, Player.whoAmI, summoningItem, summoningItem.damage);
 							for (int d = 0; d < 5; d++)
@@ -81,13 +84,18 @@ namespace StarlightRiver.Content.Items.Jungle
 					}
 				}
 			}
+			else if (slotsUsed > 0)
+			{
+				slotsUsed = 0;
+			}
 		}
 
 		public override void AddRecipes()
 		{
 			CreateRecipe().
-				AddIngredient(ItemID.Amethyst, 5).
-				AddTile(TileID.DemonAltar).
+				AddIngredient<HauntedAmethyst>().
+				AddIngredient<AquaSapphire>().
+				AddTile(TileID.TinkerersWorkbench).
 				Register();
 		}
 	}
