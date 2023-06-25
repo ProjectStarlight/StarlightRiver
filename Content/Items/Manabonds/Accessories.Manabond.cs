ead9cc5c1e9a1af83ed074020a39b913d406280b
bobfishkins23 <bobfishkins23@yahoo.com>
2023-06-24 23:26:04 -0400

    Basic manabond

---
+++ Content/Items/Manabonds/Accessories.Manabond.cs
@@ -0,0 +1,164 @@
+﻿using StarlightRiver.Content.Items.BaseTypes;
+using System;
+using System.Linq;
+
+namespace StarlightRiver.Content.Items.Manabonds
+{
+	/// <summary>
+	/// An abstract class for which to build manabond accessories off of
+	/// </summary>
+	internal abstract class Manabond : SmartAccessory
+	{
+		public Manabond(string name, string tooltip) : base(name, tooltip) { }
+
+		/// <summary>
+		/// The behavior that the minion should follow while effected by this Manabond.
+		/// </summary>
+		/// <param name="minion">The minion to effect the behavior of</param>
+		/// <param name="mp">The ManabondProjectile on this minion</param>
+		public abstract void MinionAI(Projectile minion, ManabondProjectile mp);
+
+		/// <summary>
+		/// Used to find the target that a given minion should use for it's magical attack target.
+		/// By default this checks if an NPC is active, hostile, can be targeted, and has LoS from the minion.
+		/// </summary>
+		/// <param name="minion">The minion to find the target for</param>
+		/// <param name="mp">The ManabondProjectile of this minion</param>
+		/// <returns>The desired target, or null if no valid targets</returns>
+		public virtual NPC FindTarget(Projectile minion, ManabondProjectile mp)
+		{
+			Player owner = Main.player[minion.owner];
+			int range = 800;
+
+			// Check player's target first, and prioritize that if possible
+			if (owner?.HasMinionAttackTargetNPC ?? false)
+			{
+				NPC npc = Main.npc[owner.MinionAttackTargetNPC];
+
+				if (Vector2.Distance(minion.Center, npc.Center) <= range && Collision.CanHit(minion.Center + Vector2.UnitY * -10, 1, 1, npc.Center, 1, 1))
+					return npc;
+			}
+
+			// Else, scan for other valid NPCs
+			foreach (NPC npc in Main.npc)
+			{
+				if (!npc.active || npc.friendly || !npc.CanBeChasedBy(this))
+					continue;
+
+				if (Vector2.Distance(minion.Center, npc.Center) <= range && Collision.CanHit(minion.Center + Vector2.UnitY * -10, 1, 1, npc.Center, 1, 1))
+					return npc;
+			}
+
+			return null;
+		}
+
+		public override bool SafeCanAccessoryBeEquippedWith(Item equipped, Item incoming, Player player)
+		{
+			if (equipped.ModItem is Manabond)
+				return false;
+
+			return true;
+		}
+	}
+
+	/// <summary>
+	/// This GlobalProjectile handles the mana system and AI for minions under the effect of a manabond accessory.
+	/// This should only ever do anything if a minion's owner has a valid manabond equipped.
+	/// </summary>
+	internal class ManabondProjectile : GlobalProjectile
+	{
+		public int mana;
+		public int maxMana = 50;
+
+		public int timer = 0;
+
+		public NPC target;
+
+		public override bool InstancePerEntity => true;
+
+		public override bool AppliesToEntity(Projectile entity, bool lateInstantiation)
+		{
+			return entity.minion;
+		}
+
+		/// <summary>
+		/// Determines if this minion's owner has a manabond on, and thus should have their mana siphoned
+		/// </summary>
+		/// <param name="projectile">The minion to check the owner's status of</param>
+		/// <returns>If the minions owner has a manabond equipped</returns>
+		public bool ManaActive(Projectile projectile)
+		{
+			Player owner = Main.player[projectile.owner];
+			return owner.armor.Any(n => n.ModItem is Manabond); // Checks if the player has any manabond on
+		}
+
+		/// <summary>
+		/// Retrieves the currently equipped manabond of the given projectile's owner, or null if one is not equipped
+		/// </summary>
+		/// <param name="projectile">The minion to check the owner's manabond of</param>
+		/// <returns>The equipped manabond if one is equipped, or null if not</returns>
+		public Manabond GetActiveBond(Projectile projectile)
+		{
+			if (!ManaActive(projectile))
+				return null;
+
+			Player owner = Main.player[projectile.owner];
+
+			return owner.armor.FirstOrDefault(n => n.ModItem is Manabond).ModItem as Manabond;
+		}
+
+		public override void PostAI(Projectile projectile)
+		{
+			if (!ManaActive(projectile))
+				return;
+
+			Manabond bond = GetActiveBond(projectile);
+			Player owner = Main.player[projectile.owner];
+
+			timer++;
+
+			// Drop target attempt
+			if (target != null && (!target.active || Vector2.Distance(projectile.Center, target.Center) > 800))
+				target = null;
+
+			// Retarget attempt
+			if (target is null)
+				target = bond?.FindTarget(projectile, this);
+
+			// Mana siphon attempt
+			if (timer % 5 == 0 && owner.statMana >= 5)
+			{
+				int toSiphon = Math.Min(1, maxMana - mana);
+
+				mana += toSiphon;
+				owner.statMana -= toSiphon;
+
+				if (toSiphon > 0 && owner.manaRegenDelay < 15)
+					owner.manaRegenDelay = 15;
+			}
+
+			// hand off to bond AI
+			bond?.MinionAI(projectile, this);
+		}
+
+		public override void PostDraw(Projectile projectile, Color lightColor)
+		{
+			if (!ManaActive(projectile))
+				return;
+
+			float fill = mana / (float)maxMana;
+
+			Texture2D tex = ModContent.Request<Texture2D>(AssetDirectory.GUI + "SmallBar1").Value;
+			Texture2D tex2 = ModContent.Request<Texture2D>(AssetDirectory.GUI + "SmallBar0").Value;
+
+			var pos = (projectile.Center + new Vector2(-tex.Width / 2, -40) + Vector2.UnitY * projectile.height / 2f - Main.screenPosition).ToPoint();
+			var target = new Rectangle(pos.X, pos.Y, (int)(fill * tex.Width), tex.Height);
+			var source = new Rectangle(0, 0, (int)(fill * tex.Width), tex.Height);
+			var target2 = new Rectangle(pos.X, pos.Y + 2, tex2.Width, tex2.Height);
+			var color = Vector3.Lerp(Color.Purple.ToVector3(), Color.Aqua.ToVector3(), fill);
+
+			Main.spriteBatch.Draw(tex2, target2, new Color(40, 40, 40));
+			Main.spriteBatch.Draw(tex, target, source, new Color(color.X, color.Y, color.Z));
+		}
+	}
+}
---
+++ Content/Items/Manabonds/BasicManabond.cs
@@ -0,0 +1,155 @@
+﻿using StarlightRiver.Helpers;
+using System.Collections.Generic;
+using Terraria.Graphics.Effects;
+using Terraria.ID;
+
+namespace StarlightRiver.Content.Items.Manabonds
+{
+	internal class BasicManabond : Manabond
+	{
+		public override string Texture => AssetDirectory.ManabondItem + Name;
+
+		public BasicManabond() : base("Manabond", "Your minions can store 50 mana\nYour minions siphon 6 mana per second from you untill full\nYour minions spend 10 mana to attack with a magic bolt occasionally") { }
+
+		public override void SafeSetDefaults()
+		{
+			Item.value = Item.sellPrice(gold: 5);
+			Item.rare = ItemRarityID.Blue;
+		}
+
+		public override void MinionAI(Projectile minion, ManabondProjectile mp)
+		{
+			if (mp.timer % 60 == 0 && mp.mana >= 10 && mp.target != null)
+			{
+				mp.mana -= 10;
+				int i = Projectile.NewProjectile(minion.GetSource_FromThis(), minion.Center, Vector2.Normalize(mp.target.Center - minion.Center).RotatedByRandom(0.5f) * 15, ModContent.ProjectileType<MagicBolt>(), 12, 0.25f, minion.owner);
+
+				var bolt = Main.projectile[i].ModProjectile as MagicBolt;
+				bolt.target = mp.target;
+			}
+		}
+	}
+
+	internal class MagicBolt : ModProjectile, IDrawPrimitive
+	{
+		private List<Vector2> cache;
+		private Trail trail;
+
+		public NPC target;
+
+		public ref float State => ref Projectile.ai[0];
+
+		public override string Texture => AssetDirectory.Invisible;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Magical bolt");
+			ProjectileID.Sets.TrailCacheLength[Projectile.type] = 2;
+			ProjectileID.Sets.TrailingMode[Projectile.type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 16;
+			Projectile.height = 16;
+			Projectile.friendly = true;
+			Projectile.DamageType = DamageClass.Summon;
+			Projectile.timeLeft = 120;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+		}
+
+		public override void AI()
+		{
+			if (State == 0)
+			{
+				Projectile.rotation = Projectile.velocity.ToRotation();
+
+				Projectile.velocity += Vector2.Normalize(target.Center - Projectile.Center);
+
+				if (Projectile.velocity.Length() > 15f)
+					Projectile.velocity = Vector2.Normalize(Projectile.velocity) * 15;
+
+				if (Main.rand.NextBool(4))
+				{
+					var d = Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<Dusts.Aurora>(), Projectile.velocity.RotatedByRandom(0.1f) * 0.5f, 0, new Color(0, 100, 200) * 0.6f);
+					d.customData = Main.rand.NextFloat(0.6f, 1f);
+				}
+			}
+			else if (State == 1)
+			{
+				Projectile.velocity *= 0;
+			}
+
+			ManageCaches();
+			ManageTrail();
+		}
+
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		{
+			State = 1;
+			Projectile.friendly = false;
+
+			var d = Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<Dusts.Aurora>(), Main.rand.NextVector2Circular(2, 2), 0, new Color(40, 200, 255));
+			d.customData = 1.2f;
+		}
+
+		private void ManageCaches()
+		{
+			if (cache == null)
+			{
+				cache = new List<Vector2>();
+
+				for (int i = 0; i < 30; i++)
+				{
+					cache.Add(Projectile.Center);
+				}
+			}
+
+			cache.Add(Projectile.Center);
+
+			while (cache.Count > 30)
+			{
+				cache.RemoveAt(0);
+			}
+		}
+
+		private void ManageTrail()
+		{
+			trail ??= new Trail(Main.instance.GraphicsDevice, 30, new TriangularTip(40 * 4), factor => factor * 12, factor =>
+			{
+				float alpha = 1;
+
+				if (factor.X > 0.8f)
+					alpha = 1 + (factor.X - 0.8f) * 30;
+
+				if (factor.X >= 0.95f)
+					alpha = 0;
+
+				if (Projectile.timeLeft < 15)
+					alpha *= Projectile.timeLeft / 15f;
+
+				return new Color(40, 50 + (int)(factor.X * 100), 255) * factor.X * alpha;
+			});
+
+			trail.Positions = cache.ToArray();
+			trail.NextPosition = Projectile.Center + Projectile.velocity;
+		}
+
+		public void DrawPrimitives()
+		{
+			Effect effect = Filters.Scene["CeirosRing"].GetShader().Shader;
+
+			var world = Matrix.CreateTranslation(-Main.screenPosition.Vec3());
+			Matrix view = Main.GameViewMatrix.TransformationMatrix;
+			var projection = Matrix.CreateOrthographicOffCenter(0, Main.screenWidth, Main.screenHeight, 0, -1, 1);
+
+			effect.Parameters["time"].SetValue(Main.GameUpdateCount * 0.05f);
+			effect.Parameters["repeats"].SetValue(2f);
+			effect.Parameters["transformMatrix"].SetValue(world * view * projection);
+			effect.Parameters["sampleTexture"].SetValue(ModContent.Request<Texture2D>("StarlightRiver/Assets/EnergyTrail").Value);
+
+			trail?.Render(effect);
+		}
+	}
+}
