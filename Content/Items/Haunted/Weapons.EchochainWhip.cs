e3ea8ff1f058001a937c79998a2acfbb17dabd74
bobfishkins23 <bobfishkins23@yahoo.com>
2023-06-28 21:45:49 -0400

    Chain graph

--- Content/Items/Haunted/Weapons.EchochainWhip.cs
+++ Content/Items/Haunted/Weapons.EchochainWhip.cs
@@ -103,9 +103,9 @@ namespace StarlightRiver.Content.Items.Haunted
 				if (i > 0 && i < tipPositions.Count)
 				{
 					whipFrame.Y = height * 4;
-					Color color = Color.Lerp(new Color(20, 135, 15, 0), new Color(100, 200, 10, 0), fade);
+					var color = Color.Lerp(new Color(20, 135, 15, 0), new Color(100, 200, 10, 0), fade);
 					Main.EntitySpriteDraw(texture.Value, tipPositions[i] - Main.screenPosition, whipFrame, Color.White * fade * fadeOut, tipRotations[i], whipFrame.Size() * 0.5f, Projectile.scale, SpriteEffects.None, 0);
-					
+
 					Main.spriteBatch.Draw(texBlur, tipPositions[i] - Main.screenPosition, null, Color.White with { A = 0 } * fade * fadeOut, tipRotations[i], texBlur.Size() / 2f, Projectile.scale, 0f, 0f);
 
 					Main.spriteBatch.Draw(bloomTex, tipPositions[i] - Main.screenPosition, null, color * fade * fadeOut, 0f, bloomTex.Size() / 2f, 1f * fade, 0f, 0f);
@@ -116,7 +116,7 @@ namespace StarlightRiver.Content.Items.Haunted
 
 					if (i < 15 && i + 1 < tipPositions.Count)
 					{
-						Vector2 newPosition = Vector2.Lerp(tipPositions[i], tipPositions[i + 1], 0.5f);
+						var newPosition = Vector2.Lerp(tipPositions[i], tipPositions[i + 1], 0.5f);
 						float newRotation = MathHelper.Lerp(tipRotations[i], tipRotations[i + 1], 0.5f);
 
 						Main.EntitySpriteDraw(texture.Value, newPosition - Main.screenPosition, whipFrame, Color.White * fade * fadeOut, newRotation, whipFrame.Size() * 0.5f, Projectile.scale, SpriteEffects.None, 0);
@@ -140,10 +140,169 @@ namespace StarlightRiver.Content.Items.Haunted
 
 		public override void Kill(int timeLeft)
 		{
-
+			EchochainSystem.AddNPCS(hitTargets);
+			hitTargets.Clear();
 		}
 	}
 
+	/// <summary>
+	/// This system acts as a handler for the echo chain damage graph
+	/// </summary>
+	class EchochainSystem : ModSystem
+	{
+		// These represent the graph for the echo chain's connection. Essentially
+		// every NPC acts as a node, and are connected by edges which are created
+		// when NPCs are struck by the weapon. Damage sharing is handled via a
+		// graph traversal, and chain expiration happens as a simple iteration over
+		// the edges collection and removing expired edges.
+		public static List<EchochainNode> nodes = new();
+		public static List<EchochainEdge> edges = new();
+
+		public override void Load()
+		{
+			StarlightNPC.OnHitByItemEvent += OnHitChains;
+			StarlightNPC.OnHitByProjectileEvent += OnHitChainsProj;
+		}
+
+		/// <summary>
+		/// Triggers a traversal over the graph to damage all enemies connected to the hit enemy
+		/// </summary>
+		/// <param name="npc"></param>
+		/// <param name="player"></param>
+		/// <param name="item"></param>
+		/// <param name="hit"></param>
+		/// <param name="damageDone"></param>
+		private void OnHitChains(NPC npc, Player player, Item item, NPC.HitInfo hit, int damageDone)
+		{
+			Traverse(npc, (n) => n.SimpleStrikeNPC(damageDone, 0, false), true);
+		}
+
+		/// <summary>
+		/// Triggers a traversal over the graph to damage all enemies connected to the hit enemy
+		/// </summary>
+		/// <param name="npc"></param>
+		/// <param name="projectile"></param>
+		/// <param name="hit"></param>
+		/// <param name="damageDone"></param>
+		private void OnHitChainsProj(NPC npc, Projectile projectile, NPC.HitInfo hit, int damageDone)
+		{
+			Traverse(npc, (n) => n.SimpleStrikeNPC(damageDone, 0, false), true);
+		}
+
+		public override void PostUpdateEverything()
+		{
+			foreach (EchochainEdge edge in edges)
+			{
+				edge.timer--;
+			}
+
+			edges.RemoveAll(n => n.timer <= 0);
+		}
+
+		/// <summary>
+		/// This function will create appropriate nodes and edges for NPCs when they are
+		/// struck by the weapon. All NPCs passed not currently connected will have connections
+		/// added to them.
+		/// </summary>
+		/// <param name="npcsToAdd"></param>
+		public static void AddNPCS(List<NPC> npcsToAdd)
+		{
+			List<EchochainNode> list = new();
+
+			for (int i = 0; i < npcsToAdd.Count; i++)
+			{
+				NPC npc = npcsToAdd[i];
+
+				// First we check if a node already exists for this NPC, and if not, add it
+				EchochainNode node = nodes.FirstOrDefault(n => n.npc == npc);
+
+				if (node == null)
+				{
+					node = new EchochainNode(new List<EchochainEdge>(), npc);
+					nodes.Add(node);
+				}
+
+				list.Add(node);
+			}
+
+			// Now we check all unordered permutations of 2 NPCs, and generate chains between
+			// them if they do not already exist
+			for (int i = 0; i < list.Count; i++)
+			{
+				for (int j = i; j < list.Count; j++)
+				{
+					// This is likely very slightly inefficient over performing the equality
+					// check here, but this is sparsely run and this is far more readable
+					var potential = new EchochainEdge(list[i], list[j], 180);
+
+					if (!edges.Any(n => n == potential))
+					{
+						edges.Add(potential);
+
+						// We append the refferences to this edge to the nodes it is connecting aswell
+						nodes.First(n => n == list[i]).edges.Add(potential);
+						nodes.First(n => n == list[j]).edges.Add(potential);
+					}
+				}
+			}
+		}
+
+		/// <summary>
+		/// Triggers a DFS traversal of the graph starting with the node containing the given
+		/// NPC, and performing the given action on any NPC connected.
+		/// </summary>
+		/// <param name="start">The NPC where the traversal should start from. If they are not present in the node colleciton, nothing happens</param>
+		/// <param name="action">The action to be performed on every connected NPC</param>
+		/// <param name="ignoreFirst">If the NPC passed should ignore the action passed or not</param>
+		public static void Traverse(NPC start, Action<NPC> action, bool ignoreFirst = false)
+		{
+			EchochainNode startNode = nodes.First(n => n.npc == start);
+
+			// If the start isnt in the graph, give up
+			if (startNode is null)
+				return;
+
+			Stack<EchochainNode> stack = new();
+			HashSet<EchochainNode> visited = new();
+
+			stack.Push(startNode);
+
+			while (stack.Count > 0)
+			{
+				EchochainNode node = stack.Pop();
+
+				if (!visited.Contains(node))
+				{
+					if (!(ignoreFirst && node == startNode))
+						action(node.npc);
+
+					foreach (EchochainEdge edge in node.edges)
+					{
+						EchochainNode adjNode = edge.start == node ? edge.end : edge.start;
+
+						if (!visited.Contains(adjNode))
+							stack.Push(adjNode);
+					}
+
+					visited.Add(node);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Removes an NPC from the node list, and disconnects all edges connected to it.
+		/// </summary>
+		/// <param name="toRemove">The NPC to be removed</param>
+		public static void RemoveNPC(NPC toRemove)
+		{
+			nodes.RemoveAll(n => n.npc == toRemove);
+			edges.RemoveAll(n => n.start.npc == toRemove || n.end.npc == toRemove);
+		}
+	}
+
+	/// <summary>
+	/// Represents an edge in the echo chain graph, essentially a chain between two NPCs
+	/// </summary>
 	class EchochainEdge
 	{
 		public EchochainNode start;
@@ -156,8 +315,29 @@ namespace StarlightRiver.Content.Items.Haunted
 			this.end = end;
 			this.timer = timer;
 		}
+
+		/// <summary>
+		/// Checks the equality of two edges. Since this graph is undirected, if the order is inversed
+		/// it should still count as equal to that edge.
+		/// </summary>
+		/// <param name="obj">The other edge to compare to</param>
+		/// <returns></returns>
+		public override bool Equals(object obj)
+		{
+			if (obj is EchochainEdge edge)
+			{
+				return
+					start == edge.start && end == edge.end ||
+					start == edge.end && end == edge.start;
+			}
+
+			return false;
+		}
 	}
 
+	/// <summary>
+	/// Represents a node in the echo chain graph, essentially a wrapper around an NPC
+	/// </summary>
 	class EchochainNode
 	{
 		public List<EchochainEdge> edges;
@@ -167,65 +347,6 @@ namespace StarlightRiver.Content.Items.Haunted
 		{
 			this.edges = edges;
 			this.npc = npc;
-		}
-	}
-
-	class EchochainSystem : ModSystem
-	{
-		public static List<EchochainNode> nodes = new();
-		public static List<EchochainEdge> edges = new();
-
-		public override void PostUpdateEverything()
-		{
-			
-		}
-
-		public static void AddNPCS(List<NPC> npcsToAdd)
-		{
-			List<EchochainNode> list = new();
-
-			for (int i = 0; i < npcsToAdd.Count; i++)
-			{
-				NPC npc = npcsToAdd[i];
-
-				EchochainNode node = nodes.FirstOrDefault(n => n.npc == npc);
-
-				if (node == null)
-				{
-					node = new EchochainNode(new List<EchochainEdge>(), npc);
-					nodes.Add(node);
-				}
-
-				list.Add(node);
-			}
-
-			for (int i = 0; i < list.Count; i++)
-			{
-				
-			}
-		}
-	}
-
-	class EchochainGlobalNPC : GlobalNPC
-	{
-		public override void OnHitByProjectile(NPC npc, Projectile projectile, NPC.HitInfo hit, int damageDone)
-		{
-			/*EchochainChain chainToUse = null;
-
-			foreach (EchochainChain chain in EchochainSystem.chains)
-			{
-				if (chain.chainedTargets.Contains(npc))
-				{
-					chainToUse = chain;
-					break;
-				}
-			}
-
-			if (chainToUse != null)
-			{
-				EchochainChainAttack attack = new(60, npc, chainToUse.chainedTargets);
-				EchochainSystem.attacks.Add(attack);
-			}*/
 		}
 	}
 }
