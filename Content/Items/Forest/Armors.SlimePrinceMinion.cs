fe59da53c888ac8b770960d948196001fb48a44d
bobfishkins23 <bobfishkins23@yahoo.com>
2023-06-29 23:16:05 -0400

    Attack logic

--- Content/Items/Forest/Armors.SlimePrinceMinion.cs
+++ Content/Items/Forest/Armors.SlimePrinceMinion.cs
@@ -1,13 +1,17 @@
-﻿namespace StarlightRiver.Content.Items.Forest
+﻿using StarlightRiver.Helpers;
+using Terraria.Audio;
+using Terraria.ID;
+
+namespace StarlightRiver.Content.Items.Forest
 {
 	public class SlimePrinceMinion : ModProjectile
 	{
 		public const int MAX_LIFE = 400;
 
 		public int life = MAX_LIFE;
-		public bool fused;
 
 		public Vector2 startPoint;
+		public NPC target;
 
 		public Player Owner => Main.player[Projectile.owner];
 		public bool Merged => State == 3;
@@ -68,6 +72,16 @@
 
 			if (Projectile.velocity.Length() > 15)
 				Projectile.velocity = Vector2.Normalize(Projectile.velocity) * 14.99f;
+
+			// Check target
+			target = Projectile.TargetStrongestNPC(800, true, true);
+
+			// Switch to attack mode if applicable
+			if (target != null)
+			{
+				State = 1;
+				Timer = 0;
+			}
 		}
 
 		/// <summary>
@@ -78,6 +92,65 @@
 		{
 			if (HealTimer % 2 == 0 && life < MAX_LIFE)
 				life++;
+
+			// If we need to retarget, do so and reset the attack
+			if (!Projectile.TargetValid(800, true, true, target))
+			{
+				target = Projectile.TargetStrongestNPC(800, true, true);
+				Timer = 0;
+			}
+
+			// return to idle if no target
+			if (target is null)
+			{
+				State = 0;
+				Timer = 0;
+				return;
+			}
+
+			// Otherwise, we follow the attack cycle
+			if (Timer == 1)
+			{
+				startPoint = Projectile.Center;
+			}
+
+			// Position above the target
+			if (Timer > 1 && Timer < 30)
+			{
+				Projectile.velocity *= 0;
+				Vector2 targetPos = target.Center + Vector2.UnitY * (target.height * -0.5f - 64);
+				Projectile.Center = Vector2.SmoothStep(startPoint, targetPos, Timer / 30f);
+			}
+
+			// Fall onto them
+			if (Timer > 30 && Timer < 90)
+			{
+				Projectile.velocity.Y += 0.4f;
+				Projectile.Center = new Vector2(target.Center.X, Projectile.Center.Y);
+
+				// Bounce off the enemy once it's been stomped on
+				if (Projectile.Hitbox.Intersects(target.Hitbox))
+				{
+					Projectile.velocity.Y = -7;
+					Projectile.velocity.X = Main.rand.NextFloat(-5, 5);
+					Timer = 90;
+				}
+			}
+
+			// Continue to experience gravity, albeit reduced
+			if (Timer >= 90)
+				Projectile.velocity.Y += 0.2f;
+
+			// Fire a thorn at them
+			if (Timer == 110)
+			{
+				Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.Center.DirectionTo(target.Center) * 11, ModContent.ProjectileType<SlimeThorn>(), 20, 0.5f, Projectile.owner);
+				SoundEngine.PlaySound(SoundID.DD2_DrakinShot, Projectile.Center);
+			}
+
+			// Do it all over again
+			if (Timer >= 120)
+				Timer = 0;
 		}
 
 		/// <summary>
@@ -124,7 +197,7 @@
 			Texture2D tex = ModContent.Request<Texture2D>(AssetDirectory.GUI + "SmallBar1").Value;
 			Texture2D tex2 = ModContent.Request<Texture2D>(AssetDirectory.GUI + "SmallBar0").Value;
 
-			var pos = (Projectile.Center + new Vector2(-tex.Width / 2, -40) + Vector2.UnitY * Projectile.height / 2f - Main.screenPosition).ToPoint();
+			var pos = (Projectile.Center + new Vector2(-tex.Width / 2, -50) + Vector2.UnitY * Projectile.height / 2f - Main.screenPosition).ToPoint();
 			var target = new Rectangle(pos.X, pos.Y, (int)(fill * tex.Width), tex.Height);
 			var source = new Rectangle(0, 0, (int)(fill * tex.Width), tex.Height);
 			var target2 = new Rectangle(pos.X, pos.Y + 2, tex2.Width, tex2.Height);
@@ -132,6 +205,28 @@
 
 			Main.spriteBatch.Draw(tex2, target2, new Color(40, 40, 40));
 			Main.spriteBatch.Draw(tex, target, source, new Color(color.X, color.Y, color.Z));
+		}
+	}
+
+	public class SlimeThorn : ModProjectile
+	{
+		public override string Texture => AssetDirectory.ForestItem + Name;
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 16;
+			Projectile.height = 16;
+			Projectile.friendly = true;
+			Projectile.hostile = false;
+			Projectile.timeLeft = 120;
+		}
+
+		public override void AI()
+		{
+			Projectile.rotation = Projectile.velocity.ToRotation() + 1.57f;
+
+			Projectile.velocity.Y += 0.01f;
+			Dust.NewDust(Projectile.position, 16, 16, DustID.t_Slime, 0, 0, 0, Color.Purple * 0.5f);
 		}
 	}
 }
