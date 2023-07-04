b5c04d7020fa7348f36ec9f09a38bb8f89f81b24
bobfishkins23 <bobfishkins23@yahoo.com>
2023-07-04 09:43:14 -0400

    New structures, manabond tile

---
+++ Content/Tiles/UndergroundTemple/ManabondTile.cs
@@ -0,0 +1,44 @@
+﻿using StarlightRiver.Content.Abilities;
+using StarlightRiver.Content.Items.Manabonds;
+using Terraria.ID;
+
+namespace StarlightRiver.Content.Tiles.UndergroundTemple
+{
+	class ManabondTile : ModTile, IHintable
+	{
+		public override string Texture => "StarlightRiver/Assets/Tiles/UndergroundTemple/" + Name;
+
+		public override void SetStaticDefaults()
+		{
+			TileID.Sets.DrawsWalls[Type] = true;
+			QuickBlock.QuickSetFurniture(this, 3, 3, DustID.MagicMirror, SoundID.DD2_DarkMageHealImpact, false, new Color(50, 50, 25), false, false, "Manabond");
+
+			RegisterItemDrop(ModContent.ItemType<BasicManabond>());
+		}
+
+		public override void NearbyEffects(int i, int j, bool closer)
+		{
+			if (Main.rand.NextBool(60))
+				Dust.NewDustPerfect(new Vector2(i, j) * 16 + new Vector2(Main.rand.Next(16), Main.rand.Next(16)), ModContent.DustType<Dusts.ArtifactSparkles.BlueArtifactSparkle>(), Vector2.Zero);
+		}
+
+		public override void MouseOver(int i, int j)
+		{
+			Player player = Main.LocalPlayer;
+			player.cursorItemIconID = ModContent.ItemType<BasicManabond>();
+			player.noThrow = 2;
+			player.cursorItemIconEnabled = true;
+		}
+
+		public override bool RightClick(int i, int j)
+		{
+			WorldGen.KillTile(i, j);
+			return true;
+		}
+
+		public string GetHint()
+		{
+			return "A magical scroll used to teach the non-magical magic by drawing from their master's mana. This could be useful!";
+		}
+	}
+}
