2a64730279676d123d46700f0b7cdf456ee95c0c
bobfishkins23 <bobfishkins23@yahoo.com>
2023-07-06 21:50:19 -0400

    Const for clarity

--- Content/Items/Beach/Acessories.SaltCogs.cs
+++ Content/Items/Beach/Acessories.SaltCogs.cs
@@ -6,6 +6,8 @@ namespace StarlightRiver.Content.Items.Beach
 {
 	class SaltCogs : SmartAccessory
 	{
+		public const int COG_DURATION = 240; // 240 = 2 seconds with the additional extra update. Will end up being shorter on sentries that already have an extra update set.
+
 		public override string Texture => AssetDirectory.Assets + "Items/Beach/" + Name;
 
 		public SaltCogs() : base("Salt Cogs", "Your sentries fire twice as fast for two seconds after placing them\n100% increased sentry placement speed") { }
@@ -33,7 +35,7 @@ namespace StarlightRiver.Content.Items.Beach
 				if (projectile.timeLeft == (Projectile.SentryLifeTime - 1))
 					projectile.extraUpdates += 1;
 
-				if (projectile.timeLeft == (Projectile.SentryLifeTime - 240))
+				if (projectile.timeLeft == (Projectile.SentryLifeTime - COG_DURATION))
 					projectile.extraUpdates -= 1;
 			}
 		}
@@ -44,7 +46,7 @@ namespace StarlightRiver.Content.Items.Beach
 
 			if (owner != null && Equipped(owner))
 			{
-				if (projectile.timeLeft >= (Projectile.SentryLifeTime - 240))
+				if (projectile.timeLeft >= (Projectile.SentryLifeTime - COG_DURATION))
 				{
 					int prog = Projectile.SentryLifeTime - projectile.timeLeft;
 
@@ -52,8 +54,8 @@ namespace StarlightRiver.Content.Items.Beach
 					Texture2D tex2 = ModContent.Request<Texture2D>("StarlightRiver/Assets/Items/Beach/SaltGearSmall").Value;
 					var color = new Color(255, 220, 220);
 
-					if (prog > 200)
-						color *= 1f - (prog - 200) / 40f;
+					if (prog > COG_DURATION - 40)
+						color *= 1f - (prog - (COG_DURATION - 40)) / 40f;
 
 					Main.spriteBatch.Draw(tex, projectile.Center - Main.screenPosition, null, color, projectile.timeLeft * 0.025f, tex.Size() / 2f, 1, 0, 0);
 					Main.spriteBatch.Draw(tex2, projectile.Center + new Vector2(22, -22) - Main.screenPosition, null, color, projectile.timeLeft * -0.05f, tex2.Size() / 2f, 1, 0, 0);
