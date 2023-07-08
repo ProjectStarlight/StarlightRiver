a42af7541d2a26c80806e8b04925a8bca7029cd3
bobfishkins23 <bobfishkins23@yahoo.com>
2023-07-08 12:59:18 -0400

    Transformation FX

--- Content/Items/Infernal/InfernalCatalyst.cs
+++ Content/Items/Infernal/InfernalCatalyst.cs
@@ -1,4 +1,5 @@
-﻿using Terraria.ID;
+﻿using StarlightRiver.Content.NPCs.Actors;
+using Terraria.ID;
 
 namespace StarlightRiver.Content.Items.Infernal
 {
@@ -7,5 +8,28 @@ namespace StarlightRiver.Content.Items.Infernal
 		public override string Texture => AssetDirectory.Debug;
 
 		public InfernalCatalyst() : base("Infernal Catalyst", "Primes the lavas of hell for transmutation", 9999, 0, ItemRarityID.Orange) { }
+
+		public override void Update(ref float gravity, ref float maxFallSpeed)
+		{
+			if (Item.lavaWet && Item.Center.Y > (Main.maxTilesY - 200) * 16)
+			{
+				NPC.NewNPC(Item.GetSource_FromThis(), (int)Item.Center.X, (int)Item.Center.Y, ModContent.NPCType<HellLavaActor>());
+
+				for (int k = 0; k < 20; k++)
+				{
+					float rand = Main.rand.NextFloat();
+					Dust.NewDustPerfect(Item.Center + Vector2.UnitX * Main.rand.NextFloat(-100, 100), ModContent.DustType<Dusts.Cinder>(), Vector2.UnitY * rand * -4, 0, new Color(255, (int)((1 - rand) * 200), 0), Main.rand.NextFloat(1f, 2f));
+					Dust.NewDustPerfect(Item.Center + Vector2.UnitX * Main.rand.NextFloat(-100, 100), ModContent.DustType<Dusts.Cinder>(), Vector2.UnitY * rand * -4, 0, new Color(255, (int)((1 - rand) * 200), 0), Main.rand.NextFloat(0.5f, 1.5f));
+				}
+
+				Projectile.NewProjectile(Item.GetSource_FromThis(), Item.Center + new Vector2(0, 20), Vector2.Zero, ModContent.ProjectileType<FirePillar>(), 0, 0, Main.myPlayer, 1.5f);
+
+				Helpers.Helper.PlayPitched("Magic/FireHit", 1, -0.5f, Item.Center);
+
+				Helpers.Helper.PlayPitched("Magic/FireHit", 0.5f, 0.5f, Item.Center);
+
+				Item.TurnToAir();
+			}
+		}
 	}
 }
--- Content/NPCs/Actors/HellLavaActor.cs
+++ Content/NPCs/Actors/HellLavaActor.cs
@@ -1,4 +1,7 @@
-﻿using Terraria.Graphics.Effects;
+﻿using System;
+using System.Collections.Generic;
+using Terraria.Graphics.Effects;
+using Terraria.ID;
 using static Terraria.ModLoader.ModContent;
 
 namespace StarlightRiver.Content.NPCs.Actors
@@ -8,12 +11,15 @@ namespace StarlightRiver.Content.NPCs.Actors
 		const int DUST_RANGE = 250;//used for horizontal dust distance and circularrange of underwater lights
 		const int ITEM_RANGE = 200;//circular range for detecting items
 
+		public ref float Timer => ref NPC.ai[0];
+		public ref float Fadeout => ref NPC.ai[1];
+
 		public override string Texture => AssetDirectory.Invisible;
 
 		public override void SetDefaults()
 		{
-			NPC.width = 1;
-			NPC.height = 1;
+			NPC.width = 200;
+			NPC.height = 10;
 			NPC.lifeMax = 100;
 			NPC.immortal = true;
 			NPC.dontTakeDamage = true;
@@ -25,54 +31,57 @@ namespace StarlightRiver.Content.NPCs.Actors
 
 		public override void AI()
 		{
+			Timer++;
+
+			if (Fadeout > 0)
+				Fadeout++;
+			/*
 			if (NPC.wet || WorldGen.SolidTile((int)(NPC.position.X / 16), (int)(NPC.position.Y / 16)))//float to surface if in water or blocks
 				NPC.position.Y -= 1;
 			else if (Main.tile[(int)(NPC.position.X / 16), (int)(NPC.position.Y / 16)].LiquidAmount < 1)
-				NPC.position.Y += 1;
-
-			int y = 0;
+				NPC.position.Y += 1;*/
 
 			float rand = Main.rand.NextFloat();
 			Dust.NewDustPerfect(NPC.Center + Vector2.UnitX * Main.rand.NextFloat(-100, 100), DustType<Dusts.Cinder>(), Vector2.UnitY * rand * -4, 0, new Color(255, (int)((1 - rand) * 200), 0), Main.rand.NextFloat());
 
-			/*
+			if (Timer % 16 == 0 && Main.rand.NextBool(2))
+				Projectile.NewProjectile(NPC.GetSource_FromThis(), NPC.Center + new Vector2(Main.rand.NextFloat(-80, 80), 20), Vector2.Zero, ProjectileType<FirePillar>(), 0, 0, Main.myPlayer, Main.rand.NextFloat(0.75f, 1f));
+
+			foreach (Item item in Main.item)
+			{
+				if (item.Hitbox.Intersects(NPC.Hitbox) && HellLavaConversion.GetConversionType(item) != 0)
+				{
+					item.type = HellLavaConversion.GetConversionType(item);
+					item.GetGlobalItem<HellTransformItem>().transformed = true;
+					item.velocity = Vector2.UnitY * -15;
+
+					for (int k = 0; k < 50; k++)
+					{
+						float rand2 = Main.rand.NextFloat();
+						Dust.NewDustPerfect(item.Center, DustType<Dusts.Cinder>(), Main.rand.NextVector2Circular(1, 1) * rand2 * -10, 0, new Color(255, (int)((1 - rand2) * 200), 0), Main.rand.NextFloat(1f, 3f));
+					}
+
+					Projectile.NewProjectile(item.GetSource_FromThis(), item.Center + new Vector2(0, 20), Vector2.Zero, ProjectileType<FirePillar>(), 0, 0, Main.myPlayer, 2.25f);
+
+					Projectile.NewProjectile(item.GetSource_FromThis(), item.Center + new Vector2(10, 20), Vector2.Zero, ProjectileType<FirePillar>(), 0, 0, Main.myPlayer, 1.25f);
+					Projectile.NewProjectile(item.GetSource_FromThis(), item.Center + new Vector2(-5, 20), Vector2.Zero, ProjectileType<FirePillar>(), 0, 0, Main.myPlayer, 1.5f);
+
+					Helpers.Helper.PlayPitched("Magic/FireHit", 1, -0.25f, item.Center);
+
+					Fadeout = 1;
+				}
+			}
+
 			if (Main.netMode == NetmodeID.SinglePlayer || Main.netMode == NetmodeID.MultiplayerClient)//clientside only
 			{
 				float distToPlayer = Vector2.Distance(Main.LocalPlayer.Center, NPC.Center);
 
-				if (distToPlayer < StarwaterConversion.MaxItemGlow)//makes the transformable items of nearby players glow
-					StarwaterConversion.StarwaterGlobalItemGlow = Math.Max(StarwaterConversion.StarwaterGlobalItemGlow, 1 - distToPlayer / StarwaterConversion.MaxItemGlow);
+				if (distToPlayer < HellLavaConversion.MAX_GLOW)//makes the transformable items of nearby players glow
+					HellLavaConversion.HellLavaItemGlow = Math.Max(HellLavaConversion.HellLavaItemGlow, 1 - distToPlayer / HellLavaConversion.MAX_GLOW);
 			}
 
-			Vector2 surfaceLightPos = NPC.Center + Vector2.UnitX * Main.rand.NextFloat(-DUST_RANGE, DUST_RANGE) + Vector2.UnitY * Main.rand.NextFloat(-6, 0);
-			Tile tile = Framing.GetTileSafely(surfaceLightPos);
-			Tile tileDown = Framing.GetTileSafely(surfaceLightPos + Vector2.UnitY * 16);
-
-			if ((tile.LiquidAmount > 0 && tile.LiquidType == LiquidID.Water || tileDown.LiquidAmount > 0 && tileDown.LiquidType == LiquidID.Water) && Main.rand.Next(10) > 3)//surface lights
-			{
-				//smaller dusts that home in on item if it exists (the dusts do the checking)
-				var d = Dust.NewDustPerfect(surfaceLightPos, ModContent.DustType<Dusts.AuroraSuction>(), Vector2.Zero, 200, new Color(Main.rand.NextBool(30) ? 200 : 0, Main.rand.Next(150), 255));
-				d.customData = new Dusts.AuroraSuctionData(this, Main.rand.NextFloat(0.6f, 0.8f));
-
-				//vertical light above water
-				if (Main.rand.NextBool())
-				{
-					bool red = Main.rand.NextBool(35);
-					bool green = Main.rand.NextBool(15) && !red;
-					var color = new Color(red ? 255 : Main.rand.Next(10), green ? 255 : Main.rand.Next(100), Main.rand.Next(240, 255));
-
-					Dust.NewDustPerfect(surfaceLightPos + new Vector2(0, Main.rand.Next(-4, 1)), DustType<Dusts.VerticalGlow>(), Vector2.UnitX * Main.rand.NextFloat(-0.15f, 0.15f), 200, color);
-				}
-			}
-
-			//circular area of dust
-			Vector2 circularLightPos = NPC.Center + Vector2.UnitX.RotatedByRandom(6.28f) * Main.rand.NextFloat(-DUST_RANGE, DUST_RANGE);
-			Tile tile2 = Framing.GetTileSafely(circularLightPos);
-			if (tile2.LiquidAmount > 0 && tile2.LiquidType == LiquidID.Water && Main.rand.NextBool(2))//under water lights
-			{
-				var d = Dust.NewDustPerfect(circularLightPos, DustType<Dusts.AuroraSuction>(), Vector2.One.RotatedByRandom(6.28f) * Main.rand.NextFloat(), 0, new Color(0, 50, 255), 0.5f);
-				d.customData = new Dusts.AuroraSuctionData(this, Main.rand.NextFloat(0.4f, 0.5f));
-			}*/
+			if (Fadeout > 60)
+				NPC.active = false;
 		}
 
 		public override void DrawBehind(int index)
@@ -85,17 +94,20 @@ namespace StarlightRiver.Content.NPCs.Actors
 			Texture2D tex = Request<Texture2D>("StarlightRiver/Assets/GlowTrailVertical").Value;
 
 			var target = new Rectangle(0, 0, 400, 200);
-			target.Offset((NPC.Center + new Vector2(-200, -200) - Main.screenPosition).ToPoint());
+			target.Offset((NPC.Center + new Vector2(-200, -196) - Main.screenPosition).ToPoint());
 
 			Effect effect = Filters.Scene["ColoredFire"].GetShader().Shader;
 
 			if (effect is null)
 				return;
 
+			float opacity = Timer < 60 ? Swoop(Timer / 60f) : 1;
+			opacity *= 1 - Fadeout / 60f;
+
 			effect.Parameters["u_time"].SetValue(Main.GameUpdateCount * 0.015f % 2f);
-			effect.Parameters["primary"].SetValue(new Vector3(1, 0.75f, 0));
+			effect.Parameters["primary"].SetValue(new Vector3(1, 0.75f, 0.05f) * opacity);
 			effect.Parameters["primaryScaling"].SetValue(new Vector3(1, 1.5f, 1));
-			effect.Parameters["secondary"].SetValue(new Vector3(0.75f, 0, 0));
+			effect.Parameters["secondary"].SetValue(new Vector3(0.75f, 0.05f, 0.05f) * opacity);
 
 			effect.Parameters["sampleTexture"].SetValue(Request<Texture2D>("StarlightRiver/Assets/Noise/MiscNoise3").Value);
 			effect.Parameters["mapTexture"].SetValue(Request<Texture2D>("StarlightRiver/Assets/Noise/MiscNoise3").Value);
@@ -103,10 +115,194 @@ namespace StarlightRiver.Content.NPCs.Actors
 			spriteBatch.End();
 			spriteBatch.Begin(default, BlendState.Additive, default, default, RasterizerState.CullNone, effect, Main.GameViewMatrix.TransformationMatrix);
 
-			spriteBatch.Draw(tex, target, Color.White);
+			spriteBatch.Draw(tex, target, Color.White * opacity);
 
 			spriteBatch.End();
 			spriteBatch.Begin(default, default, default, default, RasterizerState.CullNone, default, Main.GameViewMatrix.TransformationMatrix);
+		}
+
+		private float Swoop(float input)
+		{
+			if (input < 0.2f)
+				return input * 20;
+			else
+				return 4 - (input - 0.2f) / 0.8f * 3;
+		}
+	}
+
+	internal class FirePillar : ModProjectile
+	{
+		public override string Texture => AssetDirectory.Invisible;
+
+		public override void SetDefaults()
+		{
+			Projectile.tileCollide = false;
+			Projectile.timeLeft = 60;
+			Projectile.hide = true;
+		}
+
+		public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+		{
+			overWiresUI.Add(index);
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			SpriteBatch spriteBatch = Main.spriteBatch;
+
+			Texture2D tex = Request<Texture2D>("StarlightRiver/Assets/Misc/SpikeTell").Value;
+
+			var frame = new Rectangle(59, 0, 59, 120);
+
+			Effect effect = Filters.Scene["ColoredFire"].GetShader().Shader;
+
+			if (effect is null)
+				return false;
+
+			float Timer = 60 - Projectile.timeLeft;
+			float opacity = (Timer < 60 ? Swoop(Timer / 60f) : 1) * Projectile.ai[0];
+
+			effect.Parameters["u_time"].SetValue(Main.GameUpdateCount * 0.015f % 2f);
+			effect.Parameters["primary"].SetValue(new Vector3(1, 0.95f, 0.05f) * opacity);
+			effect.Parameters["primaryScaling"].SetValue(new Vector3(1, 1.5f, 1));
+			effect.Parameters["secondary"].SetValue(new Vector3(0.75f, 0.45f, 0.05f) * opacity);
+
+			effect.Parameters["sampleTexture"].SetValue(Request<Texture2D>("StarlightRiver/Assets/Noise/MiscNoise3").Value);
+			effect.Parameters["mapTexture"].SetValue(Request<Texture2D>("StarlightRiver/Assets/Noise/MiscNoise3").Value);
+
+			spriteBatch.End();
+			spriteBatch.Begin(default, BlendState.Additive, default, default, RasterizerState.CullNone, effect, Main.GameViewMatrix.TransformationMatrix);
+
+			spriteBatch.Draw(tex, Projectile.Center - Main.screenPosition, frame, Color.White * opacity, 0, new Vector2(28, 114), opacity, 0, 0);
+
+			spriteBatch.End();
+			spriteBatch.Begin(default, default, default, default, RasterizerState.CullNone, default, Main.GameViewMatrix.TransformationMatrix);
+
+			return false;
+		}
+
+		private float Swoop(float input)
+		{
+			if (input < 0.2f)
+				return input * 20;
+			else
+				return 4 - (input - 0.2f) / 0.8f * 3;
+		}
+	}
+
+	public class HellLavaConversion : IOrderedLoadable
+	{
+		public static float HellLavaItemGlow; //brightness for item glow, taken from distance of closetest starwater actor (0 - 1 scale)
+		public const float MAX_GLOW = 1000f;
+
+		private static Dictionary<int, int> ConversionTable; //from/to
+
+		public float Priority => 1f;
+
+		//returns 0 if there is no defined type
+		public static int GetConversionType(Item item)
+		{
+			if (ConversionTable.TryGetValue(item.type, out int conversionType))
+				return conversionType;
+
+			return 0;
+		}
+
+		public static bool ShouldItemGlow(Item item) //this does not include vanity items
+		{
+			return ConversionTable.ContainsKey(item.type);
+		}
+
+		private static void ResetInventoryGlow(StarlightPlayer Player)
+		{
+			//Main.NewText(StarwaterGlobalItemGlow);
+			if (HellLavaItemGlow > 0.075f)
+				HellLavaItemGlow *= 0.985f; //fades out the shine on items
+			else
+				HellLavaItemGlow = 0; //rounds to zero as there is a check on the item to save performance
+		}
+
+		public void Load()
+		{
+			StarlightPlayer.ResetEffectsEvent += ResetInventoryGlow;
+
+			ConversionTable = new()
+			{
+				{ ItemID.NightmarePickaxe, ItemID.MoltenPickaxe },
+				{ ItemID.WarAxeoftheNight, ItemID.MoltenHamaxe }
+			};
+		}
+
+		public void Unload()
+		{
+			StarlightPlayer.ResetEffectsEvent -= ResetInventoryGlow;
+			ConversionTable = null;
+		}
+	}
+
+	public class HellTransformItem : GlobalItem
+	{
+		public bool transformed;
+
+		public override bool InstancePerEntity => true;
+
+		public override GlobalItem Clone(Item item, Item itemClone)
+		{
+			return item.TryGetGlobalItem(out HellTransformItem gi) ? gi : base.Clone(item, itemClone);
+		}
+
+		public override bool PreDrawInInventory(Item Item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color ItemColor, Vector2 origin, float scale)
+		{
+			if (HellLavaConversion.HellLavaItemGlow != 0 && HellLavaConversion.ShouldItemGlow(Item))
+			{
+				RasterizerState RasterizerCullMode = spriteBatch.GraphicsDevice.RasterizerState;
+				SamplerState SamplerMode = spriteBatch.GraphicsDevice.SamplerStates[0];
+
+				spriteBatch.End();
+				spriteBatch.Begin(default, BlendState.Additive, SamplerMode, default, RasterizerCullMode, default, Main.UIScaleMatrix);
+
+				Texture2D tex = Request<Texture2D>("StarlightRiver/Assets/Keys/GlowSoft").Value;
+				spriteBatch.Draw(tex, position, null, new Color(255, 140, 60) * (HellLavaConversion.HellLavaItemGlow + (float)Math.Sin(StarlightWorld.visualTimer) * 0.2f), 0, tex.Size() / 2, 1, 0, 0);
+
+				spriteBatch.End();
+				spriteBatch.Begin(default, default, SamplerMode, default, RasterizerCullMode, default, Main.UIScaleMatrix);
+			}
+
+			return base.PreDrawInInventory(Item, spriteBatch, position, frame, drawColor, ItemColor, origin, scale);
+		}
+
+		public override void PostUpdate(Item Item)
+		{
+			if (transformed)
+			{
+				Item.velocity.Y *= 0.96f;
+
+				if (Item.velocity.Y > 0)
+					Item.velocity.Y *= 0.6f;
+
+				float rand2 = Main.rand.NextFloat();
+				Dust.NewDustPerfect(Item.Center, DustType<Dusts.Cinder>(), Main.rand.NextVector2Circular(1, 1) * rand2 * -3, 0, new Color(255, (int)((1 - rand2) * 200), 0), Main.rand.NextFloat());
+			}
+		}
+
+		public override bool PreDrawInWorld(Item Item, SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+		{
+			if (transformed)
+			{
+				RasterizerState RasterizerCullMode = spriteBatch.GraphicsDevice.RasterizerState;
+				SamplerState SamplerMode = spriteBatch.GraphicsDevice.SamplerStates[0];
+
+				spriteBatch.End();
+				spriteBatch.Begin(default, BlendState.Additive, SamplerMode, default, RasterizerCullMode, default, Main.GameViewMatrix.TransformationMatrix);
+
+				Texture2D tex = Request<Texture2D>("StarlightRiver/Assets/Keys/GlowSoft").Value;
+				spriteBatch.Draw(tex, Item.Center - Main.screenPosition, null, new Color(255, 140, 60), 0, tex.Size() / 2, 1, 0, 0);
+
+				spriteBatch.End();
+				spriteBatch.Begin(default, default, SamplerMode, default, RasterizerCullMode, default, Main.GameViewMatrix.TransformationMatrix);
+			}
+
+			return base.PreDrawInWorld(Item, spriteBatch, lightColor, alphaColor, ref rotation, ref scale, whoAmI);
 		}
 	}
 }
