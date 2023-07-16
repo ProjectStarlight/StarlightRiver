daf2852121cae4431d3ac9b2914107e70e604766
bobfishkins23 <bobfishkins23@yahoo.com>
2023-07-16 16:54:38 -0400

    UI updates

--- Content/PersistentData/BossRushDataStore.cs
+++ Content/PersistentData/BossRushDataStore.cs
@@ -7,17 +7,33 @@ namespace StarlightRiver.Content.PersistentData
 	[Flags]
 	internal enum BossrushUnlockFlag : int
 	{
-		Auroracle,
-		Glassweaver,
-		Ceiros
+		Auroracle = 1,
+		Glassweaver = 2,
+		Ceiros = 4
 	}
 
 	internal class BossRushDataStore : PersistentDataStore
 	{
 		BossrushUnlockFlag flags;
-		int normalScore;
-		int expertScore;
-		int masterScore;
+
+		public int normalScore;
+		public int expertScore;
+		public int masterScore;
+
+		/// <summary>
+		/// If the boss rush should be unlocked or not
+		/// </summary>
+		public static bool UnlockedBossRush => ((int)PersistentDataStoreSystem.GetDataStore<BossRushDataStore>().flags & 0b111) == 0b111;
+
+		/// <summary>
+		/// Returns if a given boss is recorded as being beaten or not
+		/// </summary>
+		/// <param name="flag">The flag to check for</param>
+		/// <returns>If that flag is true</returns>
+		public static bool DownedBoss(BossrushUnlockFlag flag)
+		{
+			return (PersistentDataStoreSystem.GetDataStore<BossRushDataStore>().flags & flag) != 0;
+		}
 
 		/// <summary>
 		/// Mark the given boss as downed for the purpose of unlocking boss rush
@@ -26,11 +42,13 @@ namespace StarlightRiver.Content.PersistentData
 		public static void DefeatBoss(BossrushUnlockFlag flag)
 		{
 			PersistentDataStoreSystem.GetDataStore<BossRushDataStore>().flags |= flag;
+			PersistentDataStoreSystem.GetDataStore<BossRushDataStore>().ForceSave();
 		}
 
 		public override void SaveGlobal(TagCompound tag)
 		{
 			tag["flags"] = (int)flags;
+
 			tag["normal"] = normalScore;
 			tag["expert"] = expertScore;
 			tag["master"] = masterScore;
@@ -40,6 +58,7 @@ namespace StarlightRiver.Content.PersistentData
 		public override void LoadGlobal(TagCompound tag)
 		{
 			flags = (BossrushUnlockFlag)tag.GetInt("flags");
+
 			normalScore = tag.GetInt("normal");
 			expertScore = tag.GetInt("expert");
 			masterScore = tag.GetInt("master");
