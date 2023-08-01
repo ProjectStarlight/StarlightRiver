3febac57a447977a5f7a6d63b4fd2c6a58f906f7
bobfishkins23 <bobfishkins23@yahoo.com>
2023-07-31 22:13:04 -0400

    Boss zen

---
+++ Core/Systems/BossZenSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace StarlightRiver.Core.Systems
+{
+	/// <summary>
+	/// Simple system that freezes mob spawns during modded bosses.
+	/// </summary>
+	internal class BossZenSystem : GlobalNPC
+	{
+		public override void EditSpawnRate(Player player, ref int spawnRate, ref int maxSpawns)
+		{
+			if (Main.npc.Any(n => n.active && n.boss && n.ModNPC?.Mod is StarlightRiver))
+			{
+				spawnRate = 0;
+				maxSpawns = 0;
+			}
+		}
+	}
+}
