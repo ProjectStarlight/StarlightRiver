0ca4b3370594267cec84eb9ce1e67f914f63aa21
SuperAndyHero <31840036+SuperAndyHero@users.noreply.github.com>
2024-03-19 21:19:58 -0500

    expert check and balance

--- Content/Items/Food/Special/Bonus.ShrimpSandwich.cs
+++ Content/Items/Food/Special/Bonus.ShrimpSandwich.cs
@@ -220,7 +220,7 @@ namespace StarlightRiver.Content.Items.Food.Special
 			if (info.player.RollLuck(chanceDenominator) < chanceNumerator)
 			{
 				const int BaseFishingLevel = 15;
-				int FishingLevel = BaseFishingLevel + info.player.fishingSkill + (int)(info.npc.value / 100f);
+				int FishingLevel = BaseFishingLevel + info.player.fishingSkill + (int)(info.npc.value / (Main.expertMode? 400f : 200f));
 				int playerPosY = (int)(info.player.position.Y / 16);
 				RollDropLevels(FishingLevel, out bool common, out bool uncommon, out bool rare, out bool veryrare, out bool legendary);
 				Main.NewText("fishing level: " + FishingLevel, Color.Gold);
