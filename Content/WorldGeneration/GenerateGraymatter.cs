e8264e2ac48385b562b5fec5897aac481ea35eb1
bobfishkins23 <bobfishkins23@yahoo.com>
2024-05-05 14:17:41 -0400

    Dendrite gen

---
+++ Content/WorldGeneration/GenerateGraymatter.cs
@@ -0,0 +1,39 @@
+﻿using StarlightRiver.Content.Tiles.Crimson;
+using StarlightRiver.Content.Tiles.Forest;
+using StarlightRiver.Content.Tiles.Palestone;
+using StarlightRiver.Helpers;
+using System;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.IO;
+using Terraria.WorldBuilding;
+using static Terraria.ModLoader.ModContent;
+
+namespace StarlightRiver.Core
+{
+	public partial class StarlightWorld : ModSystem
+	{
+		private void GraymatterGen(GenerationProgress progress, GameConfiguration configuration)
+		{
+			if (!WorldGen.crimson)
+				return;
+
+			progress.Message = "Thinking really hard...";
+			for (int k = 60; k < Main.maxTilesX - 60; k++)
+			{
+				if (WorldGen.genRand.NextBool(30)) //Dendrite
+				{
+					for (int y = 10; y < Main.worldSurface; y++)
+					{
+						if (Main.tile[k, y].TileType == TileID.CrimsonGrass)
+						{
+							WorldGen.TileRunner(k, y + 5, 5, 5, TileType<Dendrite>());
+							k += 16;
+						}
+					}
+				}
+
+			}
+		}
+	}
+}
