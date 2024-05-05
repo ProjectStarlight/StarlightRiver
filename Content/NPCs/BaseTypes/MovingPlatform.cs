296ee4ad0bdff63cd95263b6d97e36f672d415ba
bobfishkins23 <bobfishkins23@yahoo.com>
2024-05-05 12:45:59 -0400

    Square colliders

--- Content/NPCs/BaseTypes/MovingPlatform.cs
+++ Content/NPCs/BaseTypes/MovingPlatform.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
+using System.Linq;
 using Terraria.GameContent.Bestiary;
 using Terraria.ID;
 
@@ -17,21 +20,6 @@ namespace StarlightRiver.Content.NPCs.BaseTypes
 		public virtual void SafeSetDefaults() { }
 
 		public virtual void SafeAI() { }
-
-		public override bool? CanBeHitByProjectile(Projectile Projectile)
-		{
-			return false;
-		}
-
-		public override bool? CanBeHitByItem(Player Player, Item Item)
-		{
-			return false;
-		}
-
-		public override bool CheckActive()
-		{
-			return false;
-		}
 
 		public override void SetStaticDefaults()
 		{
@@ -60,6 +48,21 @@ namespace StarlightRiver.Content.NPCs.BaseTypes
 		public override void SetBestiary(BestiaryDatabase database, BestiaryEntry bestiaryEntry)
 		{
 			database.Entries.Remove(bestiaryEntry);
+		}
+
+		public override bool CheckActive()
+		{
+			return false;
+		}
+
+		public override bool? CanBeHitByProjectile(Projectile Projectile)
+		{
+			return false;
+		}
+
+		public override bool? CanBeHitByItem(Player Player, Item Item)
+		{
+			return false;
 		}
 
 		public sealed override void AI()
@@ -106,6 +109,211 @@ namespace StarlightRiver.Content.NPCs.BaseTypes
 
 			prevPos = NPC.position;
 			beingStoodOn = false;
+		}
+	}
+
+	internal class MovingPlatformSystem : ModSystem
+	{
+		public override void Load()
+		{
+			On_Player.SlopingCollision += PlatformCollision;
+
+			IL_Projectile.AI_007_GrapplingHooks += GrapplePlatforms;
+		}
+
+		public override void Unload()
+		{
+			IL_Projectile.AI_007_GrapplingHooks -= GrapplePlatforms;
+		}
+
+		private void PlatformCollision(On_Player.orig_SlopingCollision orig, Player self, bool fallThrough, bool ignorePlats)
+		{
+			if (self.GoingDownWithGrapple)
+			{
+				if (self.grapCount == 1)
+				{
+					//if the Player is using a single grappling hook we can check if they are colliding with it and its embedded in the moving platform, while its changing Y position so we can give the Player their jump back
+					foreach (int eachGrappleIndex in self.grappling)
+					{
+						if (eachGrappleIndex < 0 || eachGrappleIndex > Main.maxProjectiles)//somehow this can be invalid at this point?
+							continue;
+
+						Projectile grappleHookProj = Main.projectile[eachGrappleIndex];
+
+						foreach (NPC NPC in Main.npc)
+						{
+							if (!NPC.active || NPC.ModNPC == null || !(NPC.ModNPC is MovingPlatform))
+								continue;
+
+							if (grappleHookProj.active && NPC.Hitbox.Intersects(grappleHookProj.Hitbox) && self.Hitbox.Intersects(grappleHookProj.Hitbox))
+							{
+								self.position = grappleHookProj.position + new Vector2(grappleHookProj.width / 2 - self.width / 2, grappleHookProj.height / 2 - self.height / 2);
+								self.position += NPC.velocity;
+								self.velocity.Y = 0;
+								self.jump = 0;
+								self.fallStart = (int)(self.position.Y / 16f);
+							}
+						}
+					}
+				}
+
+				orig(self, fallThrough, ignorePlats);
+				return;
+			}
+
+			foreach (NPC npc in Main.ActiveNPCs)
+			{
+				if (npc.ModNPC == null || npc.ModNPC is not MovingPlatform || (npc.ModNPC as MovingPlatform).dontCollide)
+					continue;
+
+				var PlayerRect = new Rectangle((int)self.position.X, (int)self.position.Y + self.height, self.width, 1);
+				var NPCRect = new Rectangle((int)npc.position.X, (int)npc.position.Y, npc.width, 8 + (self.velocity.Y > 0 ? (int)self.velocity.Y : 0));
+
+				if (self.grapCount == 1 && npc.velocity.Y != 0)
+				{
+					//if the Player is using a single grappling hook we can check if they are colliding with it and its embedded in the moving platform, while its changing Y position so we can give the Player their jump back
+					foreach (int eachGrappleIndex in self.grappling)
+					{
+						if (eachGrappleIndex < 0 || eachGrappleIndex > Main.maxProjectiles)//somehow this can be invalid at this point?
+							continue;
+
+						Projectile grappleHookProj = Main.projectile[eachGrappleIndex];
+						if (grappleHookProj.active && npc.Hitbox.Intersects(grappleHookProj.Hitbox) && self.Hitbox.Intersects(grappleHookProj.Hitbox))
+						{
+							self.position = grappleHookProj.position + new Vector2(grappleHookProj.width / 2 - self.width / 2, grappleHookProj.height / 2 - self.height / 2);
+							self.position += npc.velocity;
+							self.velocity.Y = 0;
+							self.jump = 0;
+							self.fallStart = (int)(self.position.Y / 16f);
+
+							(npc.ModNPC as MovingPlatform).beingStoodOn = true;
+						}
+					}
+				}
+				else if (PlayerRect.Intersects(NPCRect) && self.position.Y <= npc.position.Y)
+				{
+					if (!self.justJumped && self.velocity.Y >= 0)
+					{
+						if (fallThrough)
+							self.GetModPlayer<StarlightPlayer>().platformTimer = 10;
+
+						self.gfxOffY = npc.gfxOffY;
+						self.velocity.Y = 0;
+						self.jump = 0;
+						self.fallStart = (int)(self.position.Y / 16f);
+						self.position.Y = npc.position.Y - self.height + 4;
+						self.position += npc.velocity;
+
+						(npc.ModNPC as MovingPlatform).beingStoodOn = true;
+					}
+				}
+			}
+
+			GravityPlayer mp = self.GetModPlayer<GravityPlayer>();
+
+			if (mp.controller != null && mp.controller.NPC.active)
+				self.velocity.Y = 0;
+
+			orig(self, fallThrough, ignorePlats);
+		}
+
+		private void GrapplePlatforms(ILContext il)
+		{
+			var c = new ILCursor(il);
+			c.TryGotoNext(i => i.MatchLdfld<Projectile>("aiStyle"), i => i.MatchLdcI4(7));
+			c.TryGotoNext(i => i.MatchLdfld<Projectile>("ai"), i => i.MatchLdcI4(0), i => i.MatchLdelemR4(), i => i.MatchLdcR4(0));
+			c.TryGotoNext(i => i.MatchLdloc(44)); //flag in source code
+			c.Index++;
+			c.Emit(OpCodes.Ldarg_0);
+			c.EmitDelegate<GrapplePlatformDelegate>(EmitGrapplePlatformDelegate);
+			c.TryGotoNext(i => i.MatchStfld<Player>("grapCount"));
+			c.Index++;
+			c.Emit(OpCodes.Ldarg_0);
+			c.EmitDelegate<UngrapplePlatformDelegate>(EmitUngrapplePlatformDelegate);
+		}
+
+		private delegate bool GrapplePlatformDelegate(bool fail, Projectile proj);
+		private bool EmitGrapplePlatformDelegate(bool fail, Projectile proj)
+		{
+			if (proj.timeLeft < 36000 - 3 && proj.TryGetGlobalProjectile(out GrapplingHookGlobal global))
+			{
+				NPC n = global.grappledTo;
+				if (n != null && n.active && n.ModNPC is MovingPlatform && !(n.ModNPC as MovingPlatform).dontCollide && n.Hitbox.Intersects(proj.Hitbox))
+				{
+					if (proj.ai[0] != 1)
+					{
+						proj.ai[0] = 2;
+						proj.netUpdate = true;
+					}
+
+					proj.position += n.velocity;
+
+					if (!proj.tileCollide) //this is kinda hacky but... oh well 
+						Terraria.Audio.SoundEngine.PlaySound(SoundID.Dig, proj.Center);
+
+					proj.tileCollide = true;
+
+					return false;
+				}
+			}
+
+			return fail;
+		}
+
+		private delegate void UngrapplePlatformDelegate(Projectile proj);
+		private void EmitUngrapplePlatformDelegate(Projectile proj)
+		{
+			Player Player = Main.player[proj.owner];
+			int numHooks = 3;
+			//time to replicate retarded vanilla hardcoding, wheee
+			if (proj.type == 165)
+				numHooks = 8;
+			if (proj.type == 256)
+				numHooks = 2;
+			if (proj.type == 372)
+				numHooks = 2;
+			if (proj.type == 652)
+				numHooks = 1;
+			if (proj.type >= 646 && proj.type <= 649)
+				numHooks = 4;
+			//end vanilla zoink
+
+			ProjectileLoader.NumGrappleHooks(proj, Player, ref numHooks);
+			if (Player.grapCount > numHooks)
+				Main.projectile[Player.grappling.OrderBy(n => (Main.projectile[n].active ? 0 : 999999) + Main.projectile[n].timeLeft).ToArray()[0]].Kill();
+		}
+	}
+
+	class GrapplingHookGlobal : GlobalProjectile
+	{
+		public NPC grappledTo;
+
+		public override bool InstancePerEntity => true;
+
+		public override bool AppliesToEntity(Projectile entity, bool lateInstantiation)
+		{
+			return entity.aiStyle == 7;
+		}
+
+		public override void PostAI(Projectile projectile)
+		{
+			if (grappledTo is null)
+			{
+				for (int k = 0; k < Main.maxNPCs; k++)
+				{
+					NPC n = Main.npc[k];
+					if (n.active && n.ModNPC is MovingPlatform && !(n.ModNPC as MovingPlatform).dontCollide && n.Hitbox.Intersects(projectile.Hitbox))
+					{
+						if (projectile.ai[0] != 1)
+						{
+							projectile.ai[0] = 2;
+							projectile.netUpdate = true;
+						}
+
+						grappledTo = n;
+					}
+				}
+			}
 		}
 	}
 }
--- Content/NPCs/BaseTypes/SquareCollider.cs
+++ Content/NPCs/BaseTypes/SquareCollider.cs
@@ -9,10 +9,47 @@ namespace StarlightRiver.Content.NPCs.BaseTypes
 	internal abstract class SquareCollider : MovingPlatform
 	{
 		public override bool CanFallThrough => false;
+	}
 
-		public override void SafeAI()
+	public class SquareColliderSystem : ModSystem
+	{
+		public override void Load()
 		{
-			
+			On_Player.DryCollision += SideCollision;
+		}
+
+		private void SideCollision(On_Player.orig_DryCollision orig, Player self, bool fallThrough, bool ignorePlats)
+		{
+			foreach (NPC npc in Main.ActiveNPCs)
+			{
+				if (npc.ModNPC == null || npc.ModNPC is not SquareCollider || (npc.ModNPC as SquareCollider).dontCollide)
+					continue;
+
+				var nextPlayer = self.Hitbox;
+				nextPlayer.Offset(self.velocity.ToPoint());
+
+				var nextBox = npc.Hitbox;
+				nextBox.Offset(npc.velocity.ToPoint());
+
+				var PlayerRect = new Rectangle((int)self.position.X, (int)self.position.Y + self.height, self.width, 1);
+				var NPCRect = new Rectangle((int)npc.position.X, (int)npc.position.Y, npc.width, 8 + (self.velocity.Y > 0 ? (int)self.velocity.Y : 0));
+
+				bool platforming = PlayerRect.Intersects(NPCRect);
+
+				if (nextPlayer.Intersects(nextBox) && !platforming)
+				{
+					if (self.position.X <= npc.position.X && self.velocity.X > npc.velocity.X)
+						self.velocity.X += npc.velocity.X - self.velocity.X;
+
+					if ((self.position.X + self.width) >= (npc.position.X + npc.width) && self.velocity.X < npc.velocity.X)
+						self.velocity.X += npc.velocity.X - self.velocity.X;
+
+					if ((self.position.Y + self.height) >= (npc.position.Y + npc.height) && self.velocity.Y < npc.velocity.Y)
+						self.velocity.Y += npc.velocity.Y - self.velocity.Y;
+				}
+			}
+
+			orig(self, fallThrough, ignorePlats);
 		}
 	}
 
@@ -28,7 +65,7 @@ namespace StarlightRiver.Content.NPCs.BaseTypes
 
 		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
 		{
-			spriteBatch.Draw();
+			spriteBatch.Draw(Assets.MagicPixel.Value, NPC.position - Main.screenPosition, null, Color.White, 0, Vector2.Zero, Assets.MagicPixel.Width()/500f, 0, 0);
 
 			return false;
 		}
