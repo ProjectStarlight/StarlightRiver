da876923e7a4308299f2c04eff79e4b73dceac71
bobfishkins23 <bobfishkins23@yahoo.com>
2024-05-19 16:33:47 -0400

    Merge fix

--- Content/Items/Debug.cs
+++ Content/Items/Debug.cs
@@ -3,12 +3,7 @@ using StarlightRiver.Content.Bosses.BrainRedux;
 using StarlightRiver.Content.Events;
 using StarlightRiver.Content.GUI;
 using StarlightRiver.Content.Items.Haunted;
-<<<<<<< HEAD
 using StarlightRiver.Content.Tiles.Crimson;
-=======
-using StarlightRiver.Content.PersistentData;
-using StarlightRiver.Core.Loaders.UILoading;
->>>>>>> master
 using StarlightRiver.Core.Systems;
 using StarlightRiver.Core.Systems.PersistentDataSystem;
 using Terraria.ID;
