3226075c57beb7f6e3c1f58baf44c8ac2094e3ec
bobfishkins23 <bobfishkins23@yahoo.com>
2024-05-21 00:33:51 -0400

    Custom UI Config element

---
+++ Content/GUI/Config/AbilityUIReposition.cs
@@ -0,0 +1,26 @@
+﻿using StarlightRiver.Content.Configs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarlightRiver.Content.GUI.Config
+{
+	internal class AbilityUIReposition : BaseUIRepositionElement
+	{
+		public override ref Vector2 modifying => ref ModContent.GetInstance<GUIConfig>().AbilityIconPosition;
+
+		public override void Update(GameTime gameTime)
+		{
+			base.Update(gameTime);
+
+			var dims = GetDimensions().ToRectangle();
+
+			if (dims.Contains(Main.MouseScreen.ToPoint()) && Main.mouseLeft)
+			{
+				Main.playerInventory = true;
+			}
+		}
+	}
+}
---
+++ Content/GUI/Config/BaseUIRepositionElement.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config.UI;
+using Terraria.UI;
+
+namespace StarlightRiver.Content.GUI.Config
+{
+	internal abstract class BaseUIRepositionElement : ConfigElement
+	{
+		public abstract ref Vector2 modifying { get; }
+		
+		public BaseUIRepositionElement()
+		{
+			Width.Set(0, 1f);
+			Recalculate();
+			var dims = GetDimensions().ToRectangle();
+
+			var ratio = Main.screenHeight / (float)Main.screenWidth;
+			Height.Set(dims.Width * ratio + 128, 0);
+			Recalculate();
+		}
+
+		public override void Draw(SpriteBatch spriteBatch)
+		{
+			Width.Set(0, 1f);
+			Recalculate();
+			var dims = GetDimensions().ToRectangle();
+
+			var ratio = Main.screenHeight / (float)Main.screenWidth;
+			Height.Set(dims.Width * ratio + 24 + 16 * ratio, 0);
+			Recalculate();
+
+			dims = GetDimensions().ToRectangle();
+			dims.Height = (int)(dims.Width * ratio);
+
+			Helpers.UIHelper.DrawBox(spriteBatch, GetDimensions().ToRectangle(), new Color(0.2f, 0.25f, 0.6f) * 0.2f);
+			Utils.DrawBorderString(spriteBatch, Label, dims.TopLeft() + Vector2.One * 8, Color.White, 0.8f);
+			Utils.DrawBorderString(spriteBatch, $"{Math.Round(modifying.X)}, {Math.Round(modifying.Y)}", dims.TopRight() + new Vector2(-8, 8), Color.White, 0.8f, 1f);
+
+			var preview = dims;
+			preview.Y += 24;
+			preview.Inflate(-16, (int)(-16 * ratio));
+
+			var mouseOver = preview.Contains(Main.MouseScreen.ToPoint());
+
+			preview.Inflate(4, 4);
+			Helpers.UIHelper.DrawBox(spriteBatch, preview, mouseOver ? Color.Orange : Color.DarkGray);
+			preview.Inflate(-4, -4);
+
+			spriteBatch.Draw(Assets.MagicPixel.Value, preview, new Color(0.2f, 0.2f, 0.2f));
+			spriteBatch.Draw(Main.screenTarget, preview, Color.White * 0.25f);
+
+			var flashColor = !mouseOver ? Color.White : Color.Lerp(Color.Orange, Color.White, 0.5f + (float)Math.Sin(Main.timeForVisualEffects * 0.2f) * 0.5f);
+
+			var tex = Assets.GUI.Infusions.Value;
+			spriteBatch.Draw(tex, preview.TopLeft() + modifying / Main.ScreenSize.ToVector2() * preview.Size(), null, flashColor, 0, tex.Size() / 2f, preview.Width / (float)Main.screenWidth, 0, 0);
+
+			if (mouseOver)
+			{
+				Main.graphics.GraphicsDevice.ScissorRectangle = new Rectangle(0, 0, Main.screenWidth, Main.screenHeight);
+				spriteBatch.Draw(tex, modifying, null, flashColor, 0, tex.Size() / 2f, 1f, 0, 0);
+			}
+		}
+
+		public override void Update(GameTime gameTime)
+		{
+			var dims = GetDimensions().ToRectangle();
+			var ratio = Main.screenHeight / (float)Main.screenWidth;
+
+			var preview = dims;
+			preview.Inflate(-16, (int)(-16 * ratio / 2));
+			preview.Y += 32;
+			preview.Height -= 32;
+
+			preview.Height = (int)(preview.Width * ratio);
+			Height.Set(preview.Width * ratio, 0);
+			Recalculate();
+
+			if (preview.Contains(Main.MouseScreen.ToPoint()) && Main.mouseLeft)
+			{
+				Vector2 relativePos = Main.MouseScreen - preview.TopLeft();
+				modifying = relativePos / preview.Size() * Main.ScreenSize.ToVector2();
+			}
+		}
+	}
+}
