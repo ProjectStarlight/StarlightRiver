affa329c80e0b3b79334ff8905ee6d167d8392f6
bobfishkins23 <bobfishkins23@yahoo.com>
2024-09-01 15:39:40 -0400

    Base

---
+++ Content/Items/Hell/Accessories.Misery.cs
@@ -0,0 +1,20 @@
+﻿using StarlightRiver.Content.Items.BaseTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarlightRiver.Content.Items.Hell
+{
+	internal class Misery : CursedAccessory
+	{
+		public override string Texture => AssetDirectory.HellItem + Name;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Misery's Company");
+			Tooltip.SetDefault("Nearby enemies have defense and innoculation equal to yours");
+		}
+	}
+}
