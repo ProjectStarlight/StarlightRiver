76e5da09975a2c9c3cefe8156345593761ff9d98
bobfishkins23 <bobfishkins23@yahoo.com>
2024-09-02 15:25:50 -0400

    Basic heavy flail

---
+++ Content/Items/BaseTypes/AbstractHeavyFlail.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria.ID;
+
+namespace StarlightRiver.Content.Items.BaseTypes
+{
+	internal abstract class AbstractHeavyFlail : ModItem
+	{
+		public abstract int ProjType { get; }
+
+		public override void SetDefaults()
+		{
+			Item.DamageType = DamageClass.MeleeNoSpeed;
+			Item.useAnimation = 10;
+			Item.useTime = 10;
+			Item.channel = true;
+			Item.shootSpeed = 10;
+			Item.noMelee = true;
+			Item.noUseGraphic = true;
+			Item.useStyle = ItemUseStyleID.Swing;
+
+			Item.shoot = ProjType;
+		}
+
+		public override bool CanShoot(Player player)
+		{
+			return !Main.projectile.Any(n => n.active && n.owner == player.whoAmI && n.type == ProjType && (n.ModProjectile as AbstractHeavyFlailProjectile).State < 4);
+		}
+	}
+
+	internal abstract class AbstractHeavyFlailProjectile : ModProjectile
+	{
+		public abstract Asset<Texture2D> ChainAsset { get; }
+		protected static Asset<Texture2D> BallAsset;
+
+		List<Vector2> chainPos = [];
+		List<Vector2> chainTarget = [];
+
+		protected bool slowing;
+
+		/// <summary>
+		/// The maximum length this flail can extend out to if it has room
+		/// </summary>
+		public virtual int MaxLength { get; set; } = 200;
+
+		public ref float Timer => ref Projectile.ai[0];
+		public ref float State => ref Projectile.ai[1];
+		public ref float Length => ref Projectile.ai[2];
+
+		protected Player Owner => Main.player[Projectile.owner];
+
+		/// <summary>
+		/// What the flail should do when it impacts tiles or an enemy
+		/// </summary>
+		public virtual void OnImpact(bool wasTile) { }
+
+		public override void SetStaticDefaults()
+		{
+			BallAsset = ModContent.Request<Texture2D>(Texture);
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.friendly = true;
+			Projectile.width = 32;
+			Projectile.height = 32;
+			Projectile.tileCollide = true;
+			Projectile.timeLeft = 180;
+			Projectile.penetrate = -1;
+		}
+
+		public override void AI()
+		{
+			//Owner.itemAnimation = 10;
+
+			if (Owner.channel)
+			{
+				Projectile.timeLeft = 180;
+			}
+			else if (State != 4)
+			{
+				Projectile.velocity *= 0.5f;
+				Timer = 0;
+				State = 4;
+			}
+
+			if (State == 0) // Extending out
+			{
+				Projectile.velocity *= 0.99f;
+				Projectile.velocity.Y += 0.6f;
+
+				if (Vector2.Distance(Owner.Center, Projectile.Center) >= MaxLength)
+				{
+					Projectile.velocity *= 0;
+					Length = MaxLength;
+					State = 1;
+				}
+			}
+			else if (State == 1) // Establish chain
+			{
+				int segments = (int)(Length / ChainAsset.Width());
+
+				for (int k = 0; k < segments; k++)
+				{
+					chainPos.Add(Vector2.Lerp(Owner.Center, Projectile.Center, k / (float)segments));
+					chainTarget.Add(Vector2.Lerp(Owner.Center, Projectile.Center, k / (float)segments));
+				}
+
+				State = Projectile.Center.X < Owner.Center.X ? 3 : 2;
+			}
+			else if (State == 2 || State == 3) // Swinging
+			{
+				if (State == 2 && Projectile.Center.X > Owner.Center.X - Length * 0.9f || State == 3 && Projectile.Center.X < Owner.Center.X + Length * 0.9f)
+				{
+					if (Timer < 60)
+						Timer++;
+				}
+				else
+				{
+					slowing = true;
+				}
+
+				if (!slowing)
+					Projectile.velocity = Projectile.DirectionTo(Owner.Center).RotatedBy(1.57f * (State == 2 ? 1 : -1)) * (Timer / 60f) * 46.7f;
+				else
+					Projectile.velocity.X *= 0.8f;
+
+				for (int k = 0; k < chainPos.Count; k++)
+				{
+					chainTarget[k] = Vector2.Lerp(Owner.Center, Projectile.Center, k / (float)chainPos.Count);
+					chainTarget[k] += Projectile.velocity * (float)Math.Sin(k / (float)chainPos.Count * 3.14f) * Length / 150f;
+				}
+
+				if (Vector2.Distance(Owner.Center, Projectile.Center) > Length)
+					Projectile.Center = Owner.Center + Owner.Center.DirectionTo(Projectile.Center) * Length;
+
+				if (State == 2 && slowing && Math.Abs(Projectile.velocity.X) < 0.01f)
+					SwitchSides();
+
+				if (State == 3 && slowing && Math.Abs(Projectile.velocity.X) < 0.01f)
+					SwitchSides();
+
+				Owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Owner.Center.DirectionTo(chainPos[1]).ToRotation() - 1.57f);
+				Owner.SetCompositeArmBack(true, Player.CompositeArmStretchAmount.Quarter, Owner.Center.DirectionTo(chainPos[1]).ToRotation() - 1.57f - 0.1f);
+				Owner.direction = Projectile.Center.X > Owner.Center.X ? 1 : -1;
+			}
+			else if (State == 4) // Retract
+			{
+				Projectile.rotation += Projectile.velocity.X * 0.0625f;
+				Projectile.velocity.X *= 0.99f;
+				Projectile.velocity.Y += 0.6f;
+
+				for (int k = 0; k < chainPos.Count; k++)
+				{
+					chainTarget[k] = Owner.Center;
+				}
+
+				if (chainPos.Count > 0)
+				{
+					chainPos.RemoveAt(chainPos.Count - 1);
+					chainTarget.RemoveAt(chainTarget.Count - 1);
+				}
+			}
+
+			// Update chain
+			for (int k = 0; k < chainPos.Count; k++)
+			{
+				chainPos[k] += (chainTarget[k] - chainPos[k]) * Math.Min(1f, Timer / 10f);
+			}
+		}
+
+		private void SwitchSides()
+		{
+			Projectile.velocity *= 0;
+
+			Timer = 0;
+			slowing = false;
+			State = State == 2 ? 3 : 2;
+		}
+
+		public override bool OnTileCollide(Vector2 oldVelocity)
+		{
+			if (State == 0)
+			{
+				OnImpact(true);
+				Projectile.velocity *= 0;
+				Length = Vector2.Distance(Owner.Center, Projectile.Center);
+				State = 1;
+			}
+			else if (State == 2 || State == 3)
+			{
+				if (oldVelocity.Length() >= 2)
+					OnImpact(true);
+
+				SwitchSides();
+			}
+			else if (State == 4)
+			{
+				if (Math.Abs(Projectile.velocity.Y) > 0.1f)
+					Projectile.velocity.Y *= -0.95f;
+				else
+					Projectile.velocity.Y *= 0;
+			}
+
+			return false;
+		}
+
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		{
+			OnImpact(false);
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			if (State == 0)
+			{
+				int max = (int)(Vector2.Distance(Owner.Center, Projectile.Center) / ChainAsset.Width());
+
+				for (int k = 0; k < max; k++)
+				{
+					var pos = Vector2.Lerp(Projectile.Center, Owner.Center, k / (float)max);
+					Main.EntitySpriteDraw(ChainAsset.Value, pos - Main.screenPosition, null, new Color(Lighting.GetSubLight(pos)), Owner.Center.DirectionTo(pos).ToRotation() - 1.57f, ChainAsset.Size() / 2f, 1f, 0, 0);
+				}
+			}
+
+			for (int k = 0; k < chainPos.Count; k++)
+			{
+				Vector2 prev = k > 0 ? chainPos[k - 1] : Owner.Center;
+				Vector2 curr = chainPos[k];
+				Main.EntitySpriteDraw(ChainAsset.Value, curr - Main.screenPosition, null, new Color(Lighting.GetSubLight(curr)), curr.DirectionTo(prev).ToRotation() - 1.57f, ChainAsset.Size() / 2f, 1f, 0, 0);
+			}
+
+			Vector2 ballPos = Projectile.oldPosition + Projectile.Size / 2f;
+			Main.EntitySpriteDraw(BallAsset.Value, ballPos - Main.screenPosition, null, lightColor, Projectile.rotation, BallAsset.Size() / 2f, Projectile.scale, 0, 0);
+
+			return false;
+		}
+	}
+}
---
+++ Content/Items/Forest/HeavyFlail.cs
@@ -0,0 +1,46 @@
+﻿using StarlightRiver.Content.Items.BaseTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarlightRiver.Content.Items.Forest
+{
+	internal class HeavyFlail : AbstractHeavyFlail
+	{
+		public override string Texture => AssetDirectory.ForestItem + Name;
+
+		public override int ProjType => ModContent.ProjectileType<HeavyFlailProjectile>();
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Heavy Flail");
+			Tooltip.SetDefault("Hold to swing a monstrous ball of metal\n`We've got the biggest balls of them all!`");
+		}
+
+		public override bool? UseItem(Player player)
+		{
+			return true;
+			for (int k = 100; k < 500; k += 100)
+			{
+				int i = Projectile.NewProjectile(null, player.Center, player.Center.DirectionTo(Main.MouseWorld) * k * 0.1f, ModContent.ProjectileType<HeavyFlailProjectile>(), 1, 1, player.whoAmI);
+				(Main.projectile[i].ModProjectile as HeavyFlailProjectile).MaxLength = k;
+			}
+
+			return true;
+		}
+	}
+
+	internal class HeavyFlailProjectile : AbstractHeavyFlailProjectile
+	{
+		public override string Texture => AssetDirectory.ForestItem + Name;
+
+		public override Asset<Texture2D> ChainAsset => Assets.Items.Forest.HeavyFlailChain;
+
+		public override void OnImpact(bool wasTile)
+		{
+			Main.NewText("Thunk!");
+		}
+	}
+}
