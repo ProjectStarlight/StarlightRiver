1718da34c669219bb8ca90284e05bd66696c28aa
ScalarVector <bobfishkins23@yahoo.com>
2025-01-25 10:19:04 -0500

    Whitespace fix

--- Content/Items/Crimson/Weapons.MirageBow.cs
+++ Content/Items/Crimson/Weapons.MirageBow.cs
@@ -9,11 +9,14 @@ namespace StarlightRiver.Content.Items.Crimson
 	internal class MirageBow : ModItem
 	{
 		private int cloneCooldown = 0;
+
 		public override string Texture => AssetDirectory.Debug;
+
 		public override void Load()
 		{
 			StarlightProjectile.OnHitNPCEvent += SpawnOnCrit;
 		}
+
 		public override void SetDefaults()
 		{
 			Item.damage = 15;
@@ -33,27 +36,35 @@ namespace StarlightRiver.Content.Items.Crimson
 			Item.useAmmo = AmmoID.Arrow;
 			Item.useTurn = true;
 		}
+
 		public override void UpdateInventory(Player player)
 		{
 			if (cloneCooldown > 0)
 				cloneCooldown--;
 		}
+
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 		{
 			Projectile clone = Main.projectile.FirstOrDefault(n => n.active && n.type == ModContent.ProjectileType<MirageBowClone>() && n.owner == player.whoAmI);
+
 			if (clone is null)
 				return true;
+
 			Projectile.NewProjectile(Item.GetSource_FromThis(), clone.Center, clone.Center.DirectionTo(Main.MouseWorld) * Item.shootSpeed * 2, type, damage / 2, knockback, player.whoAmI);
 			return true;
 		}
+
 		private void SpawnOnCrit(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
 		{
 			Player owner = Main.player[projectile.owner];
+
 			if (owner.active && owner.HeldItem.type == ModContent.ItemType<MirageBow>() && hit.Crit)
 			{
 				var moditem = owner.HeldItem.ModItem as MirageBow;
+
 				if (moditem is null)
 					return;
+
 				if (moditem.cloneCooldown <= 0)
 				{
 					Projectile.NewProjectile(owner.HeldItem.GetSource_FromThis(), owner.Center, Vector2.Zero, ModContent.ProjectileType<MirageBowClone>(), 1, 0, owner.whoAmI);
@@ -61,6 +72,7 @@ namespace StarlightRiver.Content.Items.Crimson
 				}
 			}
 		}
+
 		public override void AddRecipes()
 		{
 			Recipe recipe = CreateRecipe();
@@ -71,13 +83,16 @@ namespace StarlightRiver.Content.Items.Crimson
 			recipe.Register();
 		}
 	}
+
 	internal class MirageBowClone : ModProjectile
 	{
 		public override string Texture => AssetDirectory.Invisible;
+
 		public override void Load()
 		{
 			GraymatterBiome.onDrawHallucinationMap += DrawCloneHallucinations;
 		}
+
 		public override void SetDefaults()
 		{
 			Projectile.friendly = true;
@@ -87,6 +102,7 @@ namespace StarlightRiver.Content.Items.Crimson
 			Projectile.tileCollide = false;
 			Projectile.timeLeft = 60 * 5;
 		}
+
 		public override void AI()
 		{
 			GraymatterBiome.forceGrayMatter = true;
@@ -108,14 +124,17 @@ namespace StarlightRiver.Content.Items.Crimson
 				Projectile.netUpdate = true;
 			}
 		}
+
 		public override bool? CanHitNPC(NPC target)
 		{
 			return false;
 		}
+
 		public override bool PreDraw(ref Color lightColor)
 		{
 			if (!PlayerTarget.canUseTarget)
 				return false;
+
 			Player owner = Main.player[Projectile.owner];
 			Rectangle source = PlayerTarget.getPlayerTargetSourceRectangle(Projectile.owner);
 
@@ -125,6 +144,7 @@ namespace StarlightRiver.Content.Items.Crimson
 
 			return false;
 		}
+
 		private void DrawCloneHallucinations(SpriteBatch batch)
 		{
 			foreach (Projectile proj in Main.ActiveProjectiles)
