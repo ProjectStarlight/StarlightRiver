9abfedf9b47873b9a71396aab112331b33e25e51
Collin Summers <pumpkinboo2k6@gmail.com>
2026-02-05 14:10:20 -0500

    explosion start

--- Content/Items/Crimson/Weapons.ThoughtProvoker.cs
+++ Content/Items/Crimson/Weapons.ThoughtProvoker.cs
@@ -1,7 +1,10 @@
 ﻿using StarlightRiver.Content.Buffs.Summon;
 using StarlightRiver.Content.Items.Haunted;
 using StarlightRiver.Content.Items.Palestone;
+using StarlightRiver.Core.Loaders;
+using StarlightRiver.Core.Systems.PixelationSystem;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -284,6 +287,9 @@ namespace StarlightRiver.Content.Items.Crimson
 
 		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
 		{
+			if (Main.myPlayer == Projectile.owner)
+				Projectile.NewProjectile(Projectile.GetSource_OnHit(target), Projectile.Center, Vector2.Zero, ModContent.ProjectileType<ThoughtProvokerExplosion>(), Projectile.damage * 2, 5f, Projectile.owner, 50);
+
 			ResetToIdle();
 		}
 		public override bool? CanHitNPC(NPC target)
@@ -324,6 +330,141 @@ namespace StarlightRiver.Content.Items.Crimson
 			Projectile.position = Owner.Center;
 			resetTimer = 120;
 			AttackDelay = 45;
+		}
+	}
+	public class ThoughtProvokerExplosion : ModProjectile
+	{
+		private List<Vector2> cache;
+
+		private Trail trail;
+		private Trail trail2;
+		public override string Texture => AssetDirectory.Invisible;
+		private float Progress => Utils.Clamp(1 - Projectile.timeLeft / 30f, 0f, 1f);
+
+		private float Radius => Projectile.ai[0] * Eases.EaseQuinticOut(Progress);
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 2;
+			Projectile.height = 2;
+			Projectile.DamageType = DamageClass.Summon;
+			Projectile.friendly = true;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+			Projectile.timeLeft = 30;
+
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = 20;
+		}
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Greymatter Explosion");
+		}
+
+		public override void AI()
+		{
+			if (Main.netMode != NetmodeID.Server)
+			{
+				ManageCaches();
+				ManageTrail();
+			}
+
+			/*for (int k = 0; k < 6; k++)
+			{
+				float rot = Main.rand.NextFloat(0, 6.28f);
+
+				Dust.NewDustPerfect(Projectile.Center + Vector2.One.RotatedBy(rot) * Radius, DustID.Torch,
+					Vector2.One.RotatedBy(rot) * 0.5f, 0, default, Main.rand.NextFloat(1.5f, 3f)).noGravity = true;
+
+				Dust.NewDustPerfect(Projectile.Center + Vector2.One.RotatedBy(rot) * Radius, DustID.Torch,
+					Vector2.One.RotatedBy(rot) * 0.5f + Main.rand.NextVector2Circular(2f, 2f), 50, default, Main.rand.NextFloat(0.5f, 1f));
+			}*/
+		}
+
+		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+		{
+			Vector2 line = targetHitbox.Center.ToVector2() - Projectile.Center;
+			line.Normalize();
+			line *= Radius;
+			return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Projectile.Center, Projectile.Center + line);
+		}
+
+		public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+		{
+			//target.AddBuff(BuffID.OnFire, 300);
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			DrawPrimitives();
+			return false;
+		}
+
+		private void ManageCaches()
+		{
+			if (cache is null)
+			{
+				cache = [];
+
+				for (int i = 0; i < 40; i++)
+				{
+					cache.Add(Projectile.Center);
+				}
+			}
+
+			for (int k = 0; k < 40; k++)
+			{
+				cache[k] = Projectile.Center + Vector2.One.RotatedBy(k / 38f * 6.28f) * Radius;
+			}
+
+			while (cache.Count > 40)
+			{
+				cache.RemoveAt(0);
+			}
+		}
+
+		private void ManageTrail()
+		{
+			trail ??= new Trail(Main.instance.GraphicsDevice, 40, new TriangularTip(1), factor => 40 * (1f - Progress), factor => Color.Lerp(new Color(255, 180, 20), new Color(255, 20, 20), Eases.EaseQuinticInOut(Progress)));
+
+			trail2 ??= new Trail(Main.instance.GraphicsDevice, 40, new TriangularTip(1), factor => 30 * (1f - Progress), factor => Color.Lerp(new Color(255, 255, 255), new Color(255, 180, 20), Eases.EaseQuinticInOut(Progress)));
+
+			trail.Positions = cache.ToArray();
+			trail.NextPosition = cache[39];
+
+			trail2.Positions = cache.ToArray();
+			trail2.NextPosition = cache[39];
+		}
+
+		public void DrawPrimitives()
+		{
+			if (Projectile.timeLeft < 2)
+				return;
+
+			ModContent.GetInstance<PixelationSystem>().QueueRenderAction("UnderProjectiles", () =>
+			{
+				Effect effect = ShaderLoader.GetShader("CeirosRing").Value;
+
+				if (effect != null)
+				{
+					var world = Matrix.CreateTranslation(-Main.screenPosition.ToVector3());
+					Matrix view = Matrix.Identity;
+					var projection = Matrix.CreateOrthographicOffCenter(0, Main.screenWidth, Main.screenHeight, 0, -1, 1);
+
+					effect.Parameters["transformMatrix"].SetValue(world * view * projection);
+					effect.Parameters["time"].SetValue(Projectile.timeLeft * -0.01f);
+					effect.Parameters["repeats"].SetValue(5f);
+					effect.Parameters["sampleTexture"].SetValue(ModContent.Request<Texture2D>(AssetDirectory.Assets + "FireTrail").Value);
+
+					trail?.Render(effect);
+					trail2?.Render(effect);
+
+					effect.Parameters["sampleTexture"].SetValue(ModContent.Request<Texture2D>(AssetDirectory.Assets + "EnergyTrail").Value);
+
+					trail2?.Render(effect);
+				}
+			});
 		}
 	}
 }
