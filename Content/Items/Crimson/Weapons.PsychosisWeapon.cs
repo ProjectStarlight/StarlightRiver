abebfb155b149f30b96d389d088db1e6cc039691
Collin Summers <pumpkinboo2k6@gmail.com>
2026-02-21 16:48:33 -0500

    explosion start

--- Content/Items/Crimson/Weapons.PsychosisWeapon.cs
+++ Content/Items/Crimson/Weapons.PsychosisWeapon.cs
@@ -4,8 +4,10 @@ using StarlightRiver.Content.Items.Breacher;
 using StarlightRiver.Content.Items.Dungeon;
 using StarlightRiver.Content.Items.UndergroundTemple;
 using StarlightRiver.Content.Items.Vitric;
+using StarlightRiver.Core.Loaders;
 using StarlightRiver.Core.Systems.CameraSystem;
 using StarlightRiver.Core.Systems.InstancedBuffSystem;
+using StarlightRiver.Core.Systems.PixelationSystem;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -87,7 +89,7 @@ namespace StarlightRiver.Content.Items.Crimson
 		{
 			if (player.altFunctionUse == 2)
 			{
-				
+
 			}
 		}
 
@@ -125,7 +127,7 @@ namespace StarlightRiver.Content.Items.Crimson
 			{
 				Dust.NewDustPerfect(barrelPos + Main.rand.NextVector2Circular(5f, 5f), DustID.Torch, velocity.RotatedByRandom(0.5f).RotatedByRandom(0.5f) * Main.rand.NextFloat(0.25f), 0, default, 1.2f).noGravity = true;
 
-				Dust.NewDustPerfect(barrelPos + Main.rand.NextVector2Circular(5f, 5f), 
+				Dust.NewDustPerfect(barrelPos + Main.rand.NextVector2Circular(5f, 5f),
 					ModContent.DustType<PixelatedGlow>(), velocity.RotatedByRandom(0.5f).RotatedByRandom(0.5f) * Main.rand.NextFloat(0.2f), 0, c, 0.2f).noGravity = true;
 
 				Dust.NewDustPerfect(barrelPos + Main.rand.NextVector2Circular(5f, 5f),
@@ -201,7 +203,10 @@ namespace StarlightRiver.Content.Items.Crimson
 
 		int timer;
 
-		static Color DrawColor(bool psych) => psych ? PsychosisWeapon.Red() : PsychosisWeapon.Blue();
+		static Color DrawColor(bool psych)
+		{
+			return psych ? PsychosisWeapon.Red() : PsychosisWeapon.Blue();
+		}
 
 		public override void OnHitNPC(Projectile projectile, NPC target, NPC.HitInfo hit, int damageDone)
 		{
@@ -211,6 +216,8 @@ namespace StarlightRiver.Content.Items.Crimson
 
 				if (b != null && target.HasBuff(b.BackingType))
 				{
+					Explode(projectile, target, b.stacks.Count(), true);
+
 					Main.NewText("Cleared Neurosis, stack count: " + b.stacks.Count());
 					target.DelBuff(target.FindBuffIndex(b.BackingType));
 					(b as Neurosis).stacks.Clear();
@@ -237,9 +244,11 @@ namespace StarlightRiver.Content.Items.Crimson
 
 				if (b != null && target.HasBuff(b.BackingType))
 				{
+					Explode(projectile, target, b.stacks.Count(), false);
+
 					Main.NewText("Cleared Psychosis, stack count: " + b.stacks.Count());
 					target.DelBuff(target.FindBuffIndex(b.BackingType));
-					(b as Psychosis).stacks.Clear();				
+					(b as Psychosis).stacks.Clear();
 				}
 				else
 				{
@@ -268,8 +277,454 @@ namespace StarlightRiver.Content.Items.Crimson
 				{
 					Dust.NewDustPerfect(projectile.Center + new Vector2(0f, 1.5f * (float)Math.Sin(timer)).RotatedBy(projectile.velocity.ToRotation()) * 9f, ModContent.DustType<Dusts.PixelatedEmber>(),
 						projectile.velocity * 0.05f, 0, DrawColor(psychosis), 0.2f * (1 + (float)Math.Sin(timer / 2)));
-				}		
-			}			
+				}
+			}
+		}
+
+		void Explode(Projectile p, NPC npcHit, int count, bool psych)
+		{
+			int damage = (int)(30 + 100 * (count / 10f));
+
+			Color c = DrawColor(psych);
+
+			if (Main.myPlayer == p.owner)
+			{
+				Projectile.NewProjectile(p.GetSource_FromThis(), p.Center, Vector2.Zero, 
+					ModContent.ProjectileType<BrainBlastProjectile>(), damage, 2f, p.owner, 50, psych ? 1 : 0);
+			}
+
+			NPC[] closestNPCs = [.. Main.npc.Where(n => n.active && n.CanBeChasedBy() && n != npcHit && n.Distance(p.Center) < 500f).OrderBy(n => n.Distance(p.Center))];
+
+			for (int i = 0; i < (closestNPCs.Length > 5 ? 5 : closestNPCs.Length); i++)
+			{
+				Projectile.NewProjectile(p.GetSource_FromThis(), p.Center + Main.rand.NextVector2CircularEdge(20f, 20f), Vector2.Zero,
+					ModContent.ProjectileType<BrainBlastBolt>(), damage / 5, 1f, p.owner, closestNPCs[i].whoAmI);
+			}
+		}
+	}
+
+	public class BrainBlastBolt : ModProjectile
+	{
+		public int deathTimer;
+		public int flashTimer;
+
+		private List<Vector2> cache;
+		private Trail trail;
+		private Trail trail2;
+
+		public Vector2 startPos;
+		public override string Texture => AssetDirectory.Invisible;
+		public int TargetWhoAmI => (int)Projectile.ai[0];
+		public float Progress => 1f - Projectile.timeLeft / 30f;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Brain Blast Bolt");
+		}
+
+		public override void SetDefaults()
+		{
+			Projectile.Size = new Vector2(20);
+
+			Projectile.DamageType = DamageClass.Magic;
+
+			Projectile.hostile = false;
+			Projectile.friendly = true;
+
+			Projectile.tileCollide = false;
+
+			// five frame lifetime
+
+			Projectile.timeLeft = 30;
+			Projectile.extraUpdates = 6;
+
+			Projectile.penetrate = 2;
+		}
+
+		public override bool? CanDamage()
+		{
+			return Projectile.penetrate > 1;
+		}
+
+		public override bool? CanHitNPC(NPC target)
+		{
+			return target.whoAmI == TargetWhoAmI;
+		}
+
+		public override void OnSpawn(IEntitySource source)
+		{
+			startPos = Projectile.Center;
+			flashTimer = 90;
+		}
+
+		public override void AI()
+		{
+			if (Main.netMode != NetmodeID.Server)
+			{
+				ManageCaches();
+				ManageTrail();
+			}
+
+			if (flashTimer > 0)
+				flashTimer--;
+
+			if (deathTimer > 0)
+			{
+				Projectile.timeLeft = 2;
+
+				deathTimer--;
+
+				if (deathTimer == 1)
+					Projectile.Kill();
+
+				return;
+			}
+
+			Projectile.Center = Vector2.Lerp(startPos, Main.npc[TargetWhoAmI].Center, Progress) + Main.rand.NextVector2Circular(25f, 25f);
+
+			Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<PixelatedGlow>(), Projectile.position.DirectionTo(Projectile.oldPosition), 0, Color.Lerp(new Color(20, 20, 255, 0), new Color(255, 255, 255, 0), Progress), 0.3f);
+
+			Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<PixelatedGlow>(), Projectile.position.DirectionTo(Projectile.oldPosition).RotatedByRandom(1f) * Main.rand.NextFloat(0.8f, 3f), 0, new Color(200, 50, 50, 0), 0.2f);
+
+			if (Projectile.timeLeft == 1)
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<PixelatedEmber>(),
+						-Vector2.UnitY.RotatedByRandom(1.5f) * Main.rand.NextFloat(0.8f, 2f), 0, new Color(Main.rand.Next(150, 220), 50, 255, 0), 0.1f).customData = Main.rand.NextBool() ? -1 : 1;
+				}
+
+				for (int i = 0; i < 10; i++)
+				{
+					Dust.NewDustPerfect(Projectile.Center, ModContent.DustType<PixelatedGlow>(),
+						-Vector2.UnitY.RotatedByRandom(1.5f) * Main.rand.NextFloat(4f, 14f), 0, new Color(Main.rand.Next(150, 220), 50, 255, 0), 0.35f);
+				}
+
+				CameraSystem.shake += 4;
+
+				deathTimer = 600;
+				Projectile.timeLeft = 2;
+			}
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			DrawPrimitives();
+
+			Texture2D starTex = Assets.StarTexture_Alt.Value;
+			Texture2D bloomTex = Assets.Masks.GlowSoftAlpha.Value;
+			Texture2D bloomCircle = Assets.Masks.GlowWithRing.Value;
+
+			SpriteBatch sb = Main.spriteBatch;
+
+			Vector2 pos = startPos - Main.screenPosition;
+
+			float flashProg = flashTimer / 90f;
+
+			if (flashProg > 0)
+			{
+				Vector2 stretch = new Vector2(MathHelper.Lerp(5f, 1f, Eases.EaseQuinticOut(flashProg)), 1f);
+
+				sb.Draw(bloomCircle, pos, null, new Color(200, 80, 255, 0) * flashProg, 0f, bloomCircle.Size() / 2f, MathHelper.Lerp(0.1f, 0.3f, 1f - flashProg), 0f, 0f);
+				sb.Draw(bloomCircle, pos, null, new Color(255, 255, 255, 0) * flashProg, 0f, bloomCircle.Size() / 2f, MathHelper.Lerp(0.1f, 0.25f, 1f - flashProg), 0f, 0f);
+
+				sb.Draw(bloomTex, pos, null, new Color(200, 80, 255, 0) * flashProg, 0f, bloomTex.Size() / 2f, 3f, 0f, 0f);
+				sb.Draw(bloomTex, pos, null, new Color(255, 255, 255, 0) * flashProg * 0.8f, 0f, bloomTex.Size() / 2f, 2.5f, 0f, 0f);
+
+				sb.Draw(bloomTex, pos, null, new Color(200, 80, 255, 0) * flashProg, 0f, bloomTex.Size() / 2f, stretch, 0f, 0f);
+				sb.Draw(bloomTex, pos, null, new Color(255, 255, 255, 0) * flashProg * 0.8f, 0f, bloomTex.Size() / 2f, stretch * 0.75f, 0f, 0f);
+
+				sb.Draw(starTex, pos, null, new Color(200, 80, 255, 0) * flashProg, 0f, starTex.Size() / 2f, stretch, 0f, 0f);
+				sb.Draw(starTex, pos, null, new Color(255, 255, 255, 0) * flashProg * 0.8f, 0f, starTex.Size() / 2f, stretch * 0.75f, 0f, 0f);
+			}
+
+			return false;
+		}
+
+		private void ManageCaches()
+		{
+			Vector2 offset = new Vector2(Main.rand.NextFloat(-5f, 5f), 0f);
+
+			if (cache == null)
+			{
+				cache = new List<Vector2>();
+				for (int i = 0; i < 100; i++)
+				{
+					cache.Add(Projectile.Center);
+				}
+			}
+
+			cache.Add(Projectile.Center);
+
+			while (cache.Count > 100)
+			{
+				cache.RemoveAt(0);
+			}
+		}
+
+		private void ManageTrail()
+		{
+			trail ??= new Trail(Main.instance.GraphicsDevice, 100, new TriangularTip(190), factor => factor * 4f, factor =>
+			new Color(100, 100, 255) * TrailFade());
+
+			trail.Positions = cache.ToArray();
+			trail.NextPosition = Projectile.Center;
+
+			trail2 ??= new Trail(Main.instance.GraphicsDevice, 100, new TriangularTip(190), factor => factor * 12f, factor =>
+			Color.Lerp(new Color(50, 50, 255, 0), new Color(220, 65, 65, 0), 1f - deathTimer / 600f * TrailFade()));
+
+			trail2.Positions = cache.ToArray();
+			trail2.NextPosition = Projectile.Center;
+		}
+
+		private float TrailFade()
+		{
+			if (deathTimer <= 0)
+				return 1f;
+			else
+				return deathTimer / 600f;
+		}
+
+		public void DrawPrimitives()
+		{
+			ModContent.GetInstance<PixelationSystem>().QueueRenderAction("UnderTiles", () =>
+			{
+				Effect effect = ShaderLoader.GetShader("CeirosRing").Value;
+
+				if (effect != null)
+				{
+					var world = Matrix.CreateTranslation(-Main.screenPosition.ToVector3());
+					Matrix view = Matrix.Identity;
+					var projection = Matrix.CreateOrthographicOffCenter(0, Main.screenWidth, Main.screenHeight, 0, -1, 1);
+
+					effect.Parameters["time"].SetValue(Projectile.timeLeft * -0.05f);
+					effect.Parameters["repeats"].SetValue(1);
+					effect.Parameters["transformMatrix"].SetValue(world * view * projection);
+					effect.Parameters["sampleTexture"].SetValue(Assets.GlowTrail.Value);
+
+					trail2?.Render(effect);
+					trail?.Render(effect);
+				}
+			});
+		}
+	}
+
+	public class BrainBlastProjectile : ModProjectile
+	{
+		// not sure how performance intensive this is
+
+		private List<Vector2> cache;
+		private List<Vector2> cache2;
+
+		private Trail trail;
+		private Trail trail2;
+
+		private Trail trail3;
+		private Trail trail4;
+		public override string Texture => AssetDirectory.Invisible;
+		private float Progress => Utils.Clamp(1 - Projectile.timeLeft / 30f, 0f, 1f);
+
+		private float Radius => Projectile.ai[0] * Eases.EaseBackOut(Progress);
+
+		private bool Psychosis => Projectile.ai[1] == 1;
+
+		public override void SetDefaults()
+		{
+			Projectile.width = 2;
+			Projectile.height = 2;
+			Projectile.DamageType = DamageClass.Ranged;
+			Projectile.friendly = true;
+			Projectile.tileCollide = false;
+			Projectile.penetrate = -1;
+			Projectile.timeLeft = 30;
+
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = 20;
+		}
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("BRAIN BLAST!");
+		}
+
+		public override void AI()
+		{
+			if (Main.netMode != NetmodeID.Server)
+			{
+				ManageCaches();
+				ManageTrail();
+			}
+
+			if (Main.rand.NextBool(3))
+			{
+				for (int k = 0; k < 6; k++)
+				{
+					float rot = Main.rand.NextFloat(0, 6.28f);
+
+					Dust.NewDustPerfect(Projectile.Center + Vector2.One.RotatedBy(rot) * Radius, ModContent.DustType<PixelatedGlow>(),
+						-Vector2.UnitY * 5f * Progress, 0, new Color(50, 50, 255, 0), Main.rand.NextFloat(0.15f, 0.25f)).noGravity = true;
+				}
+			}
+		}
+
+		public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+		{
+			Vector2 line = targetHitbox.Center.ToVector2() - Projectile.Center;
+			line.Normalize();
+			line *= Radius;
+			return Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), Projectile.Center, Projectile.Center + line);
+		}
+
+		public override bool PreDraw(ref Color lightColor)
+		{
+			DrawPrimitives();
+
+			Texture2D starTex = Assets.StarTexture_Alt.Value;
+			Texture2D bloomTex = Assets.Masks.GlowSoftAlpha.Value;
+			Texture2D bloomCircle = Assets.Masks.GlowWithRing.Value;
+
+			SpriteBatch sb = Main.spriteBatch;
+
+			ModContent.GetInstance<PixelationSystem>().QueueRenderAction("OverPlayers", () =>
+			{
+				Main.spriteBatch.End();
+				Main.spriteBatch.Begin(default, default, default, default, default, default, Main.GameViewMatrix.EffectMatrix);
+
+				sb.Draw(bloomCircle, Projectile.Center - Main.screenPosition, null, new Color(50, 50, 255, 0) * (1f - Progress), 0f, bloomCircle.Size() / 2f, MathHelper.Lerp(0.2f, 0.45f, Eases.EaseCircularOut(Progress)), 0f, 0f);
+
+				Effect effect = ShaderLoader.GetShader("ElectricExplosion").Value;
+
+				if (effect is null)
+					return;
+
+				if (Progress >= 1f)
+					return;
+
+				Main.spriteBatch.End();
+				Main.spriteBatch.Begin(default, BlendState.AlphaBlend, Main.DefaultSamplerState, default, Main.Rasterizer, effect, Main.GameViewMatrix.EffectMatrix);
+
+				effect.Parameters["uTime"].SetValue((float)Main.timeForVisualEffects * 0.005f);
+
+				effect.Parameters["uImage1"].SetValue(Assets.Noise.ElectricNoise.Value);
+				effect.Parameters["uImage2"].SetValue(Assets.Noise.PerlinNoise.Value);
+				effect.Parameters["uProgress"].SetValue(Eases.EaseQuinticIn(Progress));
+				Color color = Color.Lerp(new Color(200, 60, 60), new Color(50, 50, 255), Eases.EaseQuinticIn(1f - Progress));
+
+				effect.Parameters["uColor"].SetValue(color.ToVector4());
+				effect.Parameters["uOpacity"].SetValue(0f);
+
+				effect.CurrentTechnique.Passes[0].Apply();
+
+				sb.Draw(bloomCircle, Projectile.Center - Main.screenPosition, null, Color.White, 0f, bloomCircle.Size() / 2f, MathHelper.Lerp(0.2f, 0.45f, Eases.EaseCircularOut(Progress)), 0f, 0f);
+
+				Main.spriteBatch.End();
+				Main.spriteBatch.Begin(default, default, default, default, default, default, Main.GameViewMatrix.TransformationMatrix);
+			});
+
+			return false;
+		}
+
+		private void ManageCaches()
+		{
+			if (cache is null)
+			{
+				cache = [];
+
+				for (int i = 0; i < 40; i++)
+				{
+					cache.Add(Projectile.Center);
+				}
+			}
+
+			float strength = MathHelper.Lerp(15f, 5f, Progress);
+
+			for (int k = 0; k < 40; k++)
+			{
+				cache[k] = Projectile.Center + Main.rand.NextVector2CircularEdge(strength, strength) + Vector2.One.RotatedBy(k / 38f * 6.28f) * Radius;
+			}
+
+			while (cache.Count > 40)
+			{
+				cache.RemoveAt(0);
+			}
+
+			if (cache2 is null)
+			{
+				cache2 = [];
+
+				for (int i = 0; i < 40; i++)
+				{
+					cache2.Add(Projectile.Center);
+				}
+			}
+
+			strength = MathHelper.Lerp(3f, 1f, Progress);
+
+			for (int k = 0; k < 40; k++)
+			{
+				cache2[k] = Projectile.Center + Main.rand.NextVector2CircularEdge(strength, strength) + Vector2.One.RotatedBy(k / 38f * 6.28f) * Radius * 0.85f;
+			}
+
+			while (cache2.Count > 40)
+			{
+				cache2.RemoveAt(0);
+			}
+		}
+
+		private void ManageTrail()
+		{
+			trail ??= new Trail(Main.instance.GraphicsDevice, 40, new TriangularTip(1), factor => 9 * (1f - Progress),
+				factor => new Color(100, 100, 255));
+
+			trail2 ??= new Trail(Main.instance.GraphicsDevice, 40, new TriangularTip(1), factor => 18 * (1f - Progress),
+				factor => Color.Lerp(new Color(50, 50, 255, 0), new Color(220, 65, 65, 0), Eases.EaseQuinticInOut(1f - Progress)));
+
+			trail.Positions = cache.ToArray();
+			trail.NextPosition = cache[39];
+
+			trail2.Positions = cache.ToArray();
+			trail2.NextPosition = cache[39];
+
+			trail3 ??= new Trail(Main.instance.GraphicsDevice, 40, new TriangularTip(1), factor => 9 * (1f - Progress),
+				factor => Color.White);
+
+			trail4 ??= new Trail(Main.instance.GraphicsDevice, 40, new TriangularTip(1), factor => 18 * (1f - Progress),
+				factor => Color.Lerp(new Color(210, 100, 255, 0), new Color(85, 65, 255, 0), Eases.EaseQuinticInOut(1f - Progress)));
+
+			trail3.Positions = cache2.ToArray();
+			trail3.NextPosition = cache2[39];
+
+			trail4.Positions = cache2.ToArray();
+			trail4.NextPosition = cache2[39];
+		}
+
+		public void DrawPrimitives()
+		{
+			if (Projectile.timeLeft < 2)
+				return;
+
+			ModContent.GetInstance<PixelationSystem>().QueueRenderAction("UnderProjectiles", () =>
+			{
+				Effect effect = ShaderLoader.GetShader("CeirosRing").Value;
+
+				if (effect != null)
+				{
+					var world = Matrix.CreateTranslation(-Main.screenPosition.ToVector3());
+					Matrix view = Matrix.Identity;
+					var projection = Matrix.CreateOrthographicOffCenter(0, Main.screenWidth, Main.screenHeight, 0, -1, 1);
+
+					effect.Parameters["transformMatrix"].SetValue(world * view * projection);
+					effect.Parameters["time"].SetValue(Projectile.timeLeft * -0.01f);
+					effect.Parameters["repeats"].SetValue(5f);
+					effect.Parameters["sampleTexture"].SetValue(Assets.GlowTrail.Value);
+
+					//trail3?.Render(effect);
+					//trail4?.Render(effect);
+
+					trail2?.Render(effect);
+					trail?.Render(effect);
+				}
+			});
 		}
 	}
 }
