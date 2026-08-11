6c926fe37836fd4d5754e6f1d004f876424214a3
bobfishkins23 <bobfishkins23@yahoo.com>
2026-08-10 21:19:59 -0400

    platform drawing cleanup

--- Content/Bosses/TheThinkerBoss/NPCs.BrainPlatform.cs
+++ Content/Bosses/TheThinkerBoss/NPCs.BrainPlatform.cs
@@ -42,31 +42,24 @@ namespace StarlightRiver.Content.Bosses.TheThinkerBoss
 			}
 		}
 
-		public override void DrawBehind(int index)
-		{
-			//Main.instance.DrawCacheNPCsMoonMoon.Add(index);
-		}
-
 		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
 		{
 			float dist = Vector2.Distance(NPC.Center, targetPos);
 
 			Texture2D tex = Assets.Bosses.TheThinkerBoss.BrainPlatform.Value;
-			LightingBufferRenderer.DrawWithLighting(tex, NPC.position - Main.screenPosition, tex.Bounds, Color.White * (ThisThinker?.ArenaOpacity ?? 1));
+			LightingBufferRenderer.DrawWithLighting(tex, NPC.position - screenPos, tex.Bounds, Color.White * (ThisThinker?.ArenaOpacity ?? 1));
 
-			Texture2D glowTex = Assets.Bosses.TheThinkerBoss.BrainPlatformGlow.Value;
-			Texture2D glowTex2 = Assets.Masks.GlowAlpha.Value;
+			Texture2D glowTex = Assets.Masks.GlowAlpha.Value;
 			Color glowColor = new Color(255, 100, 100, 0) * glow * 0.15f;
 
 			Rectangle target = NPC.Hitbox;
-			target.Offset((-Main.screenPosition).ToPoint());
+			target.Offset((-screenPos).ToPoint());
 			target.Height = tex.Height;
 
-			spriteBatch.Draw(glowTex2, target, null, glowColor, 0, default, 0, 0);
+			spriteBatch.Draw(glowTex, target, null, glowColor, 0, default, 0, 0);
 
-			//spriteBatch.Draw(glowTex, targetPos - NPC.Size / 2f - Main.screenPosition, glowColor);
-			target = new Rectangle((int)(targetPos.X - NPC.width / 2 - Main.screenPosition.X), (int)(targetPos.Y - NPC.height / 2 - Main.screenPosition.Y), tex.Width, tex.Height);
-			spriteBatch.Draw(glowTex2, target, null, glowColor, 0, default, 0, 0);
+			target = new Rectangle((int)(targetPos.X - NPC.width / 2 - screenPos.X), (int)(targetPos.Y - NPC.height / 2 - screenPos.Y), tex.Width, tex.Height);
+			spriteBatch.Draw(glowTex, target, null, glowColor, 0, default, 0, 0);
 
 			return false;
 		}
