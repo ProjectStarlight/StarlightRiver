eed7c6392ab55d5979064cf575fe85b5f399eb48
bobfishkins23 <bobfishkins23@yahoo.com>
2026-08-13 23:00:29 -0400

    Thought poliece leaves only when hallucinating

--- Content/NPCs/Crimson/ThoughtPolice.cs
+++ Content/NPCs/Crimson/ThoughtPolice.cs
@@ -1,5 +1,6 @@
 ﻿using StarlightRiver.Content.Biomes;
 using StarlightRiver.Content.Buffs;
+using StarlightRiver.Content.Items.Hovers;
 using StarlightRiver.Core.Loaders;
 using System;
 using System.Collections.Generic;
@@ -271,7 +272,6 @@ namespace StarlightRiver.Content.NPCs.Crimson
 		public override bool PreDraw(SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
 		{
 			Texture2D tex = Assets.NPCs.Crimson.ThoughtPolice.Value;
-			Texture2D over = Assets.NPCs.Crimson.ThoughtPoliceMask.Value;
 			var frame = new Rectangle((int)Variant * 104, (int)(Timer / 10 % 6) * 148, 104, 148);
 
 			if (State == ThoughtPoliceState.Aggroed || State == ThoughtPoliceState.Attack)
@@ -281,25 +281,6 @@ namespace StarlightRiver.Content.NPCs.Crimson
 				float colorPulse = 1f + MathF.Sin(Timer * 0.1f) * 0.35f;
 
 				spriteBatch.Draw(aura, NPC.Center - screenPos, null, new Color(255, 80, 80) * 0.25f * colorPulse, 0, aura.Size() / 2f, pulse, 0, 0);
-			}
-
-			Effect effect = ShaderLoader.GetShader("MirageItemFilter").Value;
-			var rasterizer = NPC.IsABestiaryIconDummy ? new RasterizerState() { ScissorTestEnable = true, CullMode = CullMode.None } : Main.Rasterizer;
-
-			if (effect != null)
-			{
-				effect.Parameters["u_color"].SetValue(Vector3.One);
-				effect.Parameters["u_fade"].SetValue(Vector3.One);
-				effect.Parameters["u_resolution"].SetValue(over.Size());
-				effect.Parameters["u_time"].SetValue(Main.GameUpdateCount * 0.01f);
-
-				spriteBatch.End();
-				spriteBatch.Begin(default, BlendState.Additive, SamplerState.LinearClamp, default, rasterizer, effect, Main.GameViewMatrix.TransformationMatrix);
-
-				spriteBatch.Draw(over, NPC.Center - screenPos, frame, Color.White, NPC.rotation, frame.Size() / 2f, NPC.scale, 0, 0);
-
-				spriteBatch.End();
-				spriteBatch.Begin(default, default, Main.DefaultSamplerState, default, rasterizer, default, Main.GameViewMatrix.TransformationMatrix);
 			}
 
 			spriteBatch.Draw(tex, NPC.Center - screenPos, frame, drawColor, NPC.rotation, frame.Size() / 2f, NPC.scale, 0, 0);
@@ -329,6 +310,27 @@ namespace StarlightRiver.Content.NPCs.Crimson
 					var color = new Color(255, 100, 100, (byte)(125 * tp.localScanOpacity));
 
 					spriteBatch.Draw(tex, pos, null, color, tp.AimRotation + 1.57f / 2f, origin, 1f, 0, 0);
+
+					Effect effect = ShaderLoader.GetShader("MirageItemFilter").Value;
+
+					if (effect != null)
+					{
+						Texture2D over = Assets.NPCs.Crimson.ThoughtPoliceMask.Value;
+
+						effect.Parameters["u_color"].SetValue(Vector3.One);
+						effect.Parameters["u_fade"].SetValue(Vector3.One);
+						effect.Parameters["u_resolution"].SetValue(over.Size());
+						effect.Parameters["u_time"].SetValue(Main.GameUpdateCount * 0.01f);
+
+						spriteBatch.End();
+						spriteBatch.Begin(default, default, SamplerState.LinearClamp, default, RasterizerState.CullNone, effect, Main.GameViewMatrix.TransformationMatrix);
+
+						var frame = new Rectangle((int)tp.Variant * 104, (int)(tp.Timer / 10 % 6) * 148, 104, 148);
+						spriteBatch.Draw(over, tp.NPC.Center - Main.screenPosition, frame, Color.White, tp.NPC.rotation, frame.Size() / 2f, tp.NPC.scale, 0, 0);
+
+						spriteBatch.End();
+						spriteBatch.Begin(default, default, SamplerState.PointWrap, default, RasterizerState.CullNone, default, Main.GameViewMatrix.ZoomMatrix);
+					}
 				}
 
 				toRender.RemoveAll(n => n is null || !n.NPC.active);
